b6f95b2ef677365ea983a5abc8331914439faf9f
Joshua Manchester <joshuamanchester4@gmail.com>
2023-12-20 15:51:37 +0000

    ssh: config

---
+++ hosts/common/global/ssh.nix
@@ -0,0 +1,18 @@
+{ inputs, lib, config, ... }:
+{
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+    };
+  };
+
+  programs.ssh = {
+    startAgent = true;
+    agentTimeout = "1h";
+    pubkeyAcceptedKeyTypes = [ "ssh-ed25519" ];
+  };
+
+  security.pam.enableSSHAgentAuth = true;
+}

5bb9899dcf6975e7733eea16bdb3fd1d2781168a
Joshua Manchester <joshuamanchester4@gmail.com>
2023-12-20 15:52:06 +0000

    hyprland: move screen shader

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -1,32 +1,10 @@
-{ config, lib, pkgs, ... }:
+{ inputs, config, lib, pkgs, ... }:
 
 {
   home.packages = with pkgs; [
     hyprshot
     swww
   ];
-
-  xdg.configFile."screenShader" = {
-    enable = true;
-    target = "hypr/screenShader.frag";
-    text = ''
-      precision mediump float;
-      varying vec2 v_texcoord;
-      uniform sampler2D tex;
-      uniform int output;
-
-      void main() {
-          // Apply gamma adjustment to monitor
-          if (output == 1) {
-              vec4 pixColor = texture2D(tex, v_texcoord);
-              pixColor.rgb = pow(pixColor.rgb, vec3(1.2));
-              gl_FragColor = pixColor;
-          } else {
-              gl_FragColor = texture2D(tex, v_texcoord);
-          }
-      }
-    '';
-  };
 
   wayland.windowManager.hyprland = {
     enable = true;
@@ -290,5 +268,27 @@
       ];
     };
 
+  };
+
+  xdg.configFile."screenShader" = {
+    enable = true;
+    target = "hypr/screenShader.frag";
+    text = ''
+      precision mediump float;
+      varying vec2 v_texcoord;
+      uniform sampler2D tex;
+      uniform int output;
+
+      void main() {
+          // Apply gamma adjustment to monitor
+          if (output == 1) {
+              vec4 pixColor = texture2D(tex, v_texcoord);
+              pixColor.rgb = pow(pixColor.rgb, vec3(1.2));
+              gl_FragColor = pixColor;
+          } else {
+              gl_FragColor = texture2D(tex, v_texcoord);
+          }
+      }
+    '';
   };
 }

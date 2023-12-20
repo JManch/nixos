7b7a2b70c4cfb91d7e11ce59650d2108dc5964f5
Joshua Manchester <joshuamanchester4@gmail.com>
2023-12-20 15:52:40 +0000

    hyprland: enable xdg portal

--- hosts/common/optional/desktop.nix
+++ hosts/common/optional/desktop.nix
@@ -1,6 +1,19 @@
+{ pkgs, ... }:
 {
   services.xserver = {
     enable = true;
     displayManager.lightdm.enable = false;
+  };
+
+  # INFO: Hopefully this can be moved to home-manager once
+  # https://github.com/nix-community/home-manager/pull/4707 is merged
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-hyprland
+      xdg-desktop-portal-gtk
+    ];
+    configPackages = [ pkgs.hyprland ];
   };
 }

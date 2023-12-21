41d870e1d2ec14a9610bf63e9f7a696761e47cc9
Joshua Manchester <JManch@protonmail.com>
2023-12-21 01:29:23 +0000

    ssh: only generate ed25519 host key

--- hosts/common/global/ssh.nix
+++ hosts/common/global/ssh.nix
@@ -6,6 +6,12 @@
       PasswordAuthentication = false;
       PermitRootLogin = "no";
     };
+    hostKeys = [
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
   };
 
   programs.ssh = {

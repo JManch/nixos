7b3f581e3f2031c6bbe143cbeb0712a20ce9756e
Joshua Manchester <JManch@protonmail.com>
2023-12-21 01:29:04 +0000

    users: add initial hashed password

--- hosts/common/users/joshua/default.nix
+++ hosts/common/users/joshua/default.nix
@@ -3,10 +3,10 @@
   users.users = {
     joshua = {
       isNormalUser = true;
-      # TODO: Setup ssh keys
-      openssh.authorizedKeys.keys = [ ];
       shell = pkgs.zsh;
+      initialHashedPassword = "$y$jFT$Rkz7TzHZjMUgbEwnWPB8V0$nM9cf5w99RCGuc9M8lFynwD2KYOnn9vSzui990k/cN7";
       hashedPasswordFile = config.age.secrets.joshuaPasswd.path;
+      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMd4QvStEANZSnTHRuHg0edyVdRmIYYTcViO9kCyFFt7 JManch@protonmail.com" ];
       extraGroups = ["wheel"];
     };
   };

8ffbb029f477b17e795f175f10924e5ee672b461
Joshua Manchester <JManch@protonmail.com>
2023-12-22 12:37:20 +0000

    virtualisation: remove invalid dconf setting

--- hosts/common/optional/virtualisation.nix
+++ hosts/common/optional/virtualisation.nix
@@ -2,10 +2,4 @@
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
   users.users."joshua".extraGroups = [ "libvirtd" ];
-  dconf.settings = {
-    "org/virt-manager/virt-manager/connections" = {
-      autoconnect = ["qemu:///system"];
-      uris = ["qemu:///system"];
-    };
-  };
 }

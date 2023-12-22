a06a0c0dd364e5b58956f1476459c87be96844be
Joshua Manchester <JManch@protonmail.com>
2023-12-22 12:37:01 +0000

    windows: fix boot entry

--- hosts/ncase-m1/windows.nix
+++ hosts/ncase-m1/windows.nix
@@ -1,11 +1,13 @@
 {
     # https://forum.endeavouros.com/t/tutorial-add-a-systemd-boot-loader-menu-entry-for-a-windows-installation-using-a-separate-esp-partition/37431
-    loader.systemd-boot.extraEntries = {
-      "windows.conf" = ''
-        title     Windows
-        efi       /tools/shellx64.efi
-        options   -nointerrupt -noconsolein -noconsoleout windows.nsh
-      ''
+    boot.loader.systemd-boot = {
+      extraEntries = {
+        "windows.conf" = ''
+          title     Windows
+          efi       /tools/shellx64.efi
+          options   -nointerrupt -noconsolein -noconsoleout windows.nsh
+        '';
+      };
+      consoleMode = "max";
     };
-    loader.systemd-boot.consoleMode = "max";
 }

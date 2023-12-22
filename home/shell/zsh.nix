0d305ed52c6e986f5f55d039b8d952b0522aa5e6
Joshua Manchester <JManch@protonmail.com>
2023-12-22 22:46:01 +0000

    zsh: move .zsh_history and .zcompdump

--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -4,6 +4,7 @@
   # TODO: Enable fzf zsh support in fzf module
   programs.zsh = {
     enable = true;
+    dotDir = ".config/zsh";
     syntaxHighlighting = {
       enable = true;
       styles = {
@@ -15,7 +16,9 @@
     };
     enableAutosuggestions = true;
     enableCompletion = true;
+    completionInit = "autoload -U compinit -d ${config.xdg.cacheHome}/zsh/zcompdump-$ZSH_VERSION && compinit";
     history = {
+      path = "${config.xdg.stateHome}/zsh/zsh_history";
       extended = true;
       ignoreDups = true;
       expireDuplicatesFirst = true;
@@ -24,5 +27,8 @@
       reload = "exec zsh";
       rebuild-home = "home-manager switch --flake ~/.config/nixos#joshua";
     };
+    initExtra = /* bash */ ''
+      setopt interactivecomments
+    '';
   };
 }

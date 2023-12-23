95485dfca9072678b6f2539454971167f690fdc4
Joshua Manchester <JManch@protonmail.com>
2023-12-23 20:38:38 +0000

    pipewire: add easyeffects

--- hosts/common/optional/pipewire.nix
+++ hosts/common/optional/pipewire.nix
@@ -1,10 +1,24 @@
-{
-  security.rtkit.enable = true;
-  hardware.pulseaudio.enable = false;
+{pkgs, ...}: {
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     jack.enable = true;
     pulse.enable = true;
+  };
+
+  hardware.pulseaudio.enable = false;
+
+  # Reduces latency
+  security.rtkit.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    easyeffects
+    helvum
+  ];
+
+  environment.persistence."/persist".users.joshua = {
+    directories = [
+      ".config/easyeffects"
+    ];
   };
 }

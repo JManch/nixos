3d1bf81702a8fb26d251b8be8b9eb995b1c1fca8
Joshua Manchester <JManch@protonmail.com>
2023-12-23 00:48:59 +0000

    virtualisation: add persist dir

--- hosts/common/optional/virtualisation.nix
+++ hosts/common/optional/virtualisation.nix
@@ -2,4 +2,10 @@
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
   users.users."joshua".extraGroups = ["libvirtd"];
+
+  environment.persistence."/persist" = {
+    directories = [
+      "/var/lib/libvirt"
+    ];
+  };
 }

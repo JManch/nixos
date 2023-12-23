ba990833040612a0d2f30279ab5d662d548ba147
Joshua Manchester <JManch@protonmail.com>
2023-12-23 01:49:52 +0000

    windows: remove pathExists checks

--- hosts/ncase-m1/windows.nix
+++ hosts/ncase-m1/windows.nix
@@ -1,10 +1,8 @@
-{lib, ...}: let
-  windowsSetup = builtins.pathExists /boot/tools/shellx64.efi && builtins.pathExists /boot/windows.nsh;
-in {
+{
   # https://forum.endeavouros.com/t/tutorial-add-a-systemd-boot-loader-menu-entry-for-a-windows-installation-using-a-separate-esp-partition/37431
   # mirror: https://archive.is/wwZaP
   # NOTE: This requires setting up the /tools/shellx64.efi and /windows.nsh files in the ESP partition
-  boot.loader.systemd-boot = lib.mkIf windowsSetup {
+  boot.loader.systemd-boot = {
     extraEntries = {
       "windows.conf" = ''
         title     Windows
@@ -12,10 +10,9 @@ in {
         options   -nointerrupt -noconsolein -noconsoleout windows.nsh
       '';
     };
-    consoleMode = "max";
   };
 
-  programs.zsh.shellAliases = lib.mkIf windowsSetup {
+  programs.zsh.shellAliases = {
     bootwindows = "systemctl reboot --boot-loader-entry=windows.conf";
   };
 }

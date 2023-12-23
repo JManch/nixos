5f2212c83c4cf7d6a6f0e328c4b52eb15fac02ef
Joshua Manchester <JManch@protonmail.com>
2023-12-23 00:51:34 +0000

    windows: enable conditional boot entry

--- hosts/ncase-m1/windows.nix
+++ hosts/ncase-m1/windows.nix
@@ -1,6 +1,10 @@
-{
+{lib, ...}: let
+  windowsSetup = builtins.pathExists /boot/tools/shellx64.efi && builtins.pathExists /boot/windows.nsh;
+in {
   # https://forum.endeavouros.com/t/tutorial-add-a-systemd-boot-loader-menu-entry-for-a-windows-installation-using-a-separate-esp-partition/37431
-  boot.loader.systemd-boot = {
+  # mirror: https://archive.is/wwZaP
+  # NOTE: This requires setting up the /tools/shellx64.efi and /windows.nsh files in the ESP partition
+  boot.loader.systemd-boot = lib.mkIf windowsSetup {
     extraEntries = {
       "windows.conf" = ''
         title     Windows
@@ -9,5 +13,9 @@
       '';
     };
     consoleMode = "max";
+  };
+
+  programs.zsh.shellAliases = lib.mkIf windowsSetup {
+    bootwindows = "systemctl reboot --boot-loader-entry=windows.conf";
   };
 }

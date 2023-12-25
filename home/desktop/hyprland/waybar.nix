96b255ddec86c86ce3fe80037711c1ab944df72e
Joshua Manchester <JManch@protonmail.com>
2023-12-25 01:07:33 +0000

    waybar: target hyprland service

--- home/desktop/hyprland/waybar.nix
+++ home/desktop/hyprland/waybar.nix
@@ -6,7 +6,10 @@
 }: {
   programs.waybar = {
     enable = true;
-    systemd.enable = true;
+    systemd = {
+      enable = true;
+      target = "hyprland-session.target";
+    };
     style =
       /*
       css

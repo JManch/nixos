d7dda371da4a6f579f46f1d4fa64f44a875097d0
Joshua Manchester <JManch@protonmail.com>
2023-12-29 01:47:17 +0000

    home: implement new module options

--- home/ncase-m1.nix
+++ home/ncase-m1.nix
@@ -1,13 +1,11 @@
-{
-  pkgs,
-  config,
-  ...
+{ pkgs
+, config
+, ...
 }: {
   imports = [
     ./global.nix
 
     ./shell
-    ./desktop/hyprland
     ./desktop/gtk.nix
     ./programs/alacritty.nix
     ./programs/firefox.nix
@@ -23,48 +21,51 @@
   ];
 
   desktop = {
-    enable = true;
     compositor = "hyprland";
     monitors = [
       {
         name = "DP-2";
-        primary = true;
-        refreshRate = 120;
+        number = 1;
+        refreshRate = 120.0;
         width = 2560;
         height = 1440;
-        workspaces = ["1" "3" "5" "7" "9"];
+        position = "2560x0";
+        workspaces = [ 1 3 5 7 9 ];
       }
       {
         name = "HDMI-A-1";
+        number = 2;
         refreshRate = 59.951;
         width = 2560;
         height = 1440;
-        workspaces = ["2" "4" "6" "8"];
+        position = "0x0";
+        workspaces = [ 2 4 6 8 ];
       }
       {
         name = "DP-3";
+        number = 3;
         width = 2560;
         height = 1440;
         enabled = false;
       }
     ];
-    wayland = {
-      swaylock = {
-        enable = true;
-        lockScript = ''
-          # Temporarily disable shader for screenshot
-          COMMAND="${config.wayland.windowManager.hyprland.package} keyword decoration:screen_shader ${config.xdg.configHome}/hypr/shaders/"
-          ''${COMMAND}blank.frag > /dev/null 2>&1
-          ${config.swaylock.package}/bin/swaylock -f
-          ${pkgs.coreutils}/bin/sleep 0.05
-          ''${COMMAND}monitor1_gamma.frag > /dev/null 2>&1
-        '';
-      };
-      swayidle = {
-        enable = true;
-        lockTime = 3 * 60;
-        lockedScreenOffTime = 2 * 60;
-      };
+    swaylock = {
+      enable = true;
+      lockScript = ''
+        # Temporarily disable shader for screenshot
+        COMMAND="${config.wayland.windowManager.hyprland.package} keyword decoration:screen_shader ${config.xdg.configHome}/hypr/shaders/"
+        ''${COMMAND}blank.frag > /dev/null 2>&1
+        ${config.programs.swaylock.package}/bin/swaylock -f
+        ${pkgs.coreutils}/bin/sleep 0.05
+        ''${COMMAND}monitor1_gamma.frag > /dev/null 2>&1
+      '';
     };
+    swayidle = {
+      enable = true;
+      lockTime = 3 * 60;
+      lockedScreenOffTime = 2 * 60;
+    };
+    anyrun.enable = true;
+    waybar.enable = false;
   };
 }

6049a73cfa75000437ef3ba2ee19f5dad8a5c5d0
Joshua Manchester <JManch@protonmail.com>
2023-12-29 01:45:50 +0000

    desktop: add options to monitor submodule

--- modules/home-manager/desktop/default.nix
+++ modules/home-manager/desktop/default.nix
@@ -1,7 +1,6 @@
-{
-  lib,
-  config,
-  ...
+{ lib
+, config
+, ...
 }:
 with lib; let
   monitorSubmodule = {
@@ -10,9 +9,8 @@ with lib; let
         type = types.str;
         example = "DP-1";
       };
-      primary = mkOption {
-        type = types.bool;
-        default = false;
+      number = mkOption {
+        type = types.int;
       };
       width = mkOption {
         type = types.int;
@@ -23,41 +21,66 @@ with lib; let
         example = 1440;
       };
       refreshRate = mkOption {
-        type = types.int;
+        type = types.float;
         default = 60;
+      };
+      position = mkOption {
+        type = types.str;
+        example = "0x0";
+        description = "Relative position of the monitor from the top left corner.";
       };
       enabled = mkOption {
         type = types.bool;
         default = true;
       };
       workspaces = mkOption {
-        type = types.listOf types.str;
-        default = null;
+        type = types.listOf types.int;
+        default = [ ];
+        description = "Workspaces to put on the monitor.";
       };
     };
   };
-in {
+in
+{
+  imports = [
+    ./wayland
+  ];
   options.desktop = {
-    enable = mkEnableOption "desktop environment";
     compositor = mkOption {
-      type = with types; nullOr (enum []);
+      type = with types; nullOr (enum [ ]);
       description = ''
         The desktop compositor to use.
       '';
     };
     monitors = mkOption {
       type = types.listOf (types.submodule monitorSubmodule);
-      default = [];
+      default = [ ];
+    };
+    cursorSize = mkOption {
+      type = types.int;
+      default = 24;
     };
   };
-  config = {
-    assertions = [
-      {
-        assertion =
-          ((lib.length config.monitors) != 0)
-          -> ((lib.length (lib.filter (m: m.primary) config.monitors)) == 1);
-        message = "Exactly one monitor must be set to primary.";
-      }
-    ];
-  };
+  config =
+    let
+      monitors = config.desktop.monitors;
+    in
+    {
+      assertions = [
+        {
+          assertion =
+            ((lib.length monitors) != 0)
+            -> ((lib.length (lib.filter (m: m.number == 1) monitors)) == 1);
+          message = "Exactly one monitor must be set to primary (number 1).";
+        }
+        {
+          assertion = allUnique (map (m: m.number) monitors);
+          message = "Monitor numbers must be unique.";
+        }
+        # {
+        #   assertion = lib.lists.all (x: y: x + 1 == y) (lib.lists.sort (map (m: m.number) monitors));
+        #   message = "Monitor numbers must be sequential.";
+        # }
+      ];
+    };
 }

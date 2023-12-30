794b7ffcd1f0ef8e1c3f8661662050d61ecd840f
Joshua Manchester <JManch@protonmail.com>
2023-12-30 23:38:43 +0000

    gtk: remove old config

--- modules/home-manager/desktop/gtk.nix
+++ modules/home-manager/desktop/gtk.nix
@@ -9,7 +9,12 @@ lib.mkIf osConfig.usrEnv.desktop.enable {
     enable = true;
     theme = {
       name = "Plata-Noir-Compact";
-      package = pkgs.plata-theme;
+      package = pkgs.plata-theme.override {
+        selectionColor = "#${config.colorscheme.colors.base01}";
+        accentColor = "#${config.colorscheme.colors.base02}";
+        suggestionColor = "#${config.colorscheme.colors.base0D}";
+        destructionColor = "#${config.colorscheme.colors.base08}";
+      };
     };
     iconTheme = {
       name = "Papirus-Dark";

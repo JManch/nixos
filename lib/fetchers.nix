cfa8c65adbf1a2b3d1f9c121f20a5370759064a7
Joshua Manchester <JManch@protonmail.com>
2023-12-31 23:35:18 +0000

    lib: handle hosts with a single monitor

--- lib/fetchers.nix
+++ lib/fetchers.nix
@@ -1,4 +1,4 @@
 lib: {
   primaryMonitor = config: lib.lists.findFirst (m: m.number == 1) (throw "Attempted to access primary monitors but monitor config has not been set.") config.device.monitors;
-  getMonitorByNumber = config: number: lib.lists.findFirst (m: m.number == number) (throw "Monitor number ${builtins.toString number} is not defined.") config.device.monitors;
+  getMonitorByNumber = config: number: lib.lists.findFirst (m: m.number == number) (builtins.head config.device.monitors) config.device.monitors;
 }

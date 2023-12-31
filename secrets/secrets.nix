8ff6fd9f74bb0f26c6ef1d1ce9f72a70ebb81a4e
Joshua Manchester <JManch@protonmail.com>
2023-12-31 22:33:14 +0000

    virtual: add personal ssh key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   joshua = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMd4QvStEANZSnTHRuHg0edyVdRmIYYTcViO9kCyFFt7 JManch@protonmail.com";
   ncase-m1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGmso00Sb3ab0dURSMJGAYN10OQQQ3VlPxHtNz092a8s root@ncase-m1";
+  virtual = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIRMA3ZtwZr/w9QG7iFfWFFSCAIjxw0XSejZPGHdYCzW root@virtual";
 in
 {
-  "joshuaPasswd.age".publicKeys = [ ncase-m1 joshua ];
+  "joshuaPasswd.age".publicKeys = [ ncase-m1 virtual joshua ];
 }

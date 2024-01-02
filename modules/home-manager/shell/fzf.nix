d5fa21957b21f987cd0954c75b1e944f9a41ed19
Joshua Manchester <JManch@protonmail.com>
2024-01-02 18:46:03 +0000

    fzf: configure colorscheme and shortcut commands

--- modules/home-manager/shell/fzf.nix
+++ modules/home-manager/shell/fzf.nix
@@ -1,15 +1,39 @@
 { lib
 , config
+, pkgs
 , ...
 }:
+let
+  fd = "${pkgs.fd}/bin/fd";
+  bat = "${pkgs.bat}/bin/bat";
+  colors = config.colorscheme.colors;
+in
 lib.mkIf config.modules.shell.enable {
   programs.fzf = {
     enable = true;
+    defaultCommand = "${fd} -H --type f";
+    defaultOptions = [
+      "--height 80%"
+      "--bind ctrl-p:preview-up,ctrl-n:preview-down,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down"
+      "--border rounded"
+    ];
+    changeDirWidgetCommand = "${fd} --type d --hidden --exclude .git";
+    changeDirWidgetOptions = [ ];
+    fileWidgetCommand = "${fd} --type f --hidden --exclude .git --exclude .cache";
+    fileWidgetOptions = [ "--preview '${bat} --style=numbers --color=always --line-range :500 {}'" ];
     colors = {
-      # TODO: Properly setup this colorscheme
-      # bg = "#${config.colorscheme.colors.base00}";
-      # fg = "#${config.colorscheme.colors.base05}";
-      # hl = "${config.colorscheme.colors.base0B}";
+      bg = "-1";
+      "bg+" = "-1";
+      fg = "#${colors.base04}";
+      "fg+" = "#${colors.base06}";
+      hl = "#${colors.base0D}";
+      "hl+" = "#${colors.base0D}";
+      spinner = "#${colors.base0C}";
+      header = "#${colors.base0D}";
+      info = "#${colors.base0A}";
+      pointer = "#${colors.base0C}";
+      marker = "#${colors.base0C}";
+      prompt = "#${colors.base0A}";
     };
   };
 }

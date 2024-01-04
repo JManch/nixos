5d0ed89072d8389756f1e7bb1c0679b552849ee9
Joshua Manchester <JManch@protonmail.com>
2024-01-04 02:19:22 +0000

    steam: add features

--- modules/nixos/programs/steam.nix
+++ modules/nixos/programs/steam.nix
@@ -1,9 +1,34 @@
 { username
 , config
+, pkgs
 , lib
 , ...
 }:
 lib.mkIf (config.modules.programs.steam.enable) {
+
+  environment.systemPackages = [
+    pkgs.libnotify
+    pkgs.mangohud
+    pkgs.steam-run
+  ];
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    steam = pkgs.steam.override {
+      extraPkgs = pkgs: with pkgs; [
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXinerama
+        xorg.libXScrnSaver
+        libpng
+        libpulseaudio
+        libvorbis
+        stdenv.cc.cc.lib
+        libkrb5
+        keyutils
+      ];
+    };
+  };
+
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
@@ -11,8 +36,14 @@ lib.mkIf (config.modules.programs.steam.enable) {
 
   programs.gamescope.enable = true;
 
-  hardware = {
-    steam-hardware.enable = true;
+  programs.gamemode = {
+    enable = true;
+    settings = {
+      custom = {
+        start = "${pkgs.libnotify}/bin/notify-send --urgency=critical -t 3000 'GameMode started'";
+        end = "${pkgs.libnotify}/bin/notify-send --urgency=critical -t 3000 'GameMode ended'";
+      };
+    };
   };
 
   environment.persistence."/persist".users.${username} = {

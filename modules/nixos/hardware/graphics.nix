e91e2d8ed75a104086c2bf6fc7d2c2f3a4cc9073
Joshua Manchester <JManch@protonmail.com>
2024-01-04 02:21:27 +0000

    graphics: refactor

--- modules/nixos/hardware/graphics.nix
+++ modules/nixos/hardware/graphics.nix
@@ -1,7 +1,9 @@
 { config
+, outputs
 , pkgs
 , lib
 , username
+, hostname
 , ...
 }:
 let
@@ -9,6 +11,7 @@ let
   nvidia = config.device.gpu == "nvidia";
   amd = config.device.gpu == "amd";
   optional = lib.lists.optional;
+  homeManagerConfig = outputs.nixosConfigurations.${hostname}.config.home-manager.users.${username};
 in
 lib.mkIf (config.device.gpu != null) {
   # AMD Driver Explanation
@@ -35,7 +38,7 @@ lib.mkIf (config.device.gpu != null) {
     # Major issues if this is disabled
     modesetting.enable = true;
     open = true;
-    nvidiaSettings = !(lib.validators.isWayland config);
+    nvidiaSettings = !(lib.fetchers.isWayland homeManagerConfig);
   };
 
   environment.persistence."/persist".users.${username}.directories =

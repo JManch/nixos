1407fe1d7c1f1fe3de65ce998bde113ee335a658
Joshua Manchester <JManch@protonmail.com>
2024-01-04 02:20:54 +0000

    usrEnv: add support for extra desktop managers

--- modules/nixos/options/usrEnv.nix
+++ modules/nixos/options/usrEnv.nix
@@ -9,9 +9,15 @@ in
     homeManager.enable = mkEnableOption "use home manager";
     desktop = {
       enable = mkEnableOption "have a desktop environment";
-      compositor = mkOption {
-        type = with types; nullOr (enum [ "hyprland" ]);
-        description = "The desktop compositor to use";
+      desktopManager = mkOption {
+        type = with types; nullOr (enum [ "xfce" "plasma" ]);
+        default = null;
+        description = "The desktop manager to use";
+      };
+      desktopManagerWindowManager = mkOption {
+        type = types.bool;
+        default = true;
+        description = "Whether to use the desktop manager's built in window manager (if it has one)";
       };
     };
   };

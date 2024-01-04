15fbed8c106b883283edd3bb9a4aae260ee73118
Joshua Manchester <JManch@protonmail.com>
2024-01-04 02:21:17 +0000

    ncase-m1 home: implement new module options

--- home/ncase-m1.nix
+++ home/ncase-m1.nix
@@ -1,5 +1,6 @@
 { pkgs
 , config
+, lib
 , inputs
 , ...
 }: {
@@ -15,13 +16,22 @@
     shell.enable = true;
 
     desktop = {
-      font = {
-        family = "BerkeleyMono Nerd Font";
-        package = inputs.nix-resources.packages.${pkgs.system}.berkeley-mono-nerdfont;
+      windowManager = "hyprland";
+
+      style = {
+        font = {
+          family = "BerkeleyMono Nerd Font";
+          package = inputs.nix-resources.packages.${pkgs.system}.berkeley-mono-nerdfont;
+        };
+        cursorSize = 24;
+        cornerRadius = 10;
+        borderWidth = 2;
+        gapSize = 10;
       };
+
       swaylock = {
         enable = true;
-        lockScript = ''
+        lockScript = lib.mkIf (config.modules.desktop.windowManager == "hyprland") ''
           # Temporarily disable shader for screenshot
           COMMAND="${config.wayland.windowManager.hyprland.package}/bin/hyprctl keyword decoration:screen_shader ${config.xdg.configHome}/hypr/shaders/"
           ''${COMMAND}blank.frag > /dev/null 2>&1
@@ -30,11 +40,13 @@
           ''${COMMAND}monitor1_gamma.frag > /dev/null 2>&1
         '';
       };
+
       swayidle = {
         enable = true;
         lockTime = 3 * 60;
         lockedScreenOffTime = 2 * 60;
       };
+
       anyrun.enable = true;
       waybar.enable = true;
       dunst.enable = true;

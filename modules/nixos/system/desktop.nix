68142f7cc0ce590ca7e255df11a9bed72233f9ed
Joshua Manchester <JManch@protonmail.com>
2024-01-04 02:20:07 +0000

    desktop: move xdg portal to home

--- modules/nixos/system/desktop.nix
+++ modules/nixos/system/desktop.nix
@@ -1,31 +1,37 @@
-{ pkgs
-, lib
+{ lib
 , config
+, outputs
+, hostname
+, username
 , ...
 }:
 let
-  hyprland = (config.usrEnv.desktop.compositor == "hyprland");
-  optional = lib.lists.optional;
+  homeManagerConfig = outputs.nixosConfigurations.${hostname}.config.home-manager.users.${username};
+  desktopCfg = config.usrEnv.desktop;
 in
-lib.mkIf (config.usrEnv.desktop.enable) {
+lib.mkIf config.usrEnv.desktop.enable {
   services.xserver = {
     # Enable regardless of wayland for xwayland support
     enable = true;
-    # Disable default login GUI
-    displayManager.lightdm.enable = false;
-  };
 
-  # INFO: Hopefully this can be moved to home-manager once
-  # https://github.com/nix-community/home-manager/pull/4707 is merged
-  xdg.portal = {
-    enable = true;
-    wlr.enable = lib.validators.isWayland config;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-    ] ++ optional hyprland xdg-desktop-portal-hyprland;
-    configPackages = optional hyprland pkgs.hyprland;
+    displayManager = {
+      defaultSession = lib.mkIf (desktopCfg.desktopManager != null) desktopCfg.desktopManager;
+
+      # Disable default login GUI if we're using wayland
+      lightdm.enable = !lib.fetchers.isWayland homeManagerConfig;
+      sddm.enable = desktopCfg.desktopManager == "plasma";
+    };
+
+    desktopManager = {
+      xfce = {
+        enable = desktopCfg.desktopManager == "xfce";
+        noDesktop = !desktopCfg.desktopManagerWindowManager;
+      };
+      # KDE Plasma5
+      plasma5.enable = desktopCfg.desktopManager == "plasma";
+    };
   };
 
   # Needed for swaylock authentication
-  security.pam.services.swaylock = { };
+  security.pam.services.swaylock = lib.mkIf (homeManagerConfig.modules.desktop.swaylock.enable) { };
 }

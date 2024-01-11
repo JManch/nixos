14a484483f70b46dd43bb41155605a337c5b41a3
Joshua Manchester <JManch@protonmail.com>
2024-01-11 17:48:43 +0000

    mangohud: refine config

--- modules/home-manager/programs/mangohud.nix
+++ modules/home-manager/programs/mangohud.nix
@@ -13,8 +13,6 @@ in
 lib.mkIf gaming.enable {
   programs.mangohud = {
     enable = true;
-    # Launch for all applications
-    enableSessionWide = true;
     settings = {
 
       # Performance
@@ -23,11 +21,10 @@ lib.mkIf gaming.enable {
 
       # UI
       # full = true; # show all available parameters
+      legacy_layout = 0;
       no_display = true; # Whether to hide the HUD by default
       custom_text_center = "${hostname}";
-      # TODO: Figure out how to make this modular
-      font_file = "${config.modules.desktop.style.font.package}/share/fonts/truetype/NerdFonts/BerkeleyMonoNerdFont-Regular.ttf";
-      font_size = 24;
+      font_size = 20;
       round_corners = "${builtins.toString config.modules.desktop.style.cornerRadius}";
       hud_compact = true;
       text_color = "${colors.base07}";
@@ -54,9 +51,8 @@ lib.mkIf gaming.enable {
       gpu_fan = true;
       gpu_voltage = true;
       gpu_load_color = "${colors.base0B},${colors.base0A},${colors.base08}";
-      # TODO: Look into why my card always reports throttle even though it isn't
-      throttling_status = true;
-      throttling_status_graph = true;
+      # Throttling stats are misleading with 7900xt
+      throttling_status = false;
 
       # CPU
       cpu_stats = true;
@@ -76,6 +72,18 @@ lib.mkIf gaming.enable {
       vulkan_driver = true;
       gamemode = true;
       resolution = true;
+
+      # FPS
+      fps = true;
+      fps_color = "${colors.base0B},${colors.base0A},${colors.base08}";
+      frametime = true;
+      frame_timing = true;
+      histogram = true;
+      fps_metrics = "avg,0.01";
+
+      # Gamescope
+      fsr = true;
+      refresh_rate = true;
 
       # Bindings
       toggle_fps_limit = "Shift_L+F1";

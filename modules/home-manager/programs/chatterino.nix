bc1b0a8ab2d504e18f42de2d4c29001f9c1d8189
Joshua Manchester <JManch@protonmail.com>
2024-01-12 17:27:32 +0000

    chatterino: add script for automatically creating twitch hyprland workspace

--- modules/home-manager/programs/chatterino.nix
+++ modules/home-manager/programs/chatterino.nix
@@ -8,7 +8,47 @@ let
   cfg = config.modules.programs.chatterino;
   desktopCfg = config.modules.desktop;
   hyprctl = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl";
-  chatterinoRatio = 1.6;
+  chatterinoRatio = 1.65;
+  twitchWorkspaceScript =
+    let
+      firefox = "${config.programs.firefox.package}/bin/firefox";
+      chatterino = "${pkgs.chatterino2}/bin/chatterino";
+      socat = "${pkgs.socat}/bin/socat";
+    in
+    pkgs.writeShellScript "hypr-twitch-workspace" ''
+      # If a new window is created in the twitch workspace correct the
+      # splitratio and move firefox and MPV windows to the left
+      open_window() {
+        IFS=',' read -r -a args <<< "$1"
+        WINDOW_ADDRESS="''${args[0]#*>>}";
+        WORKSPACE_NAME="''${args[1]}";
+        WINDOW_CLASS="''${args[2]}";
+        if [[ "$WORKSPACE_NAME" == "TWITCH" ]]; then
+          if [[ "$WINDOW_CLASS" == "mpv" || "$WINDOW_CLASS" == "firefox" ]]; then
+            ${hyprctl} --batch "dispatch focuswindow address:0x$WINDOWADDRESS; dispatch movewindow l"
+          fi
+          ${hyprctl} dispatch splitratio exact ${builtins.toString chatterinoRatio};
+        fi
+      }
+
+      # Initialise the twitch workspace with firefox and chatterino
+      create_workspace() {
+        WORKSPACE_NAME="''${1#*>>}"
+        if [[ "$WORKSPACE_NAME" == "TWITCH" ]]; then
+          ${chatterino} > /dev/null 2>&1 &
+          ${firefox} --new-window twitch.tv > /dev/null 2>&1 &
+        fi
+      }
+
+      handle() {
+        case $1 in
+          openwindow*) open_window "$1" ;;
+          createworkspace*) create_workspace "$1" ;;
+        esac
+      }
+
+      ${socat} -U - UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do handle "$line"; done
+    '';
 in
 lib.mkIf cfg.enable {
   home.packages = with pkgs; [
@@ -20,16 +60,17 @@ lib.mkIf cfg.enable {
   ];
 
   desktop.hyprland.settings = lib.mkIf (desktopCfg.windowManager == "hyprland") {
+    exec-once = [
+      "${twitchWorkspaceScript.outPath}"
+    ];
     workspace = [
       "name:TWITCH,monitor:${(lib.fetchers.getMonitorByNumber nixosConfig 2).name},gapsin:0,gapsout:0,rounding:false,border:false}"
     ];
     bind = [
       "${config.modules.desktop.hyprland.modKey}, T, workspace, name:TWITCH"
-      "${config.modules.desktop.hyprland.modKey}SHIFT, T, exec, ${hyprctl} dispatch splitratio exact ${builtins.toString chatterinoRatio}"
     ];
     windowrulev2 = [
-      "workspace name:TWITCH,class:^(com\.chatterino\.)$"
-      "workspace name:TWITCH,class:mpv,title:^(twitch\.tv.*)$"
+      "workspace TWITCH,class:mpv,title:^(twitch\.tv.*)$"
     ];
   };
 }

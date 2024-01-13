2a17500615ea42b496ff3ec18afc33bc1928d5ef
Joshua Manchester <JManch@protonmail.com>
2024-01-13 02:39:32 +0000

    hyprland binds: add volume binds and clean-up

--- modules/home-manager/desktop/wayland/hyprland/binds.nix
+++ modules/home-manager/desktop/wayland/hyprland/binds.nix
@@ -5,16 +5,21 @@
 , ...
 }:
 let
-  hyprshot = "${pkgs.hyprshot}/bin/hyprshot";
-  desktopCfg = config.modules.desktop;
-  osDesktopEnabled = nixosConfig.usrEnv.desktop.enable;
+  # TODO: Need to clean this up by moving script and module specific
+  # functionality into options
+  inherit (lib) optionals optional;
   cfg = config.modules.desktop.hyprland;
-  mod = cfg.modKey;
-  modShift = "${cfg.modKey}SHIFT";
-  modShiftCtrl = "${cfg.modKey}SHIFTCONTROL";
+  desktopCfg = config.modules.desktop;
+
   getMonitorByNumber = number: lib.fetchers.getMonitorByNumber nixosConfig number;
-  hyprctl = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl";
   getOption = option: type: "${hyprctl} getoption ${option} -j | ${pkgs.jaq}/bin/jaq -r '.${type}'";
+
+  audio = nixosConfig.modules.system.audio;
+  osDesktop = nixosConfig.usrEnv.desktop;
+
+  wpctl = "${pkgs.wireplumber}/bin/wpctl";
+  hyprshot = "${pkgs.hyprshot}/bin/hyprshot";
+  hyprctl = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl";
 
   disableShaderCommand =
     let
@@ -32,87 +37,101 @@ let
     fi
   '';
 in
-lib.mkIf (osDesktopEnabled && desktopCfg.windowManager == "hyprland")
+lib.mkIf (osDesktop.enable && desktopCfg.windowManager == "hyprland")
 {
   home.packages = [ pkgs.jaq ];
 
-  wayland.windowManager.hyprland = {
-    settings.bind =
-      [
-        # General
-        "${modShiftCtrl}, Q, exit,"
-        "${mod}, W, killactive,"
-        "${mod}, C, exec, ${toggleFloating.outPath}"
-        "${mod}, E, fullscreen, 1"
-        "${modShift}, E, fullscreen, 0"
-        "${mod}, Z, pin, active"
-        "${modShift}, R, exec, ${hyprctl} dispatch splitratio exact 1"
+  wayland.windowManager.hyprland =
+    let
+      mod = cfg.modKey;
+      modShift = "${cfg.modKey}SHIFT";
+      modShiftCtrl = "${cfg.modKey}SHIFTCONTROL";
+    in
+    {
+      settings.bind =
+        [
+          # General
+          "${modShiftCtrl}, Q, exit,"
+          "${mod}, W, killactive,"
+          "${mod}, C, exec, ${toggleFloating.outPath}"
+          "${mod}, E, fullscreen, 1"
+          "${modShift}, E, fullscreen, 0"
+          "${mod}, Z, pin, active"
+          "${modShift}, R, exec, ${hyprctl} dispatch splitratio exact 1"
 
-        # Movement
-        "${mod}, H, movefocus, l"
-        "${mod}, L, movefocus, r"
-        "${mod}, K, movefocus, u"
-        "${mod}, J, movefocus, d"
-        "${modShift}, H, movewindow, l"
-        "${modShift}, L, movewindow, r"
-        "${modShift}, K, movewindow, u"
-        "${modShift}, J, movewindow, d"
-        "${mod}, mouse:276, workspace, r-1"
-        "${mod}, mouse:275, workspace, r+1"
-        "${mod}, mouse_up, workspace, r+1"
-        "${mod}, mouse_down, workspace, r-1"
-        "${mod}, Left, workspace, r-1"
-        "${mod}, Right, workspace, r+1"
+          # Movement
+          "${mod}, H, movefocus, l"
+          "${mod}, L, movefocus, r"
+          "${mod}, K, movefocus, u"
+          "${mod}, J, movefocus, d"
+          "${modShift}, H, movewindow, l"
+          "${modShift}, L, movewindow, r"
+          "${modShift}, K, movewindow, u"
+          "${modShift}, J, movewindow, d"
+          "${mod}, mouse:276, workspace, r-1"
+          "${mod}, mouse:275, workspace, r+1"
+          "${mod}, mouse_up, workspace, r+1"
+          "${mod}, mouse_down, workspace, r-1"
+          "${mod}, Left, workspace, r-1"
+          "${mod}, Right, workspace, r+1"
 
-        # Monitors
-        "${mod}, Comma, focusmonitor, ${(getMonitorByNumber 2).name}"
-        "${mod}, Period, focusmonitor, ${(getMonitorByNumber 1).name}"
-        "${modShift}, Comma, movecurrentworkspacetomonitor, ${(getMonitorByNumber 2).name}"
-        "${modShift}, Period, movecurrentworkspacetomonitor, ${(getMonitorByNumber 1).name}"
+          # Monitors
+          "${mod}, Comma, movecurrentworkspacetomonitor, ${(getMonitorByNumber 2).name}"
+          "${mod}, Period, movecurrentworkspacetomonitor, ${(getMonitorByNumber 1).name}"
+          "${mod}, TAB, focusmonitor, +1"
 
-        # Dwindle
-        "${mod}, P, pseudo,"
-        "${mod}, X, togglesplit,"
-        "${mod}, M, exec, ${hyprctl} keyword dwindle:no_gaps_when_only $(($(${getOption "dwindle:no_gaps_when_only" "int"}) ^ 1))"
+          # Dwindle
+          "${mod}, P, pseudo,"
+          # TODO: Move this into a script
+          "${mod}, M, exec, ${hyprctl} keyword dwindle:no_gaps_when_only $(($(${getOption "dwindle:no_gaps_when_only" "int"}) ^ 1))"
 
-        # Hyprshot
-        ", Print, exec, ${disableShaderCommand "${hyprshot} -m region --clipboard-only"}"
-        "${mod}, I, exec, ${disableShaderCommand "${hyprshot} -m output -m active --clipboard-only"}"
-        "${modShift}, Print, exec, ${disableShaderCommand "${hyprshot} -m region"}"
-        "${modShift}, I, exec, ${disableShaderCommand "${hyprshot} -m output -m active"}"
+          # Hyprshot
+          ", Print, exec, ${disableShaderCommand "${hyprshot} -m region --clipboard-only"}"
+          "${mod}, I, exec, ${disableShaderCommand "${hyprshot} -m output -m active --clipboard-only"}"
+          "${modShift}, Print, exec, ${disableShaderCommand "${hyprshot} -m region"}"
+          "${modShift}, I, exec, ${disableShaderCommand "${hyprshot} -m output -m active"}"
 
-        # Workspaces other
-        "${mod}, N, workspace, previous"
-        "${mod}, S, togglespecialworkspace,"
-        "${modShift}, S, movetoworkspacesilent, special"
-        "${mod}, G, workspace, name:GAME"
-      ] ++ (
-        # Workspaces
-        let
-          workspaceNumbers = lib.lists.map (w: builtins.toString w) (lib.lists.range 1 9);
-          workspaceBinds = w: [
-            "${mod}, ${w}, workspace, ${w}"
-            "${modShift}, ${w}, movetoworkspace, ${w}"
-            "${modShiftCtrl}, ${w}, movetoworkspacesilent, ${w}"
-          ];
-        in
-        lib.lists.concatMap workspaceBinds workspaceNumbers
-      );
-    settings.bindm = [
-      # Mouse window interaction
-      "${mod}, mouse:272, movewindow"
-      "${mod}, mouse:273, resizewindow"
-    ];
-    # Order-sensitive config has to go here
-    extraConfig = ''
-      bind = ${mod}, R, submap, resize
-      submap = resize
-      binde=, L, resizeactive, 20 0
-      binde=, H, resizeactive, -20 0
-      binde=, K, resizeactive, 0 -20
-      binde=, J, resizeactive, 0 20
-      bind= , Escape, submap, reset
-      submap = reset
-    '';
-  };
+          # Workspaces other
+          "${mod}, N, workspace, previous"
+          "${mod}, S, togglespecialworkspace,"
+          "${modShift}, S, movetoworkspacesilent, special"
+          "${mod}, G, workspace, name:GAME"
+        ] ++ (
+          # Workspaces
+          let
+            workspaceNumbers = lib.lists.map (w: builtins.toString w) (lib.lists.range 1 9);
+            workspaceBinds = w: [
+              "${mod}, ${w}, workspace, ${w}"
+              "${modShift}, ${w}, movetoworkspace, ${w}"
+              "${modShiftCtrl}, ${w}, movetoworkspacesilent, ${w}"
+            ];
+          in
+          lib.lists.concatMap workspaceBinds workspaceNumbers
+        ) ++ (optional audio.enable (
+          ", XF86AudioMute, exec, ${wpctl} set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ));
+      settings.bindm = [
+        # Mouse window interaction
+        "${mod}, mouse:272, movewindow"
+        "${mod}, mouse:273, resizewindow"
+      ];
+      settings.bindr = optionals audio.enable [
+        "${mod}ALT, ALT_L, exec, ${audio.scripts.toggleMic}"
+      ];
+      settings.binde = optionals audio.enable [
+        ", XF86AudioRaiseVolume, exec, ${wpctl} set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, ${wpctl} set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+      ];
+      # Order-sensitive config has to go here
+      extraConfig = ''
+        bind = ${mod}, R, submap, resize
+        submap = resize
+        binde=, L, resizeactive, 20 0
+        binde=, H, resizeactive, -20 0
+        binde=, K, resizeactive, 0 -20
+        binde=, J, resizeactive, 0 20
+        bind= , Escape, submap, reset
+        submap = reset
+      '';
+    };
 }

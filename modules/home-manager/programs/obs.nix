beb7807b87088c70af7eb85314e92310052f876c
Joshua Manchester <JManch@protonmail.com>
2024-01-14 02:09:35 +0000

    obs: use package from master

--- modules/home-manager/programs/obs.nix
+++ modules/home-manager/programs/obs.nix
@@ -1,12 +1,18 @@
-{ config
-, lib
+{ lib
+, pkgs
+, inputs
+, config
 , ...
 }:
 let
   cfg = config.modules.programs.obs;
 in
 lib.mkIf cfg.enable {
-  programs.obs-studio.enable = true;
+  # Need version 30.0.2 otherwise screen cap is broken
+  programs.obs-studio = {
+    enable = true;
+    package = inputs.nixpkgs-master.legacyPackages.${pkgs.system}.obs-studio;
+  };
 
   impermanence.directories = [
     ".config/obs-studio"

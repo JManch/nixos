9df06a5ea1efe11bdc4ce49afdf73548384413de
Joshua Manchester <JManch@protonmail.com>
2024-01-14 02:10:04 +0000

    matlab: remove matlab-shell

--- modules/nixos/programs/matlab.nix
+++ modules/nixos/programs/matlab.nix
@@ -12,12 +12,11 @@ lib.mkIf cfg.enable
 {
   # Follow install instructions here https://gitlab.com/doronbehar/nix-matlab
 
-  # This overlay just adds the matlab packages to the system packages
+  # This overlay just adds the linux-x86_64 matlab packages to pkgs
   nixpkgs.overlays = [ inputs.nix-matlab.overlay ];
 
   environment.systemPackages = with pkgs; [
     matlab
-    matlab-shell
   ];
 
   environment.persistence."/persist".users.${username} = {

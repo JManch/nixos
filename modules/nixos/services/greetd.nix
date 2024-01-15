44d74f3ffab9d5ec35161930bc844143436d3c3d
Joshua Manchester <JManch@protonmail.com>
2024-01-15 23:51:31 +0000

    greetd: fix log printing over login screen

--- modules/nixos/services/greetd.nix
+++ modules/nixos/services/greetd.nix
@@ -19,6 +19,18 @@ lib.mkIf cfg.enable
     };
   };
 
+  # These settings ensure that boot logs won't get spammed over greetd
+  # https://github.com/apognu/tuigreet/issues/68#issuecomment-1586359960
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal";
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
+
   # Enable gnome keyring for saving login credentials in apps such as vscode
   # Works with greetd through pam
   services.gnome.gnome-keyring.enable = true;

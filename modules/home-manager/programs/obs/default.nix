1420ac2923b87ecbde3078db37bf8d0a4a52beb0
Joshua Manchester <JManch@protonmail.com>
2024-01-15 04:00:09 +0000

    obs: restructure to include profiles

--- modules/home-manager/programs/obs/default.nix
+++ modules/home-manager/programs/obs/default.nix
@@ -10,11 +10,8 @@ in
 lib.mkIf cfg.enable {
   # The folders in this directory are OBS profiles which can be
   # imported/exported in OBS
-
-  # Need version 30.0.2 otherwise screen cap is broken
   programs.obs-studio = {
     enable = true;
-    package = inputs.nixpkgs-master.legacyPackages.${pkgs.system}.obs-studio;
   };
 
   impermanence.directories = [

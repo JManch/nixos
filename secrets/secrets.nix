1e90ae6f3d34688d0b59008623d07fabf16721c8
Joshua Manchester <JManch@protonmail.com>
2024-01-16 23:59:03 +0000

    secrets: rekey all

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,14 +1,25 @@
 let
-  joshua = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMd4QvStEANZSnTHRuHg0edyVdRmIYYTcViO9kCyFFt7 JManch@protonmail.com";
+  joshua = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIObU6Fxl5fshbiTZ53wkuvWT3lainInWSdfk/FXQYIxv joshua";
   ncase-m1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGmso00Sb3ab0dURSMJGAYN10OQQQ3VlPxHtNz092a8s root@ncase-m1";
   virtual = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIRMA3ZtwZr/w9QG7iFfWFFSCAIjxw0XSejZPGHdYCzW root@virtual";
-  everyone = [ joshua ncase-m1 virtual ];
+  allHosts = [ ncase-m1 virtual ];
 in
 {
-  "passwds/joshua.age".publicKeys = everyone;
-  "wireless-networks.age".publicKeys = everyone;
-  "syncthing/ncase-m1/cert.age".publicKeys = [ ncase-m1 joshua ];
-  "syncthing/ncase-m1/key.age".publicKeys = [ ncase-m1 joshua ];
-  "nordvpn/token.age".publicKeys = [ ncase-m1 joshua ];
-  "wireguard/ncase-m1/key.age".publicKeys = [ ncase-m1 joshua ];
+  # Editing
+  # `agenix -e [file] -i [PATH_TO_REQUIRED_KEY]`
+
+  # Rekeying
+  # All of the private keys need to be provided when rekeying
+  # `sudo agenix -r -i /etc/ssh/ssh_host_ed25519_key -i /home/joshua/.ssh/joshua_ed25519 ...`
+
+  "passwds/joshua.age".publicKeys = allHosts;
+  "wireless-networks.age".publicKeys = allHosts;
+  "nordvpn/token.age".publicKeys = allHosts;
+
+  "syncthing/ncase-m1/cert.age".publicKeys = [ ncase-m1 ];
+  "syncthing/ncase-m1/key.age".publicKeys = [ ncase-m1 ];
+  "wireguard/ncase-m1/key.age".publicKeys = [ ncase-m1 ];
+
+  # Home-manager secrets can only be decrypted by the "joshua" key
+  "spotify.age".publicKeys = [ joshua ];
 }

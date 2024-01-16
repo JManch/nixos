eb6e77178432f9bf07bd538be04a13598688548e
Joshua Manchester <JManch@protonmail.com>
2024-01-16 01:11:04 +0000

    wireguard: fix interface address subnet

--- modules/nixos/services/wireguard.nix
+++ modules/nixos/services/wireguard.nix
@@ -27,7 +27,14 @@ lib.mkIf cfg.enable
 
   networking.wg-quick.interfaces = {
     wg-discord = {
-      address = [ "10.0.0.2/32" ];
+      # Unlike the allowedIPs setting, the subnet mask here (/24) doesn't
+      # represent a group of 256 IP addresses, it represents the network
+      # mask for the interface. Since the subnet mask is 255.255.255.0, it
+      # tells the interface that other devices on the network will have IP
+      # addresses in that range. It is used for routing to determine if a
+      # destination IP address is on the same network and if it can be directly
+      # communicated with rather than going through the default gateway.
+      address = [ "10.0.0.2/24" ];
       autostart = false;
       privateKeyFile = config.age.secrets.wireguardKey.path;
       listenPort = 13232;

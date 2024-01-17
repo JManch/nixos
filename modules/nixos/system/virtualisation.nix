c47800f5cba15c8bbf1a52f9e1aba239caf16b71
Joshua Manchester <JManch@protonmail.com>
2024-01-17 21:33:05 +0000

    virtualisation: add support for build-vm

--- modules/nixos/system/virtualisation.nix
+++ modules/nixos/system/virtualisation.nix
@@ -5,13 +5,29 @@
 , ...
 }:
 lib.mkIf config.modules.system.virtualisation.enable {
-  virtualisation.libvirtd.enable = true;
+  virtualisation = {
+    libvirtd.enable = true;
+
+    # Only applies when building VM with nixos-rebuild build-vm
+    vmVariant = {
+      virtualisation = {
+        # TODO: Make this modular based on host spec
+        memorySize = 4096; # Use 2048MiB memory.
+        cores = 8;
+      };
+    };
+  };
   programs.virt-manager.enable = true;
   users.users."${username}".extraGroups = [ "libvirtd" ];
 
-  environment.persistence."/persist" = {
-    directories = [
+  environment = {
+    persistence."/persist".directories = [
       "/var/lib/libvirt"
     ];
+    variables = {
+      # Allows nixos-rebuild build-vm graphical session
+      # https://github.com/NixOS/nixpkgs/issues/59219
+      QEMU_OPTS = "-device virtio-vga-gl -display gtk,gl=on";
+    };
   };
 }

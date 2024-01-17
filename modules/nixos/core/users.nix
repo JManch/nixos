25d9736291c80aba81920744dfbb9a84f9f6c3f9
Joshua Manchester <JManch@protonmail.com>
2024-01-17 21:38:34 +0000

    users: use placeholder password for vms

--- modules/nixos/core/users.nix
+++ modules/nixos/core/users.nix
@@ -1,17 +1,22 @@
-{ config
+{ lib
 , pkgs
+, config
 , username
 , ...
 }: {
   age.secrets.joshuaPasswd.file = ../../../secrets/passwds/joshua.age;
-  users.mutableUsers = false;
-  users.users = {
-    ${username} = {
-      isNormalUser = true;
-      shell = pkgs.zsh;
-      hashedPasswordFile = config.age.secrets.joshuaPasswd.path;
-      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMd4QvStEANZSnTHRuHg0edyVdRmIYYTcViO9kCyFFt7 JManch@protonmail.com" ];
-      extraGroups = [ "wheel" ];
+  users = {
+    mutableUsers = false;
+    users = {
+      ${username} = {
+        isNormalUser = true;
+        shell = pkgs.zsh;
+        password = lib.mkIf (config.device.type == "vm") "test";
+        hashedPasswordFile = lib.mkIf (config.device.type != "vm") config.age.secrets.joshuaPasswd.path;
+        # TODO: Change authorized keys to all other hosts
+        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMd4QvStEANZSnTHRuHg0edyVdRmIYYTcViO9kCyFFt7 JManch@protonmail.com" ];
+        extraGroups = [ "wheel" ];
+      };
     };
   };
 }

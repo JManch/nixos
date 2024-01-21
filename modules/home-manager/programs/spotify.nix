d3edec5c24d50f189c6c001bfc84726a04b46391
Joshua Manchester <JManch@protonmail.com>
2024-01-21 20:40:57 +0000

    spotify: customise colors

--- modules/home-manager/programs/spotify.nix
+++ modules/home-manager/programs/spotify.nix
@@ -15,47 +15,81 @@ in
       spotify-player
     ];
 
-    # TODO: Make the colors nicer
-    xdg.configFile."spotify-player/app.toml".text = /* toml */ ''
-      theme = "default"
-      playback_format = """
-      {track}
-      {artists} • {album}
-      {metadata}"""
-      app_refresh_duration_in_ms = 32
-      playback_refresh_duration_in_ms = 0
-      page_size_in_rows = 20
-      play_icon = ""
-      pause_icon = ""
-      liked_icon = ""
-      border_type = "Rounded"
-      progress_bar_type = "Line"
-      playback_window_position = "Bottom"
-      cover_img_length = 12
-      cover_img_width = 6
-      cover_img_scale = 1.0
-      playback_window_width = 6
-      enable_media_control = true
-      enable_streaming = "Always"
-      enable_notify = true
-      enable_cover_image_cache = true
-      default_device = "spotify-player"
+    xdg.configFile = {
+      "spotify-player/app.toml".text = /* toml */ ''
+        theme = "default2"
+        playback_format = """
+        {track}
+        {artists} • {album}
+        {metadata}"""
+        app_refresh_duration_in_ms = 32
+        playback_refresh_duration_in_ms = 0
+        page_size_in_rows = 20
+        play_icon = ""
+        pause_icon = ""
+        liked_icon = ""
+        border_type = "Rounded"
+        progress_bar_type = "Line"
+        playback_window_position = "Bottom"
+        cover_img_length = 12
+        cover_img_width = 6
+        cover_img_scale = 1.0
+        playback_window_width = 6
+        enable_media_control = true
+        enable_streaming = "Always"
+        enable_notify = true
+        enable_cover_image_cache = true
+        default_device = "spotify-player"
 
-      [copy_command]
-      command = "${pkgs.wl-clipboard}/bin/wl-copy"
+        [copy_command]
+        command = "${pkgs.wl-clipboard}/bin/wl-copy"
 
-      [notify_format]
-      summary = "{track}"
-      body = "{artists} • {album}"
+        [notify_format]
+        summary = "{track}"
+        body = "{artists} • {album}"
 
-      [device]
-      name = "spotify-player"
-      device_type = "computer"
-      volume = 70
-      bitrate = 320
-      audio_cache = true
-      normalization = false
-    '';
+        [device]
+        name = "spotify-player"
+        device_type = "computer"
+        volume = 70
+        bitrate = 320
+        audio_cache = true
+        normalization = false
+      '';
+      "spotify-player/theme.toml".text = /* toml */ ''
+        [[themes]]
+        name = "default2"
+        [themes.palette]
+        black = "black"
+        red = "red"
+        green = "green"
+        yellow = "yellow"
+        blue = "blue"
+        magenta = "magenta"
+        cyan = "cyan"
+        white = "white"
+        bright_black = "bright_black"
+        bright_red = "bright_red"
+        bright_green = "bright_green"
+        bright_yellow = "bright_yellow"
+        bright_blue = "bright_blue"
+        bright_magenta = "bright_magenta"
+        bright_cyan = "bright_cyan"
+        bright_white = "bright_white"
+        [themes.component_style]
+        block_title = { fg = "Green", modifiers = ["Bold"] }
+        border = { fg = "BrightBlack" }
+        playback_track = { fg = "White", modifiers = ["Bold"] }
+        playback_artists = { fg = "White" }
+        playback_album = { fg = "White", modifiers = ["Italic"] }
+        playback_metadata = { fg = "BrightBlack" }
+        playback_progress_bar = { bg = "BrightBlack", fg = "Green" }
+        current_playing = { fg = "Green", modifiers = ["Bold"] }
+        page_desc = { fg = "Blue", modifiers = ["Bold"] }
+        table_header = { fg = "Blue" }
+        selection = { modifiers = ["Bold", "Reversed"] }
+      '';
+    };
 
     xdg.desktopEntries."spotify-player" = {
       name = "Spotify";

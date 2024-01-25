0f0b8215aec39129fa2c4f651b70068f69eb009a
Joshua Manchester <JManch@protonmail.com>
2024-01-25 22:18:07 +0000

    windows: add ntfs filesystem support

--- modules/nixos/system/windows.nix
+++ modules/nixos/system/windows.nix
@@ -7,6 +7,10 @@ let
   cfg = config.modules.system.windowsBootEntry;
 in
 lib.mkIf cfg.enable {
+
+  # Enable mounting windows ntfs drive
+  environment.systemPackages = [ pkgs.ntfs3g ];
+
   # https://forum.endeavouros.com/t/tutorial-add-a-systemd-boot-loader-menu-entry-for-a-windows-installation-using-a-separate-esp-partition/37431
   # mirror: https://archive.is/wwZaP
   # NOTE: This requires setting up /windows.nsh in the ESP partition

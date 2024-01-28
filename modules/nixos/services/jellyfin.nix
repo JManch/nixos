087704531600adb3b52dbfe213003691e27e1410
Joshua Manchester <JManch@protonmail.com>
2024-01-28 04:08:14 +0000

    jellyfin: configure media bind mounts

--- modules/nixos/services/jellyfin.nix
+++ modules/nixos/services/jellyfin.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }:
+{ lib, config, username, ... }:
 let
   cfg = config.modules.services.jellyfin;
 in
@@ -9,5 +9,23 @@ lib.mkIf cfg.enable
     openFirewall = true;
   };
 
-  systemd.services.jellyfin.wantedBy = lib.lists.optional cfg.autostart [ "multi-user.target" ];
+  systemd.services.jellyfin = {
+    wantedBy = lib.mkForce (lib.lists.optional cfg.autostart [ "multi-user.target" ]);
+    # Bind mount home media directories so jellyfin can access them
+    serviceConfig = {
+      BindReadOnlyPaths = [
+        "/home/${username}/videos/shows:/var/lib/jellyfin/media/shows"
+        "/home/${username}/videos/movies:/var/lib/jellyfin/media/movies"
+      ];
+    };
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/jellyfin/media/shows 755 root root"
+    "d /var/lib/jellyfin/media/movies 755 root root"
+  ];
+
+  environment.persistence."/persist".directories = [
+    "/var/lib/jellyfin"
+  ];
 }

e577a160edb9f078b78b3f0232000be757be80e6
Joshua Manchester <JManch@protonmail.com>
2024-02-03 19:20:12 +0000

    desktop: add hyprland note

--- modules/nixos/system/desktop.nix
+++ modules/nixos/system/desktop.nix
@@ -18,7 +18,6 @@ in
   ];
 
   # TODO: Improve the isWayland function to take desktopEnvironment into account
-  # TODO: 
   config = mkIf config.usrEnv.desktop.enable (mkMerge [
     {
       services.xserver.layout = "us";
@@ -66,6 +65,17 @@ in
     })
 
     (mkIf (windowManager == "hyprland") {
+      # The purpose of enabling hyprland here (in addition to enabling it in
+      # home-manager) is to create the hyprland.desktop session file which
+      # enables login GUI managers to launch hyprland. However we use greetd
+      # which is unique in the sense that it doesn't use session files. Instead
+      # it uses a manually-configured launch command. Other login managers
+      # would let the user pick a desktop session from a list of options
+      # (generated from the .desktop session files).
+      #
+      # Ultimately this means that enabling hyprland on the system-level is
+      # unnecessary if we're using greetd. Regardless, we enable it for
+      # compatibility with other login managers.
       programs.hyprland.enable = true;
     })
 

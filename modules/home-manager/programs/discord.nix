15a055aeb0b54c7325ca2a66e5659dd86b7da4d7
Joshua Manchester <JManch@protonmail.com>
2024-02-04 13:40:59 +0000

    discord: move window to workspace 4 on launch

--- modules/home-manager/programs/discord.nix
+++ modules/home-manager/programs/discord.nix
@@ -1,17 +1,20 @@
-{ config
-, inputs
-, pkgs
-, lib
-, ...
-}:
+{ lib, pkgs, config, ... }:
 let
   cfg = config.modules.programs.discord;
+  desktopCfg = config.modules.desktop;
 in
 lib.mkIf cfg.enable {
   home.packages = with pkgs; [
     discord
     vesktop
   ];
+
+  desktop.hyprland.settings = lib.mkIf (desktopCfg.windowManager == "hyprland") {
+    windowrulev2 = [
+      "noinitialfocus,class:^(vesktop|discord)$"
+      "workspace 4 silent,class:^(vesktop|discord)$"
+    ];
+  };
 
   impermanence.directories = [
     ".config/discord"

a7811498a33fd6ae4d02aedc93b3e2cc5296e1a6
Joshua Manchester <JManch@protonmail.com>
2024-02-09 06:08:19 +0000

    impermanence: persist /var/lib/private

--- modules/nixos/system/impermanence.nix
+++ modules/nixos/system/impermanence.nix
@@ -40,6 +40,14 @@ in
       "/var/lib/systemd"
       "/var/lib/nixos"
       "/var/db/sudo/lectured"
+      # Unfortunately it isn't possible to persist individual state folders for
+      # services using DynamicUsers=yes. This is because systemd assigns
+      # dynamic UIDs to users of this service so it's impossible to set the
+      # required permissions with impermanence. Services place this dynamic
+      # user folder in /var/lib/private/<service>. I will add commented out
+      # persistence definitions in the relevant services so their files are
+      # still documented.
+      { directory = "/var/lib/private"; mode = "0700"; }
     ];
     files = [
       "/etc/machine-id"

05bc3e3ba60aeaa0579284122c167953d41e5168
Joshua Manchester <JManch@protonmail.com>
2024-02-19 02:52:19 +0000

    swayidle: remove module

--- modules/home-manager/desktop/wayland/services/default.nix
+++ modules/home-manager/desktop/wayland/services/default.nix
@@ -9,22 +9,6 @@ in
     waybar.enable = mkEnableOption "Waybar";
     wlsunset.enable = mkEnableOption "wlsunset";
 
-    swayidle = {
-      enable = mkEnableOption "Swayidle";
-
-      lockTime = mkOption {
-        type = types.int;
-        default = 3 * 60;
-        description = "Idle seconds to lock screen";
-      };
-
-      screenOffTime = mkOption {
-        type = types.int;
-        default = 4 * 60;
-        description = "Idle seconds to turn off screen";
-      };
-    };
-
     hypridle = {
       enable = mkEnableOption "Hypridle";
 

96a848795276317c83545e715fde58ac01473818
Joshua Manchester <JManch@protonmail.com>
2024-02-22 01:38:11 +0000

    neovim: use extraPackages for runtime deps

--- modules/home-manager/programs/neovim.nix
+++ modules/home-manager/programs/neovim.nix
@@ -5,40 +5,64 @@
 , ...
 } @ args:
 let
-  inherit (lib) mkIf utils;
+  inherit (lib) mkIf utils optionalString;
   cfg = config.modules.programs.neovim;
 in
 mkIf (cfg.enable && config.modules.shell.enable) {
-  home.packages = with pkgs; [
-    neovide
-    # Language servers
-    lua-language-server
-    nil
-    nixd
-    # Formatters
-    stylua
-    nixpkgs-fmt
-  ];
-
-  programs.ripgrep.enable = true;
-  programs.fzf.enable = true;
+  home.packages = [ pkgs.neovide ];
 
   programs.neovim = {
     enable = true;
     package = (utils.flakePkgs args "neovim-nightly-overlay").default;
+    viAlias = true;
     vimAlias = true;
+    vimdiffAlias = true;
+    defaultEditor = true;
+    extraPackages = with pkgs; [
+      # Runtime dependendies
+      fzf
+      ripgrep
+      gnumake
+      gcc
+
+      # Language servers
+      lua-language-server
+      nil
+      nixd
+      clang-tools
+      ltex-ls
+      omnisharp-roslyn
+      matlab-language-server
+
+      # Formatters
+      stylua
+      prettierd
+      black
+      nixpkgs-fmt
+    ];
   };
 
-  home.sessionVariables = {
-    EDITOR = "nvim";
-    NIX_NEOVIM = 1;
-  };
+  # For conditional nix-specific config in nvim config
+  home.sessionVariables.NIX_NEOVIM = 1;
 
   xdg.mimeApps = mkIf osConfig.usrEnv.desktop.enable {
     defaultApplications = {
       "text/plain" = [ "nvim.desktop" ];
     };
   };
+
+  programs.zsh.initExtra =
+    let
+      inherit (config.modules.programs) alacritty;
+      inherit (config.programs.alacritty.settings.window) opacity;
+    in
+    optionalString alacritty.enable /*bash*/ ''
+      nvim() {
+        alacritty msg config window.opacity=1 && \
+          command nvim "$@" && alacritty msg config window.opacity=${toString opacity}
+      }
+    ''
+  ;
 
   persistence.directories = [
     ".cache/nvim"

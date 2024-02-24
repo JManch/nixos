34df46bf2a3420f1961d1b32ccd548fe6f86b635
Joshua Manchester <JManch@protonmail.com>
2024-02-24 11:11:49 +0000

    homelab: setup host

---
+++ home/homelab.nix
@@ -0,0 +1,18 @@
+{
+  imports = [ ./core.nix ];
+
+  modules = {
+    shell.enable = true;
+
+    programs = {
+      btop.enable = true;
+      git.enable = true;
+      neovim.enable = true;
+      fastfetch.enable = true;
+    };
+
+    services = {
+      syncthing.enable = true;
+    };
+  };
+}

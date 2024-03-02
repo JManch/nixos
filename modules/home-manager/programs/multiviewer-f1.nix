4608195563f3e7dfdad3f8e84a9c6a153fcdef6a
Joshua Manchester <JManch@protonmail.com>
2024-03-02 13:38:07 +0000

    multiviewer: add automatic hyprland layout script

--- modules/home-manager/programs/multiviewer-f1.nix
+++ modules/home-manager/programs/multiviewer-f1.nix
@@ -1,31 +1,221 @@
-{ lib, pkgs, config, ... }:
+{ lib
+, pkgs
+, config
+, osConfig
+, ...
+}:
 let
+  inherit (lib) mkIf take fetchers getExe genAttrs listToAttrs;
+  inherit (osConfig.device) monitors;
   cfg = config.modules.programs.multiviewerF1;
+  desktopCfg = config.modules.desktop;
+
+  multiviewer-for-f1 =
+    pkgs.multiviewer-for-f1.overrideAttrs (_: rec {
+      version = "1.31.4";
+      src = pkgs.fetchurl {
+        url = "https://releases.multiviewer.app/download/154454223/multiviewer-for-f1_${version}_amd64.deb";
+        sha256 = "sha256-HlKs4GMyHLLfQUGSsX6O8ImJ9GCoSc9KAHEuDESohvw=";
+      };
+      installPhase = ''
+        runHook preInstall
+
+        mkdir -p $out/bin $out/share
+        mv -t $out/share usr/share/* usr/lib/multiviewer-for-f1
+
+        makeWrapper "$out/share/multiviewer-for-f1/MultiViewer for F1" $out/bin/multiviewer-for-f1 \
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland --enable-features=WaylandWindowDecorations}}" \
+        --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [ pkgs.libudev0-shim pkgs.libglvnd ]}:\"$out/share/Multiviewer for F1\""
+
+        runHook postInstall
+      '';
+    });
+
+  # This script acts as a replacement for the Multiviewer layout saving which
+  # doesn't work with a tiling window manager. The idea is that Multiviewer
+  # layouts are still used to load all the windows. This windows get stacked in
+  # a pile on the F1 workspace. Then I press a keybind to activate this script
+  # which positions all the windows where I want across two monitors.
+  multiviewerWorkspaceScript =
+    let
+      inherit (lib) remove head last optionalString;
+      monitor1 = fetchers.getMonitorByNumber osConfig 1;
+      monitor2 = fetchers.getMonitorByNumber osConfig 2;
+      monitor1Positions = remove [ ] (builtins.split "x" monitor1.position);
+      monitor2Positions = remove [ ] (builtins.split "x" monitor2.position);
+    in
+    pkgs.writeShellApplication {
+      name = "hypr-multiviewer-workspace";
+
+      runtimeInputs = with pkgs; [
+        config.wayland.windowManager.hyprland.package
+        jaq
+      ];
+
+      # Only generate script if two monitors exist
+      text = optionalString (monitor1.number != monitor2.number) /*bash*/ ''
+
+        m1_pos_x=${head monitor1Positions}
+        m1_pos_y=${last monitor1Positions}
+        m1_res_x=${toString monitor1.width}
+        m1_res_y=${toString monitor1.height}
+        m2_pos_x=${head monitor2Positions}
+        m2_pos_y=${last monitor2Positions}
+        m2_res_x=${toString monitor2.width}
+        m2_res_y=${toString monitor2.height}
+
+        # Get window address and title of all F1 windows
+        windows=$(hyprctl clients -j | jaq -r '((.[] | select((.workspace.name | test("F1-(\\d+)")) and (.class == "MultiViewer for F1") )) | "\(.address),\(.title)")')
+
+        hyprctl_cmd="hyprctl --batch \""
+        declare -a drivers
+        declare -A driver_prio
+
+        driver_prio["Esteban Ocon"]=20
+        driver_prio["Pierre Gasly"]=20
+        driver_prio["Fernando Alonso"]=2
+        driver_prio["Lance Stroll"]=20
+        driver_prio["Carlos Sainz"]=4
+        driver_prio["Charles Leclerc"]=3
+        driver_prio["Kevin Magnussen"]=20
+        driver_prio["Nico Hulkenberg"]=20
+        driver_prio["Guanyu Zhou"]=20
+        driver_prio["Valtteri Bottas"]=20
+        driver_prio["Lando Norris"]=6
+        driver_prio["Oscar Piastri"]=5
+        driver_prio["George Russell"]=20
+        driver_prio["Lewis Hamilton"]=20
+        driver_prio["Daniel Ricciardo"]=20
+        driver_prio["Yuki Tsunoda"]=20
+        driver_prio["Max Verstappen"]=1
+        driver_prio["Sergio Perez"]=20
+        driver_prio["Alexander Albon"]=20
+        driver_prio["Logan Sergeant"]=20
+
+        while IFS=, read -r address title; do
+          case $title in
+            "Live Timing"*|"Replay Live Timing"*)
+              echo "Matched live timing"
+              res_x=$((m1_res_x * 1 / 4))
+              res_y=$((m1_res_y * 3 / 4))
+              hyprctl_cmd+="dispatch movetoworkspacesilent name:F1-1,address:$address;"
+              hyprctl_cmd+="dispatch resizewindowpixel exact $res_x $res_y,address:$address;"
+              hyprctl_cmd+="dispatch movewindowpixel exact $m1_pos_x $m1_pos_y,address:$address;"
+              ;;
+            "F1 Live"*)
+              res_x=$((m1_res_x * 3 / 4))
+              res_y=$((m1_res_y * 3 / 4))
+              hyprctl_cmd+="dispatch movetoworkspacesilent name:F1-1,address:$address;"
+              hyprctl_cmd+="dispatch resizewindowpixel exact $res_x $res_y,address:$address;"
+              hyprctl_cmd+="dispatch movewindowpixel exact $((m1_pos_x + (m1_res_x - res_x))) $m1_pos_y,address:$address;"
+              ;;
+            "Track Map"*)
+              res_x="480"
+              res_y="300"
+              hyprctl_cmd+="dispatch movetoworkspacesilent name:F1-1,address:$address;"
+              hyprctl_cmd+="dispatch resizewindowpixel exact $res_x $res_y,address:$address;"
+              hyprctl_cmd+="dispatch movewindowpixel exact $((m1_pos_x + m1_res_x - res_x)) $((m1_pos_y + (m1_res_y * 3 / 4) - (res_y + 60))),address:$address;"
+              hyprctl_cmd+="dispatch alterzorder top,address:$address;"
+              ;;
+            "Radio Transcriptions"*|"Race Control"*)
+              res_x=$((m1_res_x * 1 / 4))
+              res_y=$((m1_res_y * 1 / 4))
+              hyprctl_cmd+="dispatch movetoworkspacesilent name:F1-1,address:$address;"
+              hyprctl_cmd+="dispatch resizewindowpixel exact $res_x $res_y,address:$address;"
+              hyprctl_cmd+="dispatch movewindowpixel exact $m1_pos_x $((m1_pos_y + (m1_res_y * 3 / 4))),address:$address;"
+              ;;
+            *)
+              # Assume it's a driver cam
+              regex="^([^—]+)"
+              if [[ $title =~ $regex ]]; then
+                driver=''${BASH_REMATCH[1]}
+                driver="''${driver%?}"
+                drivers+=("''${driver_prio["$driver"]},$address")
+              fi
+              ;;
+          esac
+        done <<< "$windows"
+
+        # Sort the driver windows according to priority
+        sorted_drivers=$(printf "%s\n" "''${drivers[@]}" | sort -n -t ',' -k 1)
+
+        # Iterate through sorted driver windows and place in a grid-style layout
+        counter=0
+        while IFS=, read -r _ address; do
+          res_x=$((m2_res_x * 1 / 4))
+          res_y=$((m2_res_y * 1 / 4))
+
+          # First 3 drivers go on primary monitor and rest go on secondary
+          if [[ "$counter" -lt 3 ]]; then
+            hyprctl_cmd+="dispatch movetoworkspacesilent name:F1-1,address:$address;"
+            hyprctl_cmd+="dispatch resizewindowpixel exact $res_x $res_y,address:$address;"
+            hyprctl_cmd+="dispatch movewindowpixel exact $((m1_pos_x + (counter + 1) * res_x)) $((m1_pos_y + (m1_res_y * 3 / 4))),address:$address;"
+          else
+            column=$(((counter-3) / 4))
+            row=$(((counter-3) % 4))
+            pos_x=$((m2_pos_x + (3 - column) * res_x))
+            pos_y=$((m2_pos_y + row * res_y))
+            hyprctl_cmd+="dispatch movetoworkspacesilent name:F1-2,address:$address;"
+            hyprctl_cmd+="dispatch resizewindowpixel exact $res_x $res_y,address:$address;"
+            hyprctl_cmd+="dispatch movewindowpixel exact $pos_x $pos_y,address:$address;"
+          fi
+          ((++counter))
+        done <<< "$sorted_drivers"
+
+        hyprctl_cmd+="\""
+        eval "$hyprctl_cmd"
+
+      '';
+    };
 in
 lib.mkIf cfg.enable
 {
-  home.packages = [
-    (pkgs.multiviewer-for-f1.overrideAttrs
-      (_: {
-        version = "1.31.0";
-        src = pkgs.fetchurl {
-          url = "https://releases.multiviewer.app/download/152564927/multiviewer-for-f1_1.31.0_amd64.deb";
-          sha256 = "sha256-M4ILpP4voOPDG6MqgVuEF1HiJPAKQ5nFwMEz2PXiDMY=";
-        };
-        installPhase = ''
-          runHook preInstall
+  home.packages = [ multiviewer-for-f1 ];
 
-          mkdir -p $out/bin $out/share
-          mv -t $out/share usr/share/* usr/lib/multiviewer-for-f1
+  programs.waybar.settings.bar."hyprland/workspaces".format-icons = mkIf (desktopCfg.windowManager == "Hyprland")
+    (listToAttrs
+      (map (m: { name = "F1-${toString m.number}"; value = "F1"; })
+        (take 2 monitors)
+      )
+    );
 
-          makeWrapper "$out/share/multiviewer-for-f1/MultiViewer for F1" $out/bin/multiviewer-for-f1 \
-          --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland --enable-features=WaylandWindowDecorations}}" \
-          --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [ pkgs.libudev0-shim pkgs.libglvnd ]}:\"$out/share/Multiviewer for F1\""
+  desktop.hyprland.settings =
+    let
+      inherit (config.modules.desktop.hyprland) modKey;
+      inherit (desktopCfg.style) gapSize;
+    in
+    {
+      workspace = (map
+        (
+          # FIX: These workspace rules don't always apply to multiviewer
+          # windows for some reason. Might be a hyprland bug.
+          m: "name:F1-${toString m.number}, monitor:${m.name}, gapsin:0, gapsout:0, decorate:false, rounding:false, border:false"
+        )
+        (take 2 monitors))
+      ++ [
+        # The 'main' multiviewer windows goes into a special workspace
+        "special:multiviewer, on-created-empty:${getExe multiviewer-for-f1}, gapsin:${toString (gapSize * 2)}, gapsout:${toString (gapSize * 4)}"
+      ];
 
-          runHook postInstall
-        '';
-      }))
-  ];
+      bind = (
+        map (m: "${modKey}SHIFT, F, workspace, name:F1-${toString m.number}")
+          (take 2 monitors)
+      )
+      ++ [
+        "${modKey}, F, togglespecialworkspace, multiviewer"
+        "${modKey}SHIFTCONTROL, F, exec, ${getExe multiviewerWorkspaceScript}"
+      ];
+
+      windowrulev2 =
+        [
+          "workspace name:F1-1, class:^(MultiViewer for F1)$"
+          "noinitialfocus, class:^(MultiViewer for F1)$"
+          "float, class:^(MultiViewer for F1)$"
+          "xray 0, class:^(MultiViewer for F1)$"
+          # FIX: This noblur rule doesn't work for some reason, hyprland bug?
+          "noblur, class:^(MultiViewer for F1)$"
+        ];
+    };
 
   persistence.directories = [
     ".config/MultiViewer for F1"

8e754b54d57cbce37a0da1cf8464926d11ac44ca
Joshua Manchester <JManch@protonmail.com>
2024-03-02 13:23:35 +0000

    vscode: fix c++ debugging

--- modules/home-manager/programs/vscode.nix
+++ modules/home-manager/programs/vscode.nix
@@ -10,21 +10,24 @@ mkIf cfg.enable {
 
   programs.vscode = {
     enable = true;
-    mutableExtensionsDir = false;
-    enableExtensionUpdateCheck = false;
-    enableUpdateCheck = false;
-
-    extensions = with pkgs.vscode-extensions; [
-      ms-vsliveshare.vsliveshare
-      bbenoist.nix
-      gruntfuggly.todo-tree
-      ms-vscode.cpptools
-      ms-vscode.cmake-tools
-    ];
+    # WARN: This is the only way I'm able to get C++ debugging to work. Because
+    # vscode runs in a FHS, it won't inherit PATH from nix develop or nix shell
+    # environments. Solution is to run nix shell inside the vscode terminal.
+    # Also, some tooling such as LSP servers may need to be added here. I don't
+    # use VSCode as my main editor though so not a big deal.
+    package = pkgs.vscode.fhsWithPackages (ps: with ps; ([
+      gdb
+    ]));
 
     userSettings = mkIf (fetchers.isWayland config) {
       # Prevents crash on launch
       "window.titleBarStyle" = "custom";
+      "window.menuBarVisibility" = "toggle";
+      "editor.fontFamily" = "BerkeleyMono Nerd Font";
+      "cmake.configureOnOpen" = false;
+      "git.autofetch" = true;
+      "workbench.colorTheme" = "Ayu Mirage Bordered";
+      "cmake.showOptionsMovedNotification" = false;
     };
   };
 

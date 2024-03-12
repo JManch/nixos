de416f1ca4373baceedc3966b64c662988ea9c4a
Joshua Manchester <JManch@protonmail.com>
2024-03-12 16:37:12 +0000

    wgnord: do not route private ips through vpn

--- modules/nixos/services/wgnord.nix
+++ modules/nixos/services/wgnord.nix
@@ -8,6 +8,20 @@ lib.mkIf cfg.enable
     enable = true;
     tokenFile = config.age.secrets.nordToken.path;
     country = cfg.country;
+    # Allow all IPs apart from 192.168.0.0/16 and 10.0.0.0/8
+    template = ''
+      [Interface]
+      PrivateKey = PRIVKEY
+      Address = 10.5.0.2/32
+      MTU = 1350
+      DNS = 103.86.96.100 103.86.99.100
+
+      [Peer]
+      PublicKey = SERVER_PUBKEY
+      AllowedIPs = 0.0.0.0/5, 8.0.0.0/7, 11.0.0.0/8, 12.0.0.0/6, 16.0.0.0/4, 32.0.0.0/3, 64.0.0.0/2, 128.0.0.0/2, 192.0.0.0/9, 192.128.0.0/11, 192.160.0.0/13, 192.169.0.0/16, 192.170.0.0/15, 192.172.0.0/14, 192.176.0.0/12, 192.192.0.0/10, 193.0.0.0/8, 194.0.0.0/7, 196.0.0.0/6, 200.0.0.0/5, 208.0.0.0/4, 224.0.0.0/3, ::/0
+      Endpoint = SERVER_IP:51820
+      PersistentKeepalive = 25
+    '';
   };
 
   programs.zsh = {

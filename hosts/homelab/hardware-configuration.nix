c93e33435cf17c9fbafbd6569a42a10ca5ae645e
Joshua Manchester <JManch@protonmail.com>
2024-03-25 18:46:41 +0000

    homelab: add real hardware configuration

--- hosts/homelab/hardware-configuration.nix
+++ hosts/homelab/hardware-configuration.nix
@@ -1,13 +1,14 @@
 { modulesPath, ... }:
 {
-  # TODO: Replace this with actual hardware config on deployment
-  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
   hardware.cpu.amd.updateMicrocode = true;
   nixpkgs.hostPlatform = "x86_64-linux";
 
   boot = {
-    initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
+    initrd.availableKernelModules = [ "xhci_pci" "ahci" "ehci_pci" "nvme" ];
     kernelModules = [ "kvm-amd" ];
     # TODO: This is needed to mount in VM, check if it's needed for bare metal
     zfs.devNodes = "/dev/disk/by-partuuid";

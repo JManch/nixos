b11e1955c5661823db7c70e9c4e25944c30af383
Joshua Manchester <JManch@protonmail.com>
2024-03-25 18:45:39 +0000

    mosquitto: clean-up

--- modules/nixos/services/mosquitto.nix
+++ modules/nixos/services/mosquitto.nix
@@ -2,31 +2,29 @@
 let
   inherit (lib) mkIf;
   inherit (config.age) secrets;
-  inherit (config.modules.services) home-assistant frigate;
+  inherit (config.modules.services) hass frigate;
   cfg = config.modules.services.mosquitto;
 in
 mkIf cfg.enable
 {
   services.mosquitto = {
     enable = true;
-    listeners = [
-      {
-        address = "127.0.0.1";
-        port = 1883;
+    listeners = [{
+      address = "127.0.0.1";
+      port = cfg.port;
 
-        users = {
-          frigate = mkIf frigate.enable {
-            acl = [ "readwrite #" ];
-            hashedPasswordFile = secrets.mqttFrigatePassword.path;
-          };
-
-          hass = mkIf home-assistant.enable {
-            acl = [ "readwrite #" ];
-            hashedPasswordFile = secrets.mqttHassPassword.path;
-          };
+      users = {
+        frigate = mkIf frigate.enable {
+          acl = [ "readwrite #" ];
+          hashedPasswordFile = secrets.mqttFrigatePassword.path;
         };
-      }
-    ];
+
+        hass = mkIf hass.enable {
+          acl = [ "readwrite #" ];
+          hashedPasswordFile = secrets.mqttHassPassword.path;
+        };
+      };
+    }];
   };
 
   persistence.directories = [{

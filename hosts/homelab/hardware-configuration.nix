c3d855f0d3714d918eae8f16d617212a481edb35
Joshua Manchester <JManch@protonmail.com>
2024-03-27 18:21:06 +0000

    homelab hardware: add new ethernet fix attempt

--- hosts/homelab/hardware-configuration.nix
+++ hosts/homelab/hardware-configuration.nix
@@ -11,6 +11,10 @@
     initrd.availableKernelModules = [ "xhci_pci" "ahci" "ehci_pci" "nvme" ];
     kernelModules = [ "kvm-amd" ];
     # Attempt to fix intermittent ethernet failure
-    kernelParams = [ "pcie_aspm=off" ];
+    # (issue still occured with pcie_asmp=off, testing now with extra 3)
+    # Disabling acpi prevents "software" shutdowns of the system so I will have
+    # to physically press the power button
+    # Using acpi=off breaks cpu detection so only 1 core shows
+    kernelParams = [ "pcie_aspm=off" "noapic" "pci=noacpi" ];
   };
 }

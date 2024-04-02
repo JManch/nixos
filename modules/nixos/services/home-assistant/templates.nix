e5b6b2053d92d73b981198b04c668287fa01c40c
Joshua Manchester <JManch@protonmail.com>
2024-04-02 14:39:16 +0100

    home assistant: configure default template states

--- modules/nixos/services/home-assistant/templates.nix
+++ modules/nixos/services/home-assistant/templates.nix
@@ -14,23 +14,23 @@ lib.mkIf (cfg.enableInternal)
         {
           name = "Powerwall Grid Charge Status";
           icon = "mdi:home-export-outline";
-          state = "{{ (states('sensor.powerwall_site_power') | float) < -0.05 }}";
+          state = "{{ ((states('sensor.powerwall_site_power') | default(0)) | float) < -0.1 }}";
           device_class = "battery_charging";
         }
         {
           name = "Powerwall Battery Charge Status";
           icon = "mdi:battery-charging";
-          state = "{{ (states('sensor.powerwall_battery_power') | float) < -0.05 }}";
+          state = "{{ ((states('sensor.powerwall_battery_power') | default(0)) | float) < -0.1 }}";
           device_class = "battery_charging";
         }
         {
           name = "Washing Machine Running";
-          state = "{{ states('sensor.washing_machine_status') == 'Running' }}";
+          state = "{{ (states('sensor.washing_machine_status') | default('NA')) == 'Running' }}";
           device_class = "running";
         }
         {
           name = "Dishwasher Running";
-          state = "{{ states('sensor.dishwasher_status') == 'Running' }}";
+          state = "{{ (states('sensor.dishwasher_status') | default('NA')) == 'Running' }}";
           device_class = "running";
         }
       ];
@@ -40,14 +40,14 @@ lib.mkIf (cfg.enableInternal)
           name = "Powerwall Battery Remaining Time";
           icon = "mdi:battery-clock-outline";
           state = ''
-            {% set power = (states('sensor.powerwall_battery_power')|float) %}
-            {% set remaining = (states('sensor.powerwall_gateway_battery_remaining')|float) %}
-            {% set capacity = (states('sensor.powerwall_gateway_battery_capacity')|float) %}
+            {% set power = (states('sensor.powerwall_battery_power') | float) %}
+            {% set remaining = (states('sensor.powerwall_gateway_battery_remaining') | float) %}
+            {% set capacity = (states('sensor.powerwall_gateway_battery_capacity') | float) %}
 
-            {% if power > 0.05 %}
+            {% if power > 0.1 %}
               {% set remaining_mins = ((remaining / power) * 60) | round(0) %}
               {% set message = "until empty" %}
-            {% elif power < -0.05 %}
+            {% elif power < -0.1 %}
               {% set remaining_mins = (((capacity - remaining) / (power * -1)) * 60) | round(0) %}
               {% set message = "until fully charged" %}
             {% endif %}

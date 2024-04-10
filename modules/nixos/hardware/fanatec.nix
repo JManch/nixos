8805e584392af2c92aaf68b2497238bc9b1fcd52
Joshua Manchester <JManch@protonmail.com>
2024-04-11 00:39:47 +0100

    fanatec: add profile load script

--- modules/nixos/hardware/fanatec.nix
+++ modules/nixos/hardware/fanatec.nix
@@ -10,4 +10,26 @@ lib.mkIf cfg.enable
       (config.boot.kernelPackages.callPackage "${inputs.nix-resources}/pkgs/hid-fanatecff.nix" { })
     ];
   };
+
+  programs.zsh.interactiveShellInit = /*bash*/ ''
+
+    fanatec-load-profile() {
+      if [ -z "$1" ]; then
+        echo "Usage: fanatec-load-profile (beamng|assetto corsa)"
+        return 1
+      fi
+
+      sens=1080
+      if [ "$1" = "beamng" ]; then sens=2520; fi
+
+      dir="/sys/module/hid_fanatec/drivers/hid:fanatec/0003:0EB7:0020.0015/ftec_tuning/0003:0EB7:0020.0015"
+      echo "$sens" | sudo tee "$dir/SEN"
+      echo 70 | sudo tee "$dir/FEI"
+      echo 1 | sudo tee "$dir/NDP"
+      echo 0 | sudo tee "$dir/NFR"
+      echo 0 | sudo tee "$dir/NIN"
+      echo 2 | sudo tee "$dir/INT"
+    }
+
+  '';
 }

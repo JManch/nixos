55423b2aeb787476d7060f788279021b0be6e3a4
Joshua Manchester <JManch@protonmail.com>
2024-04-10 18:50:17 +0100

    fastfetch: improve config

--- modules/home-manager/programs/fastfetch.nix
+++ modules/home-manager/programs/fastfetch.nix
@@ -16,44 +16,43 @@ lib.mkIf cfg.enable
       "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
       "modules": [
         "title",
-          "separator",
-          {
-            "type": "os",
-            "format": "{3} {12}"
-          },
-          {
-            "type": "host",
-            "format": "{/2}{-}{/}{2}{?3} {3}{?}"
-          },
-          "kernel",
-          "uptime",
-          "packages",
-          "shell",
-          {
-            "type": "display",
-            "compactType": "original",
-            "key": "Resolution"
-          },
-          "de",
-          "wm",
-          "wmtheme",
-          "icons",
-          "terminal",
-          {
-            "type": "terminalfont",
-            "format": "{/2}{-}{/}{2}{?3} {3}{?}"
-          },
-          "cpu",
-          {
-            "type": "gpu",
-            "key": "GPU"
-          },
-          {
-            "type": "memory",
-            "format": "{/1}{-}{/}{/2}{-}{/}{} / {}"
-          },
-          "break",
-          "colors"
+        {
+          "type": "os",
+          "format": "{3} {12}"
+        },
+        {
+          "type": "host",
+          "format": "{/2}{-}{/}{2}{?3} {3}{?}"
+        },
+        "kernel",
+        "uptime",
+        "packages",
+        "shell",
+        {
+          "type": "display",
+          "compactType": "original",
+          "key": "Resolution"
+        },
+        "de",
+        "wm",
+        "wmtheme",
+        "icons",
+        "cursor",
+        "terminal",
+        {
+          "type": "terminalfont",
+          "format": "{/2}{-}{/}{2}{?3} {3}{?}"
+        },
+        "cpu",
+        {
+          "type": "gpu",
+          "key": "GPU"
+        },
+        {
+          "type": "memory",
+          "format": "{/1}{-}{/}{/2}{-}{/}{} / {}"
+        },
+        "disk",
       ]
     }
 

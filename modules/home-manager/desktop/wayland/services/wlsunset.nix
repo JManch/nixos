156bf7db0f6eb02c3549758e58f3129ab42b1858
Joshua Manchester <JManch@protonmail.com>
2024-04-12 21:51:33 +0100

    wlsunset: add note about audio stuttering

--- modules/home-manager/desktop/wayland/services/wlsunset.nix
+++ modules/home-manager/desktop/wayland/services/wlsunset.nix
@@ -4,6 +4,9 @@ let
 in
 lib.mkIf (cfg.enable && osConfig.device.gpu.type == "amd")
 {
+  # WARN: Wlsunset (or any other gamma adjuster) will cause system audio to
+  # stutter during gamma adjustments if audio is coming from monitors. This
+  # includes using the headphone jack on a monitor.
   services.wlsunset = {
     enable = true;
     latitude = "50.8";

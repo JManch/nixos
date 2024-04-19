c8d633f9002a76cdb4e4487cbef6eaa12877e2a8
Joshua Manchester <JManch@protonmail.com>
2024-04-19 16:43:53 +0100

    spotify: use terminal colors

--- modules/home-manager/programs/spotify.nix
+++ modules/home-manager/programs/spotify.nix
@@ -95,23 +95,6 @@ mkIf (cfg.enable && osConfig.modules.system.audio.enable)
 
         [[themes]]
         name = "default2"
-        [themes.palette]
-        black = "black"
-        red = "red"
-        green = "green"
-        yellow = "yellow"
-        blue = "blue"
-        magenta = "magenta"
-        cyan = "cyan"
-        white = "white"
-        bright_black = "bright_black"
-        bright_red = "bright_red"
-        bright_green = "bright_green"
-        bright_yellow = "bright_yellow"
-        bright_blue = "bright_blue"
-        bright_magenta = "bright_magenta"
-        bright_cyan = "bright_cyan"
-        bright_white = "bright_white"
         [themes.component_style]
         block_title = { fg = "Green", modifiers = ["Bold"] }
         border = { fg = "BrightBlack" }

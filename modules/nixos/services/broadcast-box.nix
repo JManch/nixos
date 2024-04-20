03225e52488196b1a6cdd0fa4538feb482f12fcc
Joshua Manchester <JManch@protonmail.com>
2024-04-20 13:10:31 +0100

    broadcast box: override package

--- modules/nixos/services/broadcast-box.nix
+++ modules/nixos/services/broadcast-box.nix
@@ -11,45 +11,45 @@ let
   cfg = config.modules.services.broadcast-box;
 
   # Overriding buildGoModule packages is not elegant...
-  # broadcast-box =
-  #   let
-  #     version = "2024-03-20";
-  #     src = pkgs.fetchFromGitHub {
-  #       repo = "broadcast-box";
-  #       owner = "Glimesh";
-  #       rev = "b37dd4ce79741849e1601812ac7f70fa6a2e0f02";
-  #       sha256 = "sha256-kDpDELp7yjDtCwZYd1rg/JsemE9zTxQh1Bucz9pgl3o=";
-  #     };
-  #   in
-  #   (pkgs.broadcast-box.overrideAttrs (oldAttrs: {
-  #     frontend = pkgs.buildNpmPackage {
-  #       version = oldAttrs.version;
-  #       pname = "${oldAttrs.pname}-web";
-  #       src = "${src}/web";
-  #       npmDepsHash = "sha256-/rlyfQMolsfgtk9wxfi5DUy/ZxCny5ahKGu9aoJEzWw=";
-  #       preBuild = ''
-  #         # The REACT_APP_API_PATH environment variable is needed
-  #         cp "${src}/.env.production" ../
-  #       '';
-  #       installPhase = ''
-  #         mkdir -p $out
-  #         cp -r build $out
-  #       '';
-  #     };
-  #   })).override {
-  #     buildGoModule = args: pkgs.buildGoModule (args // {
-  #       inherit src version;
-  #       # Because the package uses proxyVendor it seems that vendorHashe breaks
-  #       # everytime go updates...
-  #       vendorHash = "sha256-in31kbbZAexbHuqeZ8BpPw3jCTeJsEHM2IGajXlyIDg=";
-  #     });
-  #   };
+  broadcast-box =
+    let
+      version = "2024-04-20";
+      src = pkgs.fetchFromGitHub {
+        repo = "broadcast-box";
+        owner = "Glimesh";
+        rev = "b37dd4ce79741849e1601812ac7f70fa6a2e0f02";
+        sha256 = "sha256-h9Yz/pIYUdP3L5UPsUCXNK0UBpZZKLSYK+ng2l9HyTE=";
+      };
+    in
+    (pkgs.broadcast-box.overrideAttrs (oldAttrs: {
+      frontend = pkgs.buildNpmPackage {
+        version = oldAttrs.version;
+        pname = "${oldAttrs.pname}-web";
+        src = "${src}/web";
+        npmDepsHash = "sha256-pbe/J0K+7jrZGONy9Fjiyqqee0d0SeWU7o1iqFl2Y78=";
+        preBuild = ''
+          # The REACT_APP_API_PATH environment variable is needed
+          cp "${src}/.env.production" ../
+        '';
+        installPhase = ''
+          mkdir -p $out
+          cp -r build $out
+        '';
+      };
+    })).override {
+      buildGoModule = args: pkgs.buildGoModule (args // {
+        inherit src version;
+        # Because the package uses proxyVendor it seems that vendorHashe breaks
+        # everytime go updates...
+        vendorHash = "sha256-D21D2GGunkXrq3l0Pex5ap+K/0rfhTf/choSR3oaAFY=";
+      });
+    };
 in
 mkIf cfg.enable
 {
   services.broadcast-box = {
     enable = true;
-    # package = broadcast-box;
+    package = broadcast-box;
     http.port = cfg.port;
     udpMux.port = cfg.udpMuxPort;
     # This breaks local streaming without hairpin NAT so hairpin NAT is needed

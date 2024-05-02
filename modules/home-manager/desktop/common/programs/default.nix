9be8236414adc7b62bc8b88c1d78010dde2f5b31
Joshua Manchester <JManch@protonmail.com>
2024-05-02 17:12:02 +0100

    nautilus: add package

--- modules/home-manager/desktop/common/programs/default.nix
+++ modules/home-manager/desktop/common/programs/default.nix
@@ -1,4 +1,8 @@
-{ lib, ... }:
+{ lib, pkgs, osConfig, ... }:
 {
   imports = lib.utils.scanPaths ./.;
+
+  config = lib.mkIf osConfig.usrEnv.desktop.enable {
+    home.packages = [ pkgs.gnome.nautilus ];
+  };
 }

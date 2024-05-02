cd5310f6f424b9294e0bd9fe7ede25af4a2b051a
Joshua Manchester <JManch@protonmail.com>
2024-05-03 00:44:15 +0100

    chatterino: use new hyprland socket dir

--- modules/home-manager/programs/chatterino.nix
+++ modules/home-manager/programs/chatterino.nix
@@ -68,7 +68,7 @@ let
           esac
         }
 
-        socat -U - UNIX-CONNECT:"/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" | while read -r line; do handle "$line"; done
+        socat -U - UNIX-CONNECT:"/$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" | while read -r line; do handle "$line"; done
 
       '';
     };

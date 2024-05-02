6b7ad302cc6c910d1904df201545a5cf7efc12df
Joshua Manchester <JManch@protonmail.com>
2024-05-02 16:57:11 +0100

    vscode: remove mimeTypes

--- modules/home-manager/programs/vscode.nix
+++ modules/home-manager/programs/vscode.nix
@@ -12,12 +12,36 @@ mkIf cfg.enable
 
   programs.vscode = {
     enable = true;
-    # WARN: This is the only way I'm able to get C++ debugging to work. Because
-    # vscode runs in a FHS, it won't inherit PATH from nix develop or nix shell
-    # environments. Solution is to run nix shell inside the vscode terminal.
-    # Also, some tooling such as LSP servers may need to be added here. I don't
-    # use VSCode as my main editor though so not a big deal.
-    package = pkgs.vscode.fhsWithPackages (ps: with ps; [
+    package = (pkgs.vscode.overrideAttrs (finalAttrs: prevAttrs: {
+      # Override the desktopItem and remove the `mimeTypes` attribute because
+      # we never want VSCode opening as a default app
+      desktopItem =
+        let
+          inherit (finalAttrs.passthru) executableName longName;
+        in
+        pkgs.makeDesktopItem {
+          name = executableName;
+          desktopName = longName;
+          comment = "Code Editing. Redefined.";
+          genericName = "Text Editor";
+          exec = "${executableName} %F";
+          icon = "vs${executableName}";
+          startupNotify = true;
+          startupWMClass = "Code";
+          categories = [ "Utility" "TextEditor" "Development" "IDE" ];
+          keywords = [ "vscode" ];
+          actions.new-empty-window = {
+            name = "New Empty Window";
+            exec = "${executableName} --new-window %F";
+            icon = "vs${executableName}";
+          };
+        };
+      # WARN: This is the only way I'm able to get C++ debugging to work. Because
+      # vscode runs in a FHS, it won't inherit PATH from nix develop or nix shell
+      # environments. Solution is to run nix shell inside the vscode terminal.
+      # Also, some tooling such as LSP servers may need to be added here. I don't
+      # use VSCode as my main editor though so not a big deal.
+    })).fhsWithPackages (ps: with ps; [
       gdb
     ]);
 

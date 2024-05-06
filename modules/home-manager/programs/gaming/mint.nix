7f1a9854af3f8d3b4820a97d70a07725e28c472a
Joshua Manchester <JManch@protonmail.com>
2024-05-06 05:16:54 +0100

    mint: remove patch

--- modules/home-manager/programs/gaming/mint.nix
+++ modules/home-manager/programs/gaming/mint.nix
@@ -2,11 +2,7 @@
 let
   inherit (lib) mkIf getExe utils optional;
   cfg = config.modules.programs.gaming.mint;
-  mint = ((utils.flakePkgs args "mint").default).overrideAttrs (oldAttrs: {
-    # Patch removes the [MODDED] prefix from lobby names. I only use verified
-    # and local mods.
-    patches = (oldAttrs.patches or [ ]) ++ [ ../../../../patches/mint.patch ];
-  });
+  mint = (utils.flakePkgs args "mint").default;
 in
 mkIf cfg.enable
 {

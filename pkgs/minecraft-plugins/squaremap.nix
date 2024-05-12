12765d5b74fcceb4a984a40bee6b6b404e8c431e
Joshua Manchester <JManch@protonmail.com>
2024-05-12 18:05:44 +0100

    minecraft-server: add squaremarker plugin

--- pkgs/minecraft-plugins/squaremap.nix
+++ pkgs/minecraft-plugins/squaremap.nix
@@ -3,6 +3,12 @@
 , stdenvNoCC
 , ...
 }:
+let
+  squaremarker = fetchurl {
+    url = "https://github.com/SentixDev/squaremarker/releases/download/1.20.2-v1.0.5/squaremarker-paper-1.0.5.jar";
+    sha256 = "sha256-AVbHM+i4IT9ZoVWjl3I2e88onDMfY36VfRp+jLFPCUA=";
+  };
+in
 stdenvNoCC.mkDerivation rec {
   pname = "squaremap";
   version = "1.2.3";
@@ -17,6 +23,7 @@ stdenvNoCC.mkDerivation rec {
 
   installPhase = ''
     install -m555 -D ${src} -t "$out"
+    install -m555 ${squaremarker} -t "$out"
   '';
 
   meta = with lib; {

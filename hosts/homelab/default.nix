3cee275f386d12be66b78b5998e88b82f52529b3
Joshua Manchester <JManch@protonmail.com>
2024-05-25 19:55:01 +0100

    homelab: add note about go2rtc update

--- hosts/homelab/default.nix
+++ hosts/homelab/default.nix
@@ -64,6 +64,8 @@
       frigate = {
         enable = true;
         # Disabling in an attempt to workaround https://github.com/AlexxIT/go2rtc/issues/716
+        # TODO: Update: this is likely fixed by go2rtc 1.9.2 but there are new
+        # webrtc options and I need to go through them
         webrtc.enable = false;
         nvrAddress = "192.168.88.229";
       };

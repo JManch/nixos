139f0dd60c7f977ddc78dd669834ed3bbb0777e5
Joshua Manchester <JManch@protonmail.com>
2024-05-25 16:34:15 +0100

    hyprlock: use hm module and remove outdated config

--- modules/home-manager/desktop/wayland/programs/hyprlock.nix
+++ modules/home-manager/desktop/wayland/programs/hyprlock.nix
@@ -1,68 +1,18 @@
 { lib
+, pkgs
 , inputs
 , config
-, osConfig
 , ...
 }:
 let
   inherit (lib) mkIf fetchers;
   cfg = config.modules.desktop.programs.hyprlock;
-  colors = config.colorScheme.palette;
-
   isWayland = fetchers.isWayland config;
-  primaryMonitor = fetchers.primaryMonitor osConfig;
 in
-{
-  imports = [
-    inputs.hyprlock.homeManagerModules.default
-  ];
-
-  disabledModules = [ "${inputs.home-manager}/modules/programs/hyprlock.nix" ];
-
-  config = mkIf (cfg.enable && isWayland) {
-    programs.hyprlock = {
-      enable = true;
-
-      general = {
-        disable_loading_bar = false;
-        grace = 3;
-        hide_cursor = true;
-        no_fade_in = false;
-      };
-
-      backgrounds = [{
-        path = "screenshot";
-        color = "0xff${colors.base00}";
-        blur_size = 2;
-        blur_passes = 3;
-        brightness = 1;
-        contrast = 1;
-      }];
-
-      input-fields = [{
-        monitor = primaryMonitor.name;
-        size.width = 300;
-        size.height = 40;
-        outline_thickness = 3;
-        outer_color = "0xff${colors.base00}";
-        inner_color = "0xff${colors.base00}";
-        font_color = "0xff${colors.base07}";
-        fade_on_empty = true;
-        placeholder_text = "";
-        hide_input = false;
-        position = { x = 0; y = -30; };
-        halign = "center";
-        valign = "center";
-      }];
-
-      labels = [{
-        monitor = primaryMonitor.name;
-        text = "$TIME";
-        position = { x = 0; y = 30; };
-        font_family = config.modules.desktop.style.font.family;
-        font_size = 40;
-        color = "0xff${colors.base07}";
-      }];
-    };
+mkIf (cfg.enable && isWayland) {
+  programs.hyprlock = {
+    enable = true;
+    package = inputs.hyprlock.packages.${pkgs.system}.default;
+    # TODO: Configure
   };
 }

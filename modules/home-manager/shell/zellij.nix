c25255c8a8c7719cb12bb1c77c88a899720500a0
Joshua Manchester <JManch@protonmail.com>
2024-05-27 14:10:05 +0100

    zellij: add note about waiting for theming improvements

--- modules/home-manager/shell/zellij.nix
+++ modules/home-manager/shell/zellij.nix
@@ -4,8 +4,8 @@ lib.mkIf config.modules.shell.enable
   programs.zellij = {
     enable = true;
     # TODO: Configure this
-    settings = {
-      # pane_frames = false;
-    };
+    # Waiting for better theming
+    # https://github.com/zellij-org/zellij/issues/2297
+    # https://github.com/zellij-org/zellij/pull/3242
   };
 }

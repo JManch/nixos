abac7d822a5c2e14bae824c1d021f37d4ae607f7
Joshua Manchester <JManch@protonmail.com>
2024-05-31 18:20:07 +0100

    neovim: add darkman theme switch integration

--- modules/home-manager/programs/neovim.nix
+++ modules/home-manager/programs/neovim.nix
@@ -6,6 +6,7 @@
 }:
 let
   inherit (lib) mkIf optionalString;
+  inherit (config.modules.desktop.services) darkman;
   cfg = config.modules.programs.neovim;
 in
 mkIf cfg.enable
@@ -61,12 +62,15 @@ mkIf cfg.enable
   xdg.configFile."nvim".source = pkgs.fetchFromGitHub {
     repo = "nvim";
     owner = "JManch";
-    rev = "e239aaeec106dc883b751fd99554bfeea57079ad";
-    hash = "sha256-NggXREdnZ0a9QMOGOTJkgi9QUyL0xxlFzuvLD8IyOuQ=";
+    rev = "0c7104ba277c89d49fa247417ce366f84a04a59f";
+    hash = "sha256-gpplG8ARyQ3pi2+nNRU6RGnNYkFPvBZIlJFBz/yzlUE=";
   };
 
   # For conditional nix-specific config in nvim config
-  home.sessionVariables.NIX_NEOVIM = 1;
+  home.sessionVariables = {
+    NIX_NEOVIM = 1;
+    NIX_NEOVIM_DARKMAN = if darkman.enable then 1 else 0;
+  };
 
   xdg.mimeApps = mkIf osConfig.usrEnv.desktop.enable {
     defaultApplications = {
@@ -92,6 +96,12 @@ mkIf cfg.enable
       }
 
     '';
+
+  # Change theme of all active Neovim instances
+  darkman.switchScripts.neovim = theme: /*bash*/ ''
+    ls "$XDG_RUNTIME_DIR"/nvim.*.0 | xargs -I {} \
+      nvim --server {} --remote-expr "execute('Sunset${if theme == "dark" then "Night" else "Day"}')"
+  '';
 
   persistence.directories = [
     ".cache/nvim"

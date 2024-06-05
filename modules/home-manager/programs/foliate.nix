d864a1982c7dbd9bd79c01d8d4c0707741dc8daf
Joshua Manchester <JManch@protonmail.com>
2024-06-05 01:59:56 +0100

    foliate: persist cache dir

--- modules/home-manager/programs/foliate.nix
+++ modules/home-manager/programs/foliate.nix
@@ -7,5 +7,7 @@ lib.mkIf cfg.enable
   home.packages = [ pkgs.foliate ];
   persistence.directories = [
     ".local/share/com.github.johnfactotum.Foliate"
+    # Book covers do not show if cache is deleted
+    ".cache/com.github.johnfactotum.Foliate"
   ];
 }

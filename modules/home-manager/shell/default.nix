851adf889f95907861b6b506b2de2d0de7888233
Joshua Manchester <JManch@protonmail.com>
2024-06-11 21:57:53 +0100

    shell: add tomato-c package

--- modules/home-manager/shell/default.nix
+++ modules/home-manager/shell/default.nix
@@ -2,6 +2,27 @@
 let
   inherit (lib) utils mkEnableOption mkOption types mkIf optionals;
   cfg = config.modules.shell;
+
+  tomato-c = pkgs.tomato-c.overrideAttrs (_: {
+    version = "2024-06-11";
+    src = pkgs.fetchFromGitHub {
+      owner = "gabrielzschmitz";
+      repo = "Tomato.C";
+      rev = "b3b85764362a7c120f3312f5b618102a4eac9f01";
+      hash = "sha256-7i+vn1dAK+bAGpBlKTnSBUpyJyRiPc7AiUF/tz+RyTI=";
+    };
+    patches = [ ];
+    postPatch = ''
+      substituteInPlace notify.c \
+        --replace-fail "/usr/local" "${placeholder "out"}"
+      substituteInPlace util.c \
+        --replace-fail "/usr/local" "${placeholder "out"}"
+      substituteInPlace tomato.desktop \
+        --replace-fail "/usr/local" "${placeholder "out"}"
+      substituteInPlace Makefile \
+        --replace-fail "sudo" ""
+    '';
+  });
 in
 {
   imports = lib.utils.scanPaths ./.;
@@ -30,7 +51,10 @@ in
       rename
       nurl # tool for generating nix fetcher calls from urls
     ])
-    ++ [ (utils.flakePkgs args "yaml2nix").default ]
+    ++ [
+      (utils.flakePkgs args "yaml2nix").default
+      tomato-c
+    ]
     ++ optionals cfg.sillyTools (with pkgs; [
       fortune
       cowsay

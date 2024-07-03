152d89e2d61a43b6f026c9874faadd525fd3c7fa
Joshua Manchester <JManch@protonmail.com>
2024-07-03 20:19:24 +0100

    udiskie: add assertion

--- modules/home-manager/desktop/common/services/udiskie.nix
+++ modules/home-manager/desktop/common/services/udiskie.nix
@@ -10,6 +10,11 @@ let
 in
 lib.mkIf (desktopEnabled && udisks.enable && !vmVariant)
 {
+  assertions = lib.utils.asserts [
+    (osConfig.modules.system.desktop.desktopEnvironment == null)
+    "udiskie should not need to be enabled with a desktop environment"
+  ];
+
   # Use `udiskie-umount -a` to unmount all
   home.packages = [ pkgs.udiskie ];
 

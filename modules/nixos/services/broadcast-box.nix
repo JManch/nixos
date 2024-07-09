1c4ef25ef4232874bf87ca2270f45f2214dfcdbe
Joshua Manchester <JManch@protonmail.com>
2024-07-09 22:13:03 +0100

    broadcast-box: use new module

--- modules/nixos/services/broadcast-box.nix
+++ modules/nixos/services/broadcast-box.nix
@@ -1,6 +1,6 @@
 { lib, config, inputs, ... }:
 let
-  inherit (lib) mkIf utils mkForce optional genAttrs;
+  inherit (lib) mkIf utils mkForce optional genAttrs optionalString;
   inherit (inputs.nix-resources.secrets) fqDomain;
   inherit (config.modules.services) caddy;
   cfg = config.modules.services.broadcast-box;
@@ -18,12 +18,14 @@ in
 
     services.broadcast-box = {
       enable = true;
-      http.port = cfg.port;
-      udpMux.port = cfg.udpMuxPort;
-      # This breaks local streaming without hairpin NAT so hairpin NAT is needed
-      # for streaming from local network when proxying
-      nat.autoConfigure = cfg.proxy;
-      statusAPI = !cfg.proxy;
+      settings = {
+        HTTP_ADDRESS = "${optionalString cfg.proxy "127.0.0.1"}:${toString cfg.port}";
+        UDP_MUX_PORT = cfg.udpMuxPort;
+        # This breaks local streaming without hairpin NAT so hairpin NAT is needed
+        # for streaming from local network when proxying
+        INCLUDE_PUBLIC_IP_IN_NAT_1_TO_1_IP = cfg.proxy;
+        DISABLE_STATUS = true;
+      };
     };
 
     systemd.services.broadcast-box.wantedBy = mkForce (

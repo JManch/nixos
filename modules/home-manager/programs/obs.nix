5f591b1c5187037da079b71dfd5e99f59f5a8a25
Joshua Manchester <JManch@protonmail.com>
2024-07-21 02:34:55 +0100

    obs: use unstable-small package

--- modules/home-manager/programs/obs.nix
+++ modules/home-manager/programs/obs.nix
@@ -1,4 +1,9 @@
-{ lib, config, ... }:
+{
+  lib,
+  pkgs',
+  config,
+  ...
+}:
 let
   cfg = config.modules.programs.obs;
 in
@@ -8,7 +13,10 @@ lib.mkIf cfg.enable {
 
   # The folders in this directory are OBS profiles which can be
   # imported/exported in OBS
-  programs.obs-studio.enable = true;
+  programs.obs-studio = {
+    enable = true;
+    package = pkgs'.obs-studio;
+  };
 
   persistence.directories = [ ".config/obs-studio" ];
 }

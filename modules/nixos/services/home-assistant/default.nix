10c7261e9f361f106c1b871ac3020a4c9fe23e88
Joshua Manchester <JManch@protonmail.com>
2024-08-09 23:32:47 +0100

    home-assistant: update miele component

--- modules/nixos/services/home-assistant/default.nix
+++ modules/nixos/services/home-assistant/default.nix
@@ -125,8 +125,8 @@ in
             src = pkgs.fetchFromGitHub {
               owner = "astrandb";
               repo = "miele";
-              rev = "refs/tags/v2024.5.0";
-              hash = "sha256-rZSAhH19B0VzIQ6AwJN07zkvIdDHdYvUjizHLmDGh6Y=";
+              rev = "refs/tags/v2024.8.1";
+              hash = "sha256-XwaOQJvosCUXMZYrKX7sMWJIrMx36RhuVYUq163vvNg=";
             };
           })
           (pkgs.home-assistant-custom-components.waste_collection_schedule.overrideAttrs {

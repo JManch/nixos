4d3a852d61806769aab1297c745d50cf0d0454a6
Joshua Manchester <JManch@protonmail.com>
2024-08-24 20:00:23 +0100

    hyprpy: use pr branch for fix

--- pkgs/hyprpy.nix
+++ pkgs/hyprpy.nix
@@ -1,17 +1,20 @@
 {
   buildPythonPackage,
-  fetchPypi,
+  fetchFromGitHub,
   setuptools,
   pydantic,
   ...
 }:
-buildPythonPackage rec {
+buildPythonPackage {
   pname = "hyprpy";
   version = "0.1.8";
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-15FGnc09cQyJWejOLV/+TTcJQKMX4RLPe+exqPFvMc8=";
+  # PR fixes issue with fullscreen state
+  src = fetchFromGitHub {
+    repo = "hyprpy";
+    owner = "Evangelospro";
+    rev = "867e7c97670ac9a322af64d5e46dfa46c68805b6";
+    hash = "sha256-SPfFl1meI32hafFObyOvTHtcLMJB1SSd7aV29ian+Xc=";
   };
 
   format = "pyproject";

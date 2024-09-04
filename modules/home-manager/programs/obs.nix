547dfd9fa077f9d470071b856972d9b3d08fb1b5
Joshua Manchester <JManch@protonmail.com>
2024-09-04 23:52:11 +0100

    obs: add webrtc patch and pipewire capture plugin

--- modules/home-manager/programs/obs.nix
+++ modules/home-manager/programs/obs.nix
@@ -1,6 +1,6 @@
 {
   lib,
-  pkgs',
+  pkgs,
   config,
   ...
 }:
@@ -8,14 +8,31 @@ let
   cfg = config.modules.programs.obs;
 in
 lib.mkIf cfg.enable {
-  # For P2P wireguard screensharing use
-  # srt://10.0.0.2:5201?mode=listener&latency=30000 as the stream address
-
-  # The folders in this directory are OBS profiles which can be
-  # imported/exported in OBS
   programs.obs-studio = {
     enable = true;
-    package = pkgs'.obs-studio;
+
+    package = pkgs.obs-studio.overrideAttrs (old: {
+      patches = (old.patches or [ ]) ++ [
+        (pkgs.fetchpatch {
+          name = "webrtc-pacing-handler";
+          url = "https://patch-diff.githubusercontent.com/raw/obsproject/obs-studio/pull/10966.patch";
+          hash = "sha256-VKNA0N4JHat/tLSRk3CLY+NcHsIoIvRHYfHo572KSGk=";
+        })
+      ];
+    });
+
+    plugins = [
+      (pkgs.obs-studio-plugins.obs-pipewire-audio-capture.overrideAttrs {
+        version = "2024-09-04";
+        src = pkgs.fetchFromGitHub {
+          owner = "dimtpap";
+          repo = "obs-pipewire-audio-capture";
+          rev = "38176824e5f95f5e2542130f6d7c027ea64536c4";
+          sha256 = "sha256-z1eHz5uxfwfauO0zB/mMxzRmte5UYKGwsi3CkQu5Vhc=";
+        };
+        cmakeFlags = [ ];
+      })
+    ];
   };
 
   persistence.directories = [ ".config/obs-studio" ];

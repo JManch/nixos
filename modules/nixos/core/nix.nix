6dc49fc1393039d9f2c6b50b22c23fd980a05666
Joshua Manchester <JManch@protonmail.com>
2024-09-12 16:24:21 +0100

    nix: remove overlay rambling

--- modules/nixos/core/nix.nix
+++ modules/nixos/core/nix.nix
@@ -144,42 +144,6 @@ in
   persistenceAdminHome.directories = [ ".remote-builds" ];
 
   # Nice explanation of overlays: https://archive.is/f8goR
-  #
-  # My summary of the differences between overlays and `overrideAttrs`:
-  #
-  # Overlays modify the package derivation in nixpkgs whilst `overrideAttrs`
-  # only modifies the package in the current context. In practice this means
-  # that modifications made with overlays will apply to all instances of the
-  # package throughout your entire configuration. If you want to modify a core
-  # package that many modules and packages depend on, you can see how this is a
-  # powerful feature. However, if not used carefully, overlays can trigger an
-  # unwanted system-wide butterfly effect on package dependencies causing many
-  # packages to be rebuilt from source. This is because modifying a package in
-  # nixpkgs will result in all packages that depend on this package to have
-  # their derivation modified and therefore their cache invalidated.
-  #
-  # `overrideAttrs` avoids this problem because the package is ONLY modified in
-  # the context where you applied the override. This means that any dependent
-  # packages will continue to use the unmodified version and will continue to
-  # use the binary cache. If a module has a `package` option, this can be a
-  # convenient place to apply the override as the package will be stored in the
-  # option and can be easily reused elsewhere.
-  #
-  # In my experience, for the majority of packages that I'm overriding, only
-  # overriding the package in the current context is sufficient. It has the
-  # advantage of being more explicit than overlays and also guarantees no
-  # unexpected dependency butterfly effects.
-  #
-  # Overlays allow for greater modifications to packages than `overrideAttrs`.
-  # Literally anything is possible. The entire derivation can be replaced with
-  # an entirely different package if desired. However, once again, in the vast
-  # majority of cases this extra functionality is not required.
-  #
-  # To summarise, for the majority of situations where you want to modify a
-  # package (using a different src, applying a patch etc...) overlays are NOT
-  # necessary and `overrideAttrs` can be used instead. However depending on the
-  # situation, overlays might be more convenient or required for their extra
-  # functionality.
   nixpkgs = {
     overlays = [ ];
     config.allowUnfree = true;

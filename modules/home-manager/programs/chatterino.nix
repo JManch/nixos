9b1ce2d622d78cfaf1e1b9ebc8612027602f6ab5
Joshua Manchester <JManch@protonmail.com>
2024-09-14 03:06:03 +0100

    chatterino: improve hyprland workspace script

--- modules/home-manager/programs/chatterino.nix
+++ modules/home-manager/programs/chatterino.nix
@@ -17,6 +17,7 @@ let
   inherit (config.home) homeDirectory;
   cfg = config.${ns}.programs.chatterino;
   desktopCfg = config.${ns}.desktop;
+  secondMonitor = lib.${ns}.getMonitorByNumber osConfig' 2;
 
   # This is the only way to load the twitch auth secret from agenix
   streamlink = pkgs.streamlink.overrideAttrs (oldAttrs: {
@@ -30,7 +31,8 @@ let
 
   twitchWorkspaceScript =
     let
-      chatterinoRatio = 1.65;
+      chatterinoRatio = 0.175;
+      chatterinoWidth = builtins.floor (secondMonitor.width * chatterinoRatio);
     in
     pkgs.writeShellApplication {
       name = "hypr-twitch-workspace";
@@ -54,9 +56,26 @@ let
             workspace_name="''${args[1]}"
             window_class="''${args[2]}"
             if [[ "$workspace_name" =~ ^(name:|)TWITCH$ ]]; then
-              if [[ "$window_class" == "mpv" || "$window_class" == "firefox" ]]; then
-                hyprctl --batch "dispatch focuswindow address:0x$window_address; dispatch movewindow l"
-                hyprctl dispatch splitratio exact ${toString chatterinoRatio}
+              # FIX: Using exact pixel values in the movewindowpixel dispatcher
+              # is not ideal as it's making assumptions about the monitor
+              # layout. Ideally we would use % with a decimal but Hyprland does
+              # not support it.
+              if [ "$window_class" = "com.chatterino." ]; then
+                hyprctl --batch "\
+                  dispatch setfloating address:0x$window_address; \
+                  dispatch movewindowpixel exact ${
+                    toString (secondMonitor.width - chatterinoWidth)
+                  } 0%,address:0x$window_address; \
+                  dispatch resizewindowpixel exact ${toString chatterinoWidth} 100%,address:0x$window_address; \
+                "
+              elif [[ "$window_class" == "mpv" || "$window_class" == "firefox" ]]; then
+                hyprctl --batch "\
+                  dispatch setfloating address:0x$window_address; \
+                  dispatch movewindowpixel exact 0% 0%,address:0x$window_address; \
+                  dispatch resizewindowpixel exact ${
+                    toString (secondMonitor.width - chatterinoWidth)
+                  } 100%,address:0x$window_address; \
+                "
               fi
             fi
           }
@@ -66,7 +85,6 @@ let
             workspace_name="''${1#*>>}"
             if [[ "$workspace_name" == "TWITCH" ]]; then
               chatterino > /dev/null 2>&1 &
-              sleep 0.5
               firefox --new-window twitch.tv/directory > /dev/null 2>&1 &
             fi
           }
@@ -126,22 +144,18 @@ mkIf cfg.enable {
 
   ${ns}.desktop.services.waybar.autoHideWorkspaces = [ "TWITCH" ];
 
-  desktop.hyprland.settings =
-    let
-      secondMonitor = lib.${ns}.getMonitorByNumber osConfig' 2;
-    in
-    {
-      exec-once = [ (getExe twitchWorkspaceScript) ];
-      workspace = [
-        "name:TWITCH,monitor:${secondMonitor.name}, gapsin:0, gapsout:0, rounding:false, border:false"
-      ];
-      bind = [ "${desktopCfg.hyprland.modKey}, T, workspace, name:TWITCH" ];
-      windowrulev2 = [
-        # Not using "name:" here does work however it causes my current workspace
-        # to unexpectedly switch so it's needed
-        "workspace name:TWITCH, class:mpv, title:^(twitch\.tv.*)$"
-      ];
-    };
+  desktop.hyprland.settings = {
+    exec-once = [ (getExe twitchWorkspaceScript) ];
+    workspace = [
+      "name:TWITCH,monitor:${secondMonitor.name}, rounding:false, border:false"
+    ];
+    bind = [ "${desktopCfg.hyprland.modKey}, T, workspace, name:TWITCH" ];
+    windowrulev2 = [
+      # Not using "name:" here does work however it causes my current workspace
+      # to unexpectedly switch so it's needed
+      "workspace name:TWITCH, class:mpv, title:^(twitch\.tv.*)$"
+    ];
+  };
 
   persistence.directories = [ ".local/share/chatterino/Settings" ];
 }

ad1272f025e3b8abf857fdaaa193ac6d44458019
Joshua Manchester <JManch@protonmail.com>
2024-10-01 19:12:56 +0100

    steam: remove protontricks overlay

--- modules/nixos/programs/gaming/steam.nix
+++ modules/nixos/programs/gaming/steam.nix
@@ -17,32 +17,6 @@ mkIf cfg.enable {
   # Gamescope : gamescope -W 2560 -H 1440 -f -r 165 --mangoapp -- gamemoderun %command%
   userPackages = [ pkgs.steam-run ];
 
-  # Temporarily use appinfo_v29 branch to fix
-  # https://github.com/Matoking/protontricks/issues/304
-  nixpkgs.overlays = [
-    (final: prev: {
-      protontricks =
-        (prev.protontricks.overrideAttrs {
-          src = final.fetchFromGitHub {
-            owner = "Matoking";
-            repo = "protontricks";
-            rev = "f7b1fa33b0438dbd72f7222703f8442e40edc510";
-            hash = "sha256-t794WEMJx/JNX3gTMHfgquFWB7yXkleW07+QURm1NPM=";
-          };
-        }).override
-          {
-            vdf = final.python312Packages.vdf.overrideAttrs {
-              src = final.fetchFromGitHub {
-                owner = "Matoking";
-                repo = "vdf";
-                rev = "981cad270c2558aeb8eccaf42cfcf9fabbbed199";
-                hash = "sha256-OPonFrYrEFYtx0T2hvSYXl/idsm0iDPwqlnm1KbTPIo=";
-              };
-            };
-          };
-    })
-  ];
-
   programs.steam = {
     enable = true;
     protontricks.enable = true;

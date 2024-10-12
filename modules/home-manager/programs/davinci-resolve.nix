b47df712a5fc257ae4f6b66acda90bb85cadb052
Joshua Manchester <JManch@protonmail.com>
2024-10-13 00:51:40 +0100

    davinci-resolve: add hyprland windowrules

--- modules/home-manager/programs/davinci-resolve.nix
+++ modules/home-manager/programs/davinci-resolve.nix
@@ -12,6 +12,16 @@ in
   imports = [ inputs.nix-resources.homeManagerModules.davinci-resolve-studio ];
 
   config = lib.mkIf cfg.enable {
-    persistence.directories = [ ".local/share/DaVinciResolve" ];
+    persistence.directories = [
+      ".local/share/DaVinciResolve"
+      # Set this as the primary media storage
+      # Also move the backup dir here Preferences->User->Project Save and Load
+      ".local/state/DaVinciResolve"
+    ];
+
+    desktop.hyprland.settings.windowrulev2 = [
+      "size 40% 40%, floating:1, class:^(resolve)$, title:^(?!resolve$)(?!Message$).*"
+      "center, floating:1, class:^(resolve)$, title:^(?!resolve$)(?!Message$).*"
+    ];
   };
 }

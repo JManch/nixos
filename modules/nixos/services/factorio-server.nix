b78ef19775413bcd356a05a9aaa89501e0bd821c
Joshua Manchester <JManch@protonmail.com>
2024-10-21 15:50:05 +0100

    factorio-server: update to factorio 2.0

--- modules/nixos/services/factorio-server.nix
+++ modules/nixos/services/factorio-server.nix
@@ -1,6 +1,7 @@
 {
   ns,
   lib,
+  pkgs,
   config,
   ...
 }:
@@ -11,6 +12,14 @@ in
 mkIf cfg.enable {
   services.factorio = {
     enable = true;
+    package = pkgs.factorio-headless.overrideAttrs {
+      version = "2.0.7";
+      src = pkgs.fetchurl {
+        name = "factorio_headless_x64-2.0.7.tar.xz";
+        url = "https://factorio.com/get-download/2.0.7/headless/linux64";
+        sha256 = "sha256-5qZKnuWx03c80fQKpc946Rlg6t5sUI/TZcNHUq/fJ2I=";
+      };
+    };
     public = false;
     saveName = "default";
     stateDirName = "factorio-server";

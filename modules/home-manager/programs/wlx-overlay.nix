514e776a79f2acb4c05fd914118b23e67523e65e
Joshua Manchester <JManch@protonmail.com>
2024-11-03 00:25:50 +0000

    wlx-overlay: add module

---
+++ modules/home-manager/programs/wlx-overlay.nix
@@ -0,0 +1,29 @@
+{
+  ns,
+  lib,
+  pkgs,
+  osConfig',
+  ...
+}:
+let
+  inherit (lib) mkIf getExe;
+in
+mkIf (osConfig'.${ns}.hardware.valve-index.enable or false) {
+  home.packages = [ pkgs.wlx-overlay-s ];
+
+  systemd.user.services.wlx-overlay-s = {
+    Unit = {
+      Description = "Lightweight OpenXR/OpenVR overlay for Wayland and X11 desktops";
+      After = [ "monado.service" ];
+      BindsTo = [ "monado.service" ];
+      Requires = [
+        "monado.socket"
+        "graphical-session.target"
+      ];
+    };
+    Service.ExecStart = "${getExe pkgs.wlx-overlay-s} --show";
+    Install.WantedBy = [ "monado.service" ];
+  };
+
+  persistence.directories = [ ".config/wlxoverlay" ];
+}

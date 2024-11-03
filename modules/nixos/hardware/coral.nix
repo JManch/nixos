22bba45b5d464c30e1e47f7a02645fb10cdf34c9
Joshua Manchester <JManch@protonmail.com>
2024-11-03 17:16:11 +0000

    coral: use upstream libedgetpu package

--- modules/nixos/hardware/coral.nix
+++ modules/nixos/hardware/coral.nix
@@ -1,13 +1,10 @@
 # I use this module with the Coral M.2 accelerator and it works perfectly in
-# Frigate. In theory it should also work with the USB accelerator. I packaged
-# libedgetpu following the instructions here:
-# https://github.com/NixOS/nixpkgs/issues/188719#issuecomment-2094575860
+# Frigate. In theory it should also work with the USB accelerator.
 {
   ns,
   lib,
   pkgs,
   config,
-  inputs,
   ...
 }:
 let
@@ -16,21 +13,13 @@ let
     listToAttrs
     optional
     singleton
+    makeLibraryPath
     ;
   cfg = config.${ns}.hardware.coral;
-  libedgetpu = config.boot.kernelPackages.callPackage "${inputs.nix-resources}/pkgs/libedgetpu" { };
-  gasket = config.boot.kernelPackages.gasket.overrideAttrs {
-    src = pkgs.fetchFromGitHub {
-      owner = "google";
-      repo = "gasket-driver";
-      rev = "5815ee3908a46a415aac616ac7b9aedcb98a504c";
-      sha256 = "sha256-O17+msok1fY5tdX1DvqYVw6plkUDF25i8sqwd6mxYf8=";
-    };
-  };
   isPci = cfg.type == "pci";
 in
 mkIf cfg.enable {
-  boot.extraModulePackages = optional isPci gasket;
+  boot.extraModulePackages = optional isPci config.boot.kernelPackages.gasket;
 
   users.groups = listToAttrs (singleton {
     name = if isPci then "apex" else "plugdev";
@@ -38,7 +27,7 @@ mkIf cfg.enable {
   });
 
   services.udev = {
-    packages = optional (!isPci) libedgetpu;
+    packages = optional (!isPci) pkgs.libedgetpu;
     extraRules = mkIf isPci ''
       SUBSYSTEM=="apex", MODE="0660", GROUP="apex"
     '';
@@ -47,8 +36,8 @@ mkIf cfg.enable {
   systemd.services.frigate = {
     serviceConfig.SupplementaryGroups = [ (if isPci then "apex" else "plugdev") ];
 
-    environment.LD_LIBRARY_PATH = lib.makeLibraryPath [
-      libedgetpu
+    environment.LD_LIBRARY_PATH = makeLibraryPath [
+      pkgs.libedgetpu
       # Even though this is technically not needed for pci version, Frigate
       # throws an error without it
       pkgs.libusb1

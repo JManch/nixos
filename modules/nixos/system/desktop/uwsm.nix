0db6a501bca5ee39fc365fdc54ad285bd25d56ac
Joshua Manchester <JManch@protonmail.com>
2024-11-14 18:04:14 +0000

    uwsm: enable fumon service

--- modules/nixos/system/desktop/uwsm.nix
+++ modules/nixos/system/desktop/uwsm.nix
@@ -1,29 +1,51 @@
-# This module is purely for using UWSM as a display manager. UWSM itself should
-# be enabled and setup on a per-desktop basis.
 {
   ns,
   lib,
+  pkgs,
   config,
   ...
 }:
 let
-  inherit (lib) mkIf mkOrder optionalString;
+  inherit (lib)
+    mkIf
+    mkMerge
+    mkOrder
+    getExe'
+    optionalString
+    ;
   inherit (cfg.uwsm) defaultDesktop;
   cfg = config.${ns}.system.desktop;
-  select = defaultDesktop == null;
 in
-mkIf (cfg.enable && (cfg.displayManager == "uwsm")) {
-  assertions = lib.${ns}.asserts [
-    config.programs.uwsm.enable
-    "Using UWSM as a display manager requires it to be enabled"
-  ];
+mkMerge [
+  {
+    assertions = lib.${ns}.asserts [
+      (cfg.displayManager == "uwsm" -> config.programs.uwsm.enable)
+      "Using UWSM as a display manager requires it to be enabled"
+    ];
+  }
 
-  programs.zsh.interactiveShellInit =
-    mkOrder 2000
-      # bash
-      ''
-        if uwsm check may-start ${optionalString select "&& uwsm select"}; then
-          exec systemd-cat -t uwsm_start uwsm start ${if select then "default" else defaultDesktop}
-        fi
-      '';
-}
+  (mkIf (cfg.enable && config.programs.uwsm.enable) {
+    systemd.user.services.fumon = {
+      wantedBy = [ "graphical-session.target" ];
+      path = [ pkgs.libnotify ];
+      serviceConfig.ExecStart = [
+        "" # to replace original ExecStart
+        (getExe' config.programs.uwsm.package "fumon")
+      ];
+    };
+
+    programs.zsh.interactiveShellInit =
+      let
+        select = defaultDesktop == null;
+      in
+      mkIf (cfg.displayManager == "uwsm") (
+        mkOrder 2000
+          # bash
+          ''
+            if uwsm check may-start ${optionalString select "&& uwsm select"}; then
+              exec systemd-cat -t uwsm_start uwsm start ${if select then "default" else defaultDesktop}
+            fi
+          ''
+      );
+  })
+]

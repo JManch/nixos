48adad8c7fba9c36da6d2d340952887db7788138
Joshua Manchester <JManch@protonmail.com>
2024-11-28 02:10:39 +0000

    ncase-m1: enable davinci-resolve

--- homes/ncase-m1.nix
+++ homes/ncase-m1.nix
@@ -75,7 +75,7 @@
       foliate.enable = true;
       rnote.enable = true;
       jellyfin-media-player.enable = true;
-      davinci-resolve.enable = false;
+      davinci-resolve.enable = true;
 
       firefox = {
         enable = true;

79fa44a8c74eff82fab5d2ac56e795042a99c1c8
Joshua Manchester <JManch@protonmail.com>
2024-12-02 05:05:07 +0000

    gamescope: do not enable gamescope session

--- modules/nixos/programs/gaming/gamescope.nix
+++ modules/nixos/programs/gaming/gamescope.nix
@@ -7,11 +7,8 @@
 let
   inherit (lib) mkIf;
   cfg = config.${ns}.programs.gaming.gamescope;
-  gamingCfg = config.${ns}.programs.gaming;
 in
 mkIf cfg.enable {
-  programs.steam = mkIf gamingCfg.steam.enable { gamescopeSession.enable = true; };
-
   programs.gamescope = {
     enable = true;
     # Would like to enable this but it causes gamescope to stop working in lutris and steam

f2e151d4969f74f2e561a638e31db1359a42131c
Joshua Manchester <JManch@protonmail.com>
2024-12-05 04:18:29 +0000

    firefox: fix toolbar hide css and move into derivation

--- modules/home-manager/programs/firefox.nix
+++ modules/home-manager/programs/firefox.nix
@@ -12,6 +12,7 @@ let
     getExe
     getExe'
     optional
+    optionalString
     singleton
     ;
   inherit (config.${ns}.programs) mpv;
@@ -136,98 +137,32 @@ mkIf (cfg.enable && (osConfig'.${ns}.system.desktop.enable or true)) {
           "network.connectivity-service.enabled" = false;
         };
 
-        userChrome =
-          mkIf cfg.hideToolbar # css
-            ''
-              * {
-                font-family: "${desktop.style.font.family}" !important;
-                font-size: 15px !important;
-              }
+        userChrome = ''
+          * {
+            font-family: "${desktop.style.font.family}" !important;
+            font-size: 15px !important;
+          }
 
-              /* Source file https://github.com/MrOtherGuy/firefox-csshacks/tree/master/chrome/autohide_toolbox.css made available under Mozilla Public License v. 2.0
-              See the above repository for updates as well as full license text. */
-
-              /* Hide the whole toolbar area unless urlbar is focused or cursor is over the toolbar */
-              /* Dimensions on non-Win10 OS probably needs to be adjusted */
-
-              /* Compatibility options for hide_tabs_toolbar.css and tabs_on_bottom.css at the end of this file */
-
-              :root{
-                --uc-autohide-toolbox-delay: 200ms; /* Wait 0.1s before hiding toolbars */
-                --uc-toolbox-rotation: 70deg;  /* This may need to be lower on mac - like 75 or so */
-              }
-
-              :root[sizemode="maximized"]{
-                --uc-toolbox-rotation: 70deg;
-              }
-
-              @media {
-                :root:not([lwtheme]) #navigator-toolbox{ background-color: -moz-dialog !important; }
-              }
-
-              :root[sizemode="fullscreen"],
-              :root[sizemode="fullscreen"] #navigator-toolbox{ margin-top: 0 !important; }
-
-              #navigator-toolbox{
-                position: fixed !important;
-                display: block;
-                background-color: var(--lwt-accent-color,black) !important;
-                transition: transform 82ms linear, opacity 82ms linear !important;
-                transition-delay: var(--uc-autohide-toolbox-delay) !important;
-                transform-origin: top;
-                transform: rotateX(var(--uc-toolbox-rotation));
-                opacity: 0;
-                line-height: 0;
-                z-index: 1;
-                pointer-events: none;
-              }
-
-              #navigator-toolbox:hover,
-              #navigator-toolbox:focus-within{
-                transition-delay: 33ms !important;
-                transform: rotateX(0);
-                opacity: 1;
-              }
-              /* This ruleset is separate, because not having :has support breaks other selectors as well */
-              #mainPopupSet:has(> #appMenu-popup:hover) ~ toolbox{
-                transition-delay: 33ms !important;
-                transform: rotateX(0);
-                opacity: 1;
-              }
-
-              #navigator-toolbox > *{ line-height: normal; pointer-events: auto }
-
-              #navigator-toolbox,
-              #navigator-toolbox > *{
-                width: 100vw;
-                -moz-appearance: none !important;
-              }
-
-              /* These two exist for oneliner compatibility */
-              #nav-bar{ width: var(--uc-navigationbar-width,100vw) }
-              #TabsToolbar{ width: calc(100vw - var(--uc-navigationbar-width,0px)) }
-
-              /* Don't apply transform before window has been fully created */
-              :root:not([sessionrestored]) #navigator-toolbox{ transform:none !important }
-
-              :root[customizing] #navigator-toolbox{
-                position: relative !important;
-                transform: none !important;
-                opacity: 1 !important;
-              }
-
-              #navigator-toolbox[inFullscreen] > #PersonalToolbar,
-              #PersonalToolbar[collapsed="true"]{ display: none }
-
-              /* Uncomment this if tabs toolbar is hidden with hide_tabs_toolbar.css */
-               /*#titlebar{ margin-bottom: -9px }*/
-
-              /* Uncomment the following for compatibility with tabs_on_bottom.css - this isn't well tested though */
-              /*
-              #navigator-toolbox{ flex-direction: column; display: flex; }
-              #titlebar{ order: 2 }
-              */
-            '';
+          ${optionalString cfg.hideToolbar (
+            builtins.readFile (
+              let
+                src = pkgs.fetchFromGitHub {
+                  owner = "MrOtherGuy";
+                  repo = "firefox-csshacks";
+                  rev = "fe0cb2cb7bdd10471973d5713ffe1a9e6d2feaef";
+                  hash = "sha256-xUDZO3n1QhGhFKuZvrYGsnhMiA3n15o/y7egSUrJffo=";
+                };
+              in
+              pkgs.runCommand "firefox-hide-toolbar-css" { buildInputs = [ pkgs.gnused ]; } ''
+                install -m644 "${src}/chrome/autohide_toolbox.css" $out
+                # Preferred activation distance
+                sed 's/^  --uc-toolbox-rotation:.*/  --uc-toolbox-rotation: 70deg;/' -i $out
+                # Without this replacement the tab bar has a black background
+                sed 's/^@media  (-moz-platform: windows){/@media {/' -i $out
+              ''
+            )
+          )}
+        '';
       };
     };
   };

632ef3627341198d2e0c987506ff7501af8660e0
Joshua Manchester <JManch@protonmail.com>
2024-12-26 00:21:50 +0000

    modernx: update package

--- pkgs/modernx.nix
+++ pkgs/modernx.nix
@@ -4,35 +4,34 @@
   makeFontsConf,
   stdenvNoCC,
 }:
-stdenvNoCC.mkDerivation (finalAttrs: rec {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "mpv-modernx";
-  version = "0.3.5.5";
+  version = "0.3.9";
 
   src = fetchFromGitHub {
     owner = "zydezu";
     repo = "ModernX";
-    rev = version;
-    hash = "sha256-sPpVwI8w5JsP/jML0viOSqhyYBVKfxWuKbxHkX3GVug=";
+    rev = "refs/tags/${finalAttrs.version}";
+    hash = "sha256-wjBFI69lcsVY3HDXyz1VV5IkQvkJQQfMbGwUYmwlJ9w=";
   };
 
   dontBuild = true;
 
   installPhase = ''
-    runHook preInstall
     install -m644 modernx.lua -Dt $out/share/mpv/scripts
-    install -m644 Material-Design-Iconic-Font.ttf -Dt $out/share/fonts
-    install -m644 Material-Design-Iconic-Round.ttf -Dt $out/share/fonts
-    runHook postInstall
+    install -m644 fluent-system-icons.ttf -Dt $out/share/fonts
   '';
 
-  passthru.scriptName = "modernx.lua";
-  passthru.extraWrapperArgs = [
-    "--set"
-    "FONTCONFIG_FILE"
-    (toString (makeFontsConf {
-      fontDirectories = [ "${finalAttrs.finalPackage}/share/fonts" ];
-    }))
-  ];
+  passthru = {
+    scriptName = "modernx.lua";
+    extraWrapperArgs = [
+      "--set"
+      "FONTCONFIG_FILE"
+      (toString (makeFontsConf {
+        fontDirectories = [ "${finalAttrs.finalPackage}/share/fonts" ];
+      }))
+    ];
+  };
 
   meta = with lib; {
     description = "A fork of modernX (a replacement for MPV that retains the functionality of the default OSC), adding additional features";

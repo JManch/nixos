c1fb51ba80aa842c96a9014639a0d9f54edc06ee
Joshua Manchester <JManch@protonmail.com>
2024-12-27 05:32:14 +0000

    gtk: fix broken gtk 4 theming

--- modules/home-manager/desktop/common/programs/gtk.nix
+++ modules/home-manager/desktop/common/programs/gtk.nix
@@ -12,37 +12,52 @@ let
     mkIf
     getExe'
     hiPrio
+    optionals
+    optionalString
     ;
   inherit (config.${ns}) colorScheme;
-  inherit (config.${ns}.desktop.services) darkman;
-  inherit (config.${ns}.desktop.style) cursor customTheme;
+  inherit (cfg.style) cursor customTheme;
   inherit (inputs.nix-colors.lib-contrib { inherit pkgs; }) gtkThemeFromScheme;
   cfg = config.${ns}.desktop;
+
+  # Rather than generating a custom gtk.css file from our base16 colorscheme
+  # like stylix does, we generate patched versions of Materia. This is because
+  # using gtk.css does not allow hot-reloading when switching between light and
+  # dark theme variants.
+  # https://github.com/danth/stylix/blob/963e77a3a4fc2be670d5a9a6cbeb249b8a43808a/modules/gtk/gtk.mustache#L3
   darkTheme = gtkThemeFromScheme { scheme = colorScheme.dark; };
   lightTheme = gtkThemeFromScheme { scheme = colorScheme.light; };
 in
 mkIf desktopEnabled {
-  home.packages = mkIf customTheme [
-    darkTheme
-    lightTheme
-    (hiPrio (
-      pkgs.runCommand "xdg-desktop-portal-gtk-session-slice" { } ''
-        install -Dm644 ${pkgs.xdg-desktop-portal-gtk}/share/systemd/user/xdg-desktop-portal-gtk.service -t $out/share/systemd/user
-        echo "Slice=session.slice" >> $out/share/systemd/user/xdg-desktop-portal-gtk.service
-        chmod 444 $out/share/systemd/user/xdg-desktop-portal-gtk.service
-      ''
-    ))
-  ];
+  home.packages =
+    [
+      (hiPrio (
+        pkgs.runCommand "xdg-desktop-portal-gtk-session-slice" { } ''
+          install -Dm644 ${pkgs.xdg-desktop-portal-gtk}/share/systemd/user/xdg-desktop-portal-gtk.service -t $out/share/systemd/user
+          echo "Slice=session.slice" >> $out/share/systemd/user/xdg-desktop-portal-gtk.service
+          chmod 444 $out/share/systemd/user/xdg-desktop-portal-gtk.service
+        ''
+      ))
+    ]
+    ++ optionals customTheme [
+      darkTheme
+      lightTheme
+    ];
 
   gtk = {
     enable = true;
 
-    theme = mkIf cfg.style.customTheme {
+    theme = mkIf customTheme {
       name = colorScheme.dark.slug;
-      package = darkTheme;
+      # We do not set the package here because it causes home manager to
+      # generate a gtk-4.0/gtk.css file. Unfortunately this results in broken
+      # GTK 4 theming that does not respond to light/dark theme switches
+      # (because gtk.css themes cannot be hot-reloaded). Just gonna give up on
+      # GTK 4 theming and use Adwaita (the default) in GTK 4 apps.
+      # https://github.com/nix-community/home-manager/blob/2f23fa308a7c067e52dfcc30a0758f47043ec176/modules/misc/gtk.nix#L239
     };
 
-    iconTheme = mkIf cfg.style.customTheme {
+    iconTheme = mkIf customTheme {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
     };
@@ -56,7 +71,7 @@ mkIf desktopEnabled {
     theme: # bash
     ''
       export XDG_DATA_DIRS=${schemas}/share/gsettings-schemas/${schemas.name}
-      ${gsettings} set org.gnome.desktop.interface gtk-theme ${colorScheme.${theme}.slug}
+      ${optionalString customTheme "${gsettings} set org.gnome.desktop.interface gtk-theme ${colorScheme.${theme}.slug}"}
       ${gsettings} set org.gnome.desktop.interface color-scheme prefer-${theme}
     '';
 
@@ -66,12 +81,5 @@ mkIf desktopEnabled {
     name = cursor.name;
     package = cursor.package;
     size = cursor.size;
-  };
-
-  desktop.hyprland.settings = {
-    env = [
-      "XCURSOR_THEME,${cursor.name}"
-      "XCURSOR_SIZE,${toString cursor.size}"
-    ];
   };
 }

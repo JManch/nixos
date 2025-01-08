a91f68b9a1b665944408a18aa9eed201c79886df
Joshua Manchester <JManch@protonmail.com>
2025-01-08 18:15:30 +0000

    waste-collection-schedule: update package

--- modules/nixos/services/home-assistant/default.nix
+++ modules/nixos/services/home-assistant/default.nix
@@ -157,7 +157,17 @@ in
 
       customComponents =
         [
-          pkgs.home-assistant-custom-components.waste_collection_schedule
+          (pkgs.home-assistant-custom-components.waste_collection_schedule.overrideAttrs (
+            final: _: {
+              version = "2.6.0";
+              src = pkgs.fetchFromGitHub {
+                owner = "mampfes";
+                repo = "hacs_waste_collection_schedule";
+                tag = final.version;
+                hash = "sha256-gfL5Nxe8io7DTya5x8aQ5PhxiH8rb8L3/CA+UqKEDAk=";
+              };
+            }
+          ))
           (pkgs.home-assistant-custom-components.adaptive_lighting.overrideAttrs (
             final: _: {
               version = "1.25.0";

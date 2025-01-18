cb97945d06645341267fe4f4239a46d4f82b51b3
Joshua Manchester <JManch@protonmail.com>
2025-01-18 19:31:53 +0000

    filen-desktop: update package

--- pkgs/filen-desktop.nix
+++ pkgs/filen-desktop.nix
@@ -1,47 +1,35 @@
 {
-  lib,
   fetchurl,
   makeWrapper,
   appimageTools,
-  libappindicator-gtk3,
 }:
 let
   pname = "filen-desktop";
-  version = "2.0.24";
+  version = "3.0.41";
 
   src = fetchurl {
     # They use an annoying system for latest version url
     # Means package will need a hash update whenever upstream updates
     # https://github.com/FilenCloudDienste/filen-desktop/issues/208
-    url = "https://cdn.filen.io/desktop/release/filen_x86_64.AppImage";
-    hash = "sha256-5vkndT9V/81fUdzS+KTfAjPAGO0IJRx8QhNxBNG8nnU=";
+    url = "https://github.com/FilenCloudDienste/filen-desktop/releases/download/v${version}/Filen_linux_x86_64.AppImage";
+    hash = "sha256-Nao5By8Z8lMbRcp2Mgw+xaiiFzUxCm6S3SAE5FfDZpk=";
   };
 
   appimageContents = appimageTools.extractType2 { inherit pname version src; };
-
-  # Needed for the tray to function
-  libPath = lib.makeLibraryPath [ libappindicator-gtk3 ];
 in
 appimageTools.wrapType2 rec {
   inherit pname version src;
 
-  extraInstallCommands = ''
-    source "${makeWrapper}/nix-support/setup-hook"
-    wrapProgram $out/bin/filen-desktop \
-      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland --enable-features=WaylandWindowDecorations}}" \
-      --prefix LD_LIBRARY_PATH : ${libPath}
+  extraInstallCommands = # bash
+    ''
+      source "${makeWrapper}/nix-support/setup-hook"
+      wrapProgram $out/bin/filen-desktop \
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland --enable-features=WaylandWindowDecorations}}"
 
-    install -m 444 -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
-    substituteInPlace $out/share/applications/${pname}.desktop \
-      --replace 'Exec=AppRun' 'Exec=filen-desktop'
-    cp -r ${appimageContents}/usr/share/icons $out/share
-  '';
-
-  meta = with lib; {
-    description = "Desktop client for Filen.io";
-    homepage = "https://filen.io/";
-    license = licenses.agpl3Plus;
-    changelog = "https://github.com/FilenCloudDienste/filen-desktop/releases/tag/v${version}";
-    platforms = [ "x86_64-linux" ];
-  };
+      install -m 644 -D ${appimageContents}/@filendesktop.desktop $out/share/applications/${pname}.desktop
+      substituteInPlace $out/share/applications/${pname}.desktop \
+        --replace-fail 'Exec=AppRun' 'Exec=filen-desktop'
+      cp --no-preserve=mode -r ${appimageContents}/usr/share/icons $out/share
+      find $out/share/icons/hicolor -type f -name "@filendesktop.png" -execdir mv {} filen-desktop.png \;
+    '';
 }

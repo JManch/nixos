2ccdd50b7cad7ed445cbffcdca2b34422d738d96
Joshua Manchester <JManch@protonmail.com>
2025-01-22 23:25:21 +0000

    surface-pro: add hardware notes

--- hosts/surface-pro/hardware-configuration.nix
+++ hosts/surface-pro/hardware-configuration.nix
@@ -16,15 +16,19 @@
     ];
   };
 
-  systemd.sleep.extraConfig = ''
-    HibernateDelaySec=30m
-    SuspendState=mem
-  '';
+  # suspend-then-hibernate is broken. For some reason after HibernateDelaySec
+  # have passed the system wakes up from suspend and, instead of hibernating,
+  # stays awake.
+  # systemd.sleep.extraConfig = ''
+  #   HibernateDelaySec=30m
+  # '';
 
   services.logind = {
-    powerKey = "suspend-then-hibernate";
+    # Power button doesn't work so these are pointless
+    # https://github.com/linux-surface/linux-surface/issues/1424
+    powerKey = "hibernate";
     powerKeyLongPress = "poweroff";
-    lidSwitch = "suspend-then-hibernate";
+    lidSwitch = "suspend";
   };
 
   services.tlp.enable = true;

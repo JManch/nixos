6832e926a50600074232fd82fe38ca892f2ab7f0
Joshua Manchester <JManch@protonmail.com>
2025-02-02 01:54:53 +0000

    hyprland: switch back to latest

--- flake.nix
+++ flake.nix
@@ -83,8 +83,7 @@
 
     # Inputs that provide packages
 
-    # To avoid blank window issues on git. Noticeable when toggling chatterino layout.
-    hyprland.url = "github:hyprwm/Hyprland?rev=c7086f936a06abebcc863c35f634760ca1ee90c0";
+    hyprland.url = "github:hyprwm/Hyprland";
     hyprland.inputs.nixpkgs.follows = "nixpkgs";
 
     hyprland-plugins.url = "github:hyprwm/hyprland-plugins";

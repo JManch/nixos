e427875989c8b00b7cdf2d61aabedc2a1e3e5e13
Joshua Manchester <JManch@protonmail.com>
2025-02-02 01:55:29 +0000

    feishin: remove package override

--- modules/home-manager/programs/desktop/feishin.nix
+++ modules/home-manager/programs/desktop/feishin.nix
@@ -1,20 +1,10 @@
 { lib, pkgs }:
-let
-  # https://github.com/NixOS/nixpkgs/pull/377468
-  inherit
-    (import (fetchTarball {
-      url = "https://github.com/DoctorDalek1963/nixpkgs/archive/ca840a01ced92c13606cb28a68bdc2f367452f9f.tar.gz";
-      sha256 = "sha256:0bzs81v1n1nrfsk41dvypcajsrgcrlzwp06s07dfmd8wbzq6r08q";
-    }) { inherit (pkgs) system; })
-    feishin
-    ;
-in
 {
   home.packages = [
     # we don't want feishin loading mpv scripts
     (pkgs.symlinkJoin {
       name = "feishin-mpv-unwrapped";
-      paths = [ feishin ];
+      paths = [ pkgs.feishin ];
       nativeBuildInputs = [ pkgs.makeWrapper ];
       postBuild = ''
         wrapProgram $out/bin/feishin \

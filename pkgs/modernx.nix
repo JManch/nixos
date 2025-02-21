25d42b3614fefcda569b3ab4f28d7f3dad7e4c98
Joshua Manchester <JManch@protonmail.com>
2025-02-21 01:43:30 +0000

    modernx: update package

--- pkgs/modernx.nix
+++ pkgs/modernx.nix
@@ -6,13 +6,13 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "mpv-modernx";
-  version = "0.3.9";
+  version = "0.4.1";
 
   src = fetchFromGitHub {
     owner = "zydezu";
     repo = "ModernX";
     tag = finalAttrs.version;
-    hash = "sha256-wjBFI69lcsVY3HDXyz1VV5IkQvkJQQfMbGwUYmwlJ9w=";
+    hash = "sha256-tm1vsHEFX2YnQ1w3DcLd/zHASetkqQ4wYcYT9w8HVok=";
   };
 
   dontBuild = true;
@@ -36,7 +36,6 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   meta = with lib; {
     description = "A fork of modernX (a replacement for MPV that retains the functionality of the default OSC), adding additional features";
     homepage = "https://github.com/zydezu/ModernX";
-    license = licenses.free;
     maintainers = with maintainers; [ JManch ];
   };
 })

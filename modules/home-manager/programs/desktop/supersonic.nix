6c491505f9b11510653c8911233e5cb2a1b0ebff
Joshua Manchester <JManch@protonmail.com>
2025-03-16 19:54:47 +0000

    supersonic: switch to x11 build

--- modules/home-manager/programs/desktop/supersonic.nix
+++ modules/home-manager/programs/desktop/supersonic.nix
@@ -1,42 +1,44 @@
 { lib, pkgs }:
 {
   home.packages = [
-    (pkgs.supersonic-wayland.overrideAttrs (old: {
+    (pkgs.supersonic.overrideAttrs (old: {
       version = "git";
 
       src = pkgs.fetchFromGitHub {
-        owner = "JManch";
+        owner = "dweymouth";
         repo = "supersonic";
-        rev = "4e619c08f89b0560639a137bcda0c944dc30198e";
-        hash = "sha256-2gtFWBL25LQIACap7JYtFJc1ShuhUg6oSwW7dlEKYqQ=";
+        rev = "31df8ab054e010978558739d962fcb69f26fbf89";
+        hash = "sha256-s4q7RvAG3oDBQ9ktfonEJXjrX/75MUpVq+9c43TRgWM=";
       };
 
       patches = [ ../../../../patches/supersonicLargeVolumeSlider.patch ];
 
-      vendorHash = "sha256-Y1oWiQUwL6TGtHs9CfksEzjaAYb9rFEewyN3Pvv7i0Q=";
+      tags = old.tags ++ [ "migrated_fynedo" ];
 
-      desktopItems = lib.singleton (
-        pkgs.makeDesktopItem {
-          name = "supersonic";
-          exec = "supersonic-wayland";
-          icon = "supersonic";
-          desktopName = "Supersonic";
-          genericName = "Subsonic Client";
-          comment = "A lightweight cross-platform desktop client for Subsonic music servers";
-          type = "Application";
-          categories = [
-            "Audio"
-            "AudioVideo"
-          ];
-        }
-      );
+      vendorHash = "sha256-E1F/89+pyIhmPSsfxWeMFTktGekU56HzSh3qIo8KAzo=";
+
+      # desktopItems = lib.singleton (
+      #   pkgs.makeDesktopItem {
+      #     name = "supersonic";
+      #     exec = "supersonic-wayland";
+      #     icon = "supersonic";
+      #     desktopName = "Supersonic";
+      #     genericName = "Subsonic Client";
+      #     comment = "A lightweight cross-platform desktop client for Subsonic music servers";
+      #     type = "Application";
+      #     categories = [
+      #       "Audio"
+      #       "AudioVideo"
+      #     ];
+      #   }
+      # );
 
       # When it's named supersonic-wayland it breaks the icon in a bunch of places
-      postInstall =
-        old.postInstall
-        + ''
-          find $out/share/icons/hicolor -type f -name "supersonic-wayland.png" -execdir mv {} supersonic.png \;
-        '';
+      # postInstall =
+      #   old.postInstall
+      #   + ''
+      #     find $out/share/icons/hicolor -type f -name "supersonic-wayland.png" -execdir mv {} supersonic.png \;
+      #   '';
     }))
   ];
 

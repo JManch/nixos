d89e7b12902be416ce1ac720f253637f6af9371f
Joshua Manchester <JManch@protonmail.com>
2025-03-17 21:15:57 +0000

    app2unit: update and package properly

--- pkgs/app2unit.nix
+++ pkgs/app2unit.nix
@@ -1,19 +1,44 @@
 {
+  lib,
   stdenvNoCC,
   fetchFromGitHub,
+  runtimeShell,
+  makeWrapper,
 }:
 stdenvNoCC.mkDerivation {
   pname = "app2unit";
-  version = "0-unstable-2024-12-26";
+  version = "0-unstable-2025-3-17";
 
   src = fetchFromGitHub {
     owner = "Vladimir-csp";
     repo = "app2unit";
-    rev = "e7e325ec701662788e50b7ac8934a7a23e86496b";
-    hash = "sha256-5tvGktkdmB3sdnw2wg6vtbC1kMjdRk11Q8a7QbPQMhk=";
+    rev = "0900cb6bce30122b8db5a1292e8fc2b14c1732ec";
+    hash = "sha256-1sQs9g9fzghC60Hl7oHH6F0uDmzNuNwDAz92THQGmt8=";
   };
 
-  installPhase = "install -Dm755 app2unit -t $out/bin";
+  nativeBuildInputs = [ makeWrapper ];
 
-  meta.mainProgram = "app2unit";
+  postPatch = ''
+    substituteInPlace app2unit \
+      --replace-fail "/bin/sh" "${runtimeShell}"
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dt $out/bin app2unit
+    makeWrapper $out/bin/app2unit $out/bin/app2unit-open \
+      --add-flags "--open"
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Launches Desktop Entries as Systemd user units";
+    homepage = "https://github.com/Vladimir-csp/app2unit";
+    mainProgram = "app2unit";
+    license = lib.licenses.gpl3;
+    maintainers = with lib.maintainers; [ JManch ];
+    platforms = lib.platforms.linux;
+  };
 }

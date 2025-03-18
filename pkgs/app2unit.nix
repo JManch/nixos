49423226859001cb91a8779afe3f9f8d9ecae88e
Joshua Manchester <JManch@protonmail.com>
2025-03-18 23:20:52 +0000

    app2unit: symlink instead of wrapping

--- pkgs/app2unit.nix
+++ pkgs/app2unit.nix
@@ -2,8 +2,6 @@
   lib,
   stdenvNoCC,
   fetchFromGitHub,
-  runtimeShell,
-  makeWrapper,
 }:
 stdenvNoCC.mkDerivation {
   pname = "app2unit";
@@ -16,21 +14,9 @@ stdenvNoCC.mkDerivation {
     hash = "sha256-1sQs9g9fzghC60Hl7oHH6F0uDmzNuNwDAz92THQGmt8=";
   };
 
-  nativeBuildInputs = [ makeWrapper ];
-
-  postPatch = ''
-    substituteInPlace app2unit \
-      --replace-fail "/bin/sh" "${runtimeShell}"
-  '';
-
   installPhase = ''
-    runHook preInstall
-
     install -Dt $out/bin app2unit
-    makeWrapper $out/bin/app2unit $out/bin/app2unit-open \
-      --add-flags "--open"
-
-    runHook postInstall
+    ln -s $out/bin/app2unit $out/bin/app2unit-open
   '';
 
   meta = {
@@ -38,7 +24,6 @@ stdenvNoCC.mkDerivation {
     homepage = "https://github.com/Vladimir-csp/app2unit";
     mainProgram = "app2unit";
     license = lib.licenses.gpl3;
-    maintainers = with lib.maintainers; [ JManch ];
     platforms = lib.platforms.linux;
   };
 }

7b882f3c301a982e6a42ed2d57aa8c0b6d9068c9
Joshua Manchester <JManch@protonmail.com>
2025-03-18 00:58:55 +0000

    installer: clean-up

--- hosts/installer/default.nix
+++ hosts/installer/default.nix
@@ -270,11 +270,9 @@ in
         };
     };
 
-    users.users.root = {
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMd4QvStEANZSnTHRuHg0edyVdRmIYYTcViO9kCyFFt7 JManch@protonmail.com"
-      ];
-    };
+    users.users.root.openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMd4QvStEANZSnTHRuHg0edyVdRmIYYTcViO9kCyFFt7 JManch@protonmail.com"
+    ];
 
     system.stateVersion = "24.05";
   };

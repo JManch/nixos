ab4723ed05c5f07cdbace8470d3e44f84122079b
Joshua Manchester <JManch@protonmail.com>
2025-03-31 22:58:34 +0100

    ssh: install sshfs

--- modules/nixos/system/ssh.nix
+++ modules/nixos/system/ssh.nix
@@ -1,6 +1,7 @@
 {
   lib,
   cfg,
+  pkgs,
   self,
   config,
   username,
@@ -84,6 +85,8 @@ in
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILXXsn0Q4UzpMAnYa5b2TE5KxTeBjLy0ck4eIwekD93b";
       };
   };
+
+  environment.systemPackages = [ pkgs.sshfs ];
 
   programs.zsh.shellAliases.ssh-forget = "ssh -o UserKnownHostsFile=/dev/null";
 

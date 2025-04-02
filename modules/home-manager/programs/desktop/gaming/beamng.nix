847053fb42419d6e7decc6f3b73e62959ed6b8c7
Joshua Manchester <JManch@protonmail.com>
2025-04-02 15:20:14 +0100

    beamng: add scheme handler desktop entry

--- modules/home-manager/programs/desktop/gaming/beamng.nix
+++ modules/home-manager/programs/desktop/gaming/beamng.nix
@@ -26,26 +26,38 @@ in
 
   home.packages = [ selfPkgs.beammp-launcher ];
 
-  xdg.desktopEntries.beammp-launcher =
-    let
-      xdg-terminal = getExe pkgs.xdg-terminal-exec;
-      beammp-launcher = getExe selfPkgs.beammp-launcher;
-    in
-    mkIf desktop.enable {
-      name = "BeamMP Launcher";
-      exec = "${xdg-terminal} --title=BeamMP -e ${beammp-launcher} --no-launch --no-update";
-      settings.Path = "${config.xdg.dataHome}/BeamMP";
-      terminal = false;
+  xdg.desktopEntries = {
+    beamng-scheme-handler = {
+      name = "beamng:// scheme handler";
       type = "Application";
-      icon = (
-        pkgs.fetchurl {
-          name = "beammp-icon.png";
-          url = "https://avatars.githubusercontent.com/u/76395149";
-          hash = "sha256-hogRsoB4l+pRh59QN6bNdUFHIP+d94HNwhTCftmhrp8=";
-        }
-      );
-      categories = [ "Game" ];
+      terminal = false;
+      exec = "${config.xdg.dataHome}/Steam/steamapps/common/BeamNG.drive/BinLinux/BeamNG_scheme_handler.sh %u";
+      mimeType = [ "x-scheme-handler/beamng" ];
+      noDisplay = true;
+      settings.Version = "1.0";
     };
+
+    beammp-launcher =
+      let
+        xdg-terminal = getExe pkgs.xdg-terminal-exec;
+        beammp-launcher = getExe selfPkgs.beammp-launcher;
+      in
+      mkIf desktop.enable {
+        name = "BeamMP Launcher";
+        exec = "${xdg-terminal} --title=BeamMP -e ${beammp-launcher} --no-launch --no-update";
+        settings.Path = "${config.xdg.dataHome}/BeamMP";
+        terminal = false;
+        type = "Application";
+        icon = (
+          pkgs.fetchurl {
+            name = "beammp-icon.png";
+            url = "https://avatars.githubusercontent.com/u/76395149";
+            hash = "sha256-hogRsoB4l+pRh59QN6bNdUFHIP+d94HNwhTCftmhrp8=";
+          }
+        );
+        categories = [ "Game" ];
+      };
+  };
 
   # Just to create the .local/share/BeamMP dir
   xdg.dataFile."BeamMP/nix-placeholder".text = "";

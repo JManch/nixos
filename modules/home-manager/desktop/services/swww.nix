7725cb945c92a46c7728f2f38afb7418163bd760
Joshua Manchester <JManch@protonmail.com>
2025-04-28 17:55:04 +0100

    swww: assert no scaling is used

--- modules/home-manager/desktop/services/swww.nix
+++ modules/home-manager/desktop/services/swww.nix
@@ -5,6 +5,7 @@
 }:
 let
   inherit (lib) ns getExe getExe';
+  inherit (osConfig.${ns}.core.device) monitors;
   transition =
     let
       inherit (osConfig.${ns}.core) device;
@@ -13,6 +14,11 @@ let
     "--transition-bezier .43,1.19,1,.4 --transition-type center --transition-duration 1 --transition-fps ${refreshRate}";
 in
 {
+  asserts = [
+    (lib.all (monitor: monitor.scale == 1) monitors)
+    "swww doesn't work for monitors with scale != 1 as it requires `swww img` to be called twice for the wallpaper to set after boot"
+  ];
+
   categoryConfig.wallpaper = {
     wallpaperUnit = "swww.service";
     setWallpaperScript = "${getExe pkgs.swww} img ${transition} \"$1\"";

6d3c19c0a1b47913c6b423701e4afdafc154c368
Joshua Manchester <JManch@protonmail.com>
2025-05-01 19:50:08 +0100

    brightnessctl: enable logind support

--- pkgs/brightnessctl.nix
+++ pkgs/brightnessctl.nix
@@ -1,17 +1,19 @@
 {
   brightnessctl,
-  pkg-config,
+  coreutils,
   sources,
 }:
 brightnessctl.overrideAttrs {
   version = "0-unstable-${sources.brightnessctl.revision}";
   src = sources.brightnessctl;
 
-  nativeBuildInputs = [ pkg-config ];
-  postPatch = "";
+  postPatch = ''
+    substituteInPlace 90-brightnessctl.rules \
+      --replace-fail /bin/ ${coreutils}/bin/
+  '';
 
   preBuild = ''
-    ./configure --dbus-provider=systemd
+    ./configure --dbus-provider=systemd --enable-logind
   '';
 
   makeFlags = [

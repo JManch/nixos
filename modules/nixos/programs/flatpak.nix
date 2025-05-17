91b76421251806fc0f1761a0446b03bd09c392bc
Joshua Manchester <JManch@protonmail.com>
2025-05-17 22:21:44 +0100

    flatpak: match upstream module

--- modules/nixos/programs/flatpak.nix
+++ modules/nixos/programs/flatpak.nix
@@ -62,7 +62,10 @@ in
     # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
     # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-    environment.systemPackages = [ pkgs.flatpak ];
+    environment.systemPackages = with pkgs; [
+      flatpak
+      fuse3
+    ];
 
     security.polkit.enable = true;
 

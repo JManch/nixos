d60be1c1b476f52c407a43adbe691557786d3e38
Joshua Manchester <JManch@protonmail.com>
2025-05-17 22:20:35 +0100

    flake lock: update all

--- flake.nix
+++ flake.nix
@@ -34,8 +34,7 @@
     # Critical inputs that provide imported NixOS modules or overlays. Ideally
     # should review changes after updating.
 
-    # FIX: registry broke https://github.com/NixOS/nix/issues/13104
-    nixpkgs.url = "github:NixOS/nixpkgs/c11863f1e964833214b767f4a369c6e6a7aba141";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-broadcast-box.url = "github:JManch/nixpkgs/broadcast-box";
 
     impermanence.url = "github:nix-community/impermanence";

858e30c3a157e340be2e65f6f96ab59350c44484
Joshua Manchester <JManch@protonmail.com>
2025-05-30 00:59:15 +0100

    music: add script for unzipping to homelab

--- modules/home-manager/programs/desktop/music.nix
+++ modules/home-manager/programs/desktop/music.nix
@@ -2,15 +2,41 @@
   lib,
   pkgs,
   config,
+  selfPkgs,
 }:
 let
-  inherit (config.${lib.ns}.desktop) style hyprland;
+  inherit (lib) ns mkIf;
+  inherit (lib.${ns}) sshAddQuiet;
+  inherit (config.${ns}.desktop) style hyprland;
 in
 {
-  home.packages = with pkgs; [
-    picard
-    spek
+  home.packages = [
+    pkgs.picard
+    pkgs.spek
+    selfPkgs.resample-flacs
   ];
+
+  programs.zsh.initContent =
+    mkIf (config.home.username == "joshua") # bash
+      ''
+        unzip-music-homelab() {
+          mkdir -p ~/homelab-{slskd-downloads,music}
+          if ! mount -l | grep -q homelab-slskd-downloads; then
+            ${sshAddQuiet pkgs}
+            sshfs joshua@homelab.lan:/persist/media/slskd/downloads /home/joshua/homelab-slskd-downloads
+          fi
+
+          if ! mount -l | grep -q homelab-music; then
+            ${sshAddQuiet pkgs}
+            sshfs joshua@homelab.lan:/persist/media/music /home/joshua/homelab-music
+          fi
+
+          tmp=$(mktemp -d)
+          unzip "$1" -d "$tmp"
+          resample-flacs "$tmp"
+          mv "$tmp" ~/homelab-slskd-downloads/''${1%.*}
+        }
+      '';
 
   ns.desktop.hyprland.settings = {
     workspace = [

f32f5b777b939acf4c8f41fb5b07f81b0787e246
Joshua Manchester <JManch@protonmail.com>
2025-06-21 01:26:43 +0100

    waydroid: add hyprland window rules

--- modules/nixos/programs/waydroid.nix
+++ modules/nixos/programs/waydroid.nix
@@ -1,6 +1,17 @@
-{ lib, cfg }:
+{
+  lib,
+  cfg,
+  config,
+}:
 let
-  inherit (lib) mkForce optional mkEnableOption;
+  inherit (lib)
+    ns
+    mkIf
+    mkForce
+    optional
+    mkEnableOption
+    ;
+  inherit (config.${ns}.core) home-manager;
 in
 {
   opts.autoStart = mkEnableOption "Waydroid auto start";
@@ -9,6 +20,14 @@ in
   systemd.services."waydroid-container".wantedBy = mkForce (
     optional cfg.autoStart "multi-user.target"
   );
+
+  ns.hm = mkIf home-manager.enable {
+    ${ns}.desktop.hyprland.settings.windowrule = [
+      "float, class:^(Waydroid)$"
+      "center, class:^(Waydroid)$"
+      "keepaspectratio, class:^(Waydroid)$"
+    ];
+  };
 
   ns.persistence.directories = [ "/var/lib/waydroid" ];
   ns.persistenceHome.directories = [ ".local/share/waydroid" ];

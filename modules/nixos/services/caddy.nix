342da5e42fae8e000ca9b9cb9f3a6391587d0d2a
Joshua Manchester <JManch@protonmail.com>
2025-07-01 14:10:50 +0100

    flake lock: update all

--- modules/nixos/services/caddy.nix
+++ modules/nixos/services/caddy.nix
@@ -201,7 +201,7 @@ in
 
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/porkbun@v0.3.1" ];
-      hash = "sha256-7TqepCX9F5AMAUJrH8wxdnrr3JMezhowyIPlfFYUQG8=";
+      hash = "sha256-YZ4Bq0hfOJpa0C2lKipEY4fqwzJbEFM7ci5ys9S3uAo=";
     };
 
     globalConfig = ''

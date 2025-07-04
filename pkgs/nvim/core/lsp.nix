27adc4928f67f62106da0313b09b38ff4ba7b204
Joshua Manchester <JManch@protonmail.com>
2025-07-04 23:44:19 +0100

    nvim: configure lsp mappings

--- pkgs/nvim/core/lsp.nix
+++ pkgs/nvim/core/lsp.nix
@@ -3,27 +3,41 @@
     enable = true;
     formatOnSave = true;
     mappings = {
-      # TODO: Configure these
-      goToDeclaration = null;
-      goToDefinition = null;
-      goToType = null;
-      listImplementations = null;
-      listReferences = null;
-      nextDiagnostic = null;
-      previousDiagnostic = null;
-      openDiagnosticFloat = null;
+      goToDeclaration = "<LEADER>lD";
+      goToDefinition = "<LEADER>ld";
+      goToType = "<LEADER>lt";
+      listImplementations = "<LEADER>lm";
+      listReferences = "<LEADER>lr";
+      openDiagnosticFloat = "gl";
+      nextDiagnostic = "]d";
+      previousDiagnostic = "[d";
       documentHighlight = null;
       listDocumentSymbols = null;
       addWorkspaceFolder = null;
       removeWorkspaceFolder = null;
       listWorkspaceFolders = null;
       listWorkspaceSymbols = null;
-      hover = null;
-      signatureHelp = null;
-      renameSymbol = null;
-      codeAction = null;
-      format = null;
-      toggleFormatOnSave = null;
+      hover = "<LEADER>lH";
+      signatureHelp = "<LEADER>lh";
+      renameSymbol = "<LEADER>lR";
+      codeAction = "<LEADER>la";
+      format = "<LEADER>lf";
+      toggleFormatOnSave = "<LEADER>lF";
+    };
+  };
+
+  vim.diagnostics = {
+    enable = true;
+    config = {
+      virtual_text = true;
+      severity_sort = true;
+      float.source = true;
+      signs.text = {
+        "vim.diagnostic.severity.ERROR" = "󰅚 ";
+        "vim.diagnostic.severity.WARN" = "󰀪 ";
+        "vim.diagnostic.severity.INFO" = "󰋽 ";
+        "vim.diagnostic.severity.HINT" = "󰌶 ";
+      };
     };
   };
 }

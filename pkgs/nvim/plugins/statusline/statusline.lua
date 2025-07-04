2512c13ef141bbd225b720ccd3a9edc3e70d8f9c
Joshua Manchester <JManch@protonmail.com>
2025-07-04 01:36:44 +0100

    nvim: implement absolute filename centering in statusline

--- pkgs/nvim/plugins/statusline/statusline.lua
+++ pkgs/nvim/plugins/statusline/statusline.lua
@@ -1,26 +1,54 @@
 -- TODO:
--- Fix git update opening file
 -- Fix searchcount update when clearing search
-local setup_colors = function()
-  return require("ayu.colors")
-end
+local setup_colors = function() return require("ayu.colors") end
 local conditions = require("heirline.conditions")
 local utils = require("heirline.utils")
 
 require("heirline").load_colors(setup_colors)
 
-vim.api.nvim_create_augroup("Heirline", { clear = true })
+vim.api.nvim_create_augroup("Heirline", {clear = true})
 vim.api.nvim_create_autocmd("ColorScheme", {
   group = "Heirline",
-  callback = function()
-    utils.on_colorscheme(setup_colors)
-  end,
+  callback = function() utils.on_colorscheme(setup_colors) end
 })
 
-local align = { provider = "%=" }
-local space = { provider = " " }
+local left_char_count = {}
 
-local vi_mode = {
+local count_provider = function(name, provider)
+  return function(self)
+    local result
+    if type(provider) == "function" then
+      result = provider(self)
+    else
+      result = provider
+    end
+
+    if not result then
+      left_char_count[name] = 0
+    else
+      left_char_count[name] = utils.count_chars(result)
+    end
+    return result
+  end
+end
+
+local count_condition = function(names, condition)
+  return function(self)
+    local result = condition(self)
+    if not result then
+      if type(names) == "string" then
+        left_char_count[names] = 0
+      else
+        for _, v in pairs(names) do left_char_count[v] = 0 end
+      end
+    end
+    return result
+  end
+end
+
+local space = {provider = " "}
+
+local mode = {
   static = {
     mode_names = {
       ["n"] = "NORMAL",
@@ -59,176 +87,151 @@ local vi_mode = {
       ["rm"] = "MORE",
       ["r?"] = "CONFIRM",
       ["!"] = "SHELL",
-      ["t"] = "TERMINAL",
-    },
+      ["t"] = "TERMINAL"
+    }
   },
 
-  provider = function(self)
+  provider = count_provider("mode", function(self)
     return " " .. self.mode_names[vim.fn.mode(1)] .. " "
-  end,
+  end),
 
-  hl = function(self)
-    return self:mode_highlight()
-  end,
+  hl = function(self) return self:mode_highlight() end
 
-  update = {
-    "ModeChanged",
-    pattern = "*:*",
-    callback = vim.schedule_wrap(function()
-      vim.cmd("redrawstatus")
-    end),
-  },
+  -- Causes mode to get stuck on "TERMINAL" after using fzf-lua
+  -- update = {
+  --   "ModeChanged",
+  --   pattern = "*:*",
+  --   callback = vim.schedule_wrap(function() vim.cmd("redrawstatus") end)
+  -- }
 }
 
 local git = {
-  condition = conditions.is_git_repo,
-  init = function(self)
-    self.status_dict = vim.b.gitsigns_status_dict
-    self.has_changes = self.status_dict.added ~= 0 or self.status_dict.removed ~= 0 or self.status_dict.changed ~= 0
-  end,
-  hl = { fg = "entity" },
+  condition = count_condition({
+    "git_branch", "git_added", "git_removed", "git_modified"
+  }, conditions.is_git_repo),
+  init = function(self) self.status_dict = vim.b.gitsigns_status_dict end,
+  hl = {fg = "entity"},
   {
-    provider = function(self)
-      return " " .. self.status_dict.head
-    end,
-    hl = function(self)
-      return { fg = self:mode_highlight().bg }
-    end,
+    provider = count_provider("git_branch", function(self)
+      return "  " .. self.status_dict.head
+    end),
+    hl = function(self) return {fg = self:mode_highlight().bg} end
   },
   {
-    provider = function(self)
+    provider = count_provider("git_added", function(self)
       local count = self.status_dict.added or 0
-      return count > 1 and (" +" .. count)
-    end,
-    hl = { fg = "vcs_added" },
+      return count > 0 and (" +" .. count)
+    end),
+    hl = {fg = "vcs_added"}
   },
   {
-    provider = function(self)
+    provider = count_provider("git_removed", function(self)
       local count = self.status_dict.removed or 0
       return count > 0 and (" -" .. count)
-    end,
-    hl = { fg = "vcs_removed" },
+    end),
+    hl = {fg = "vcs_removed"}
   },
   {
-    provider = function(self)
+    provider = count_provider("git_modified", function(self)
       local count = self.status_dict.changed or 0
       return count > 0 and (" ~" .. count)
-    end,
-    hl = { fg = "vcs_modified" },
-  },
+    end),
+    hl = {fg = "vcs_modified"}
+  }
 }
 
-local file_name_block = {
+local filename_block = {
+  -- Calculate stuff for children here so we can predict padding in this eval.
+  -- Otherwise one eval cycling has incorrect padding and it causes stuff to
+  -- jump around. 
   init = function(self)
-    self.filename = vim.api.nvim_buf_get_name(0)
+    local filename = vim.api.nvim_buf_get_name(0)
+
+    local extension = vim.fn.fnamemodify(filename, ":e")
+    self.icon, self.icon_color = require("nvim-web-devicons").get_icon_color(
+                                     filename, extension, {default = true})
+    self.icon_str = self.icon and (self.icon .. " ")
+
+    self.lfilename = vim.fn.fnamemodify(filename, ":.")
+    if self.lfilename == "" then self.lfilename = "[No Name]" end
+
+    if vim.bo.modified then
+      self.tag_str = " [+]"
+    elseif not vim.bo.modifiable or vim.bo.readonly then
+      self.tag_str = " [-]"
+    else
+      self.tag_str = ""
+    end
+
+    self.filename_chars = utils.count_chars(
+                              self.icon_str .. self.lfilename .. self.tag_str)
   end,
-  provider = function()
-    return "%="
-  end,
+  {provider = count_provider("filename_space", " ")},
   {
-    init = function(self)
-      local filename = self.filename
-      local extension = vim.fn.fnamemodify(filename, ":e")
-      self.icon, self.icon_color =
-      require("nvim-web-devicons").get_icon_color(filename, extension, { default = true })
-    end,
     provider = function(self)
-      return self.icon and (self.icon .. " ")
-    end,
-    hl = function(self)
-      return { fg = self.icon_color }
-    end,
+      local left_chars = 0
+      for _, v in pairs(left_char_count) do left_chars = left_chars + v end
+      local right_chars = vim.o.columns - (left_chars + self.filename_chars)
+
+      -- Because we left align the filename by default right_chars will always
+      -- be >= left_chars
+      return string.rep(' ', math.floor((right_chars - left_chars) / 2))
+    end
   },
   {
-    init = function(self)
-      self.lfilename = vim.fn.fnamemodify(self.filename, ":.")
-      if self.lfilename == "" then
-        self.lfilename = "[No Name]"
-      end
-    end,
-    flexible = 2,
-    {
-      provider = function(self)
-        return self.lfilename
-      end,
-    },
-    {
-      provider = function(self)
-        return vim.fn.pathshorten(self.lfilename)
-      end,
-    },
+    provider = function(self) return self.icon_str end,
+    hl = function(self) return {fg = self.icon_color} end
   },
-  {
-    condition = function()
-      return vim.bo.modified
-    end,
-    provider = " [+]",
-  },
-  {
-    condition = function()
-      return not vim.bo.modifiable or vim.bo.readonly
-    end,
-    provider = " [-]",
-  },
+  {provider = function(self) return self.lfilename .. self.tag_str end}
 }
 
 local search_count = {
-  condition = function()
-    return vim.v.hlsearch ~= 0
-  end,
+  condition = function() return vim.v.hlsearch ~= 0 end,
 
-  hl = function(self)
-    return { fg = self:mode_highlight().bg }
-  end,
+  hl = function(self) return {fg = self:mode_highlight().bg} end,
 
   provider = function()
-    local ok, result = pcall(vim.fn.searchcount, { maxcount = 999, timeout = 500 })
-    if not ok or next(result) == nil then
-      return ""
-    end
+    local ok, result =
+        pcall(vim.fn.searchcount, {maxcount = 999, timeout = 500})
+    if not ok or next(result) == nil then return "" end
     local denominator = math.min(result.total, result.maxcount)
     return string.format("[%d/%d]", result.current, denominator)
-  end,
+  end
 }
 
 local position = {
   provider = " %P ",
-  hl = function(self)
-    return self:mode_highlight()
-  end,
+  hl = function(self) return self:mode_highlight() end
 }
 
 local statusline = {
-  vi_mode,
-  space,
-  git,
-  file_name_block,
-  align,
-  search_count,
-  space,
-  position,
-  hl = { fg = "fg", bg = "panel_border" },
+  hl = {fg = "fg", bg = "panel_border"},
   static = {
     mode_highlights_map = {
-      n = { fg = "bg", bg = "entity", bold = true },
-      i = { fg = "bg", bg = "string", bold = true },
-      v = { fg = "bg", bg = "accent", bold = true },
-      ["\22"] = { fg = "bg", bg = "accents", bold = true },
-      c = { fg = "bg", bg = "constant", bold = true },
-      s = { fg = "bg", bg = "accent", bold = true },
-      ["\19"] = { fg = "bg", bg = "accent", bold = true },
-      r = { fg = "bg", bg = "markup", bold = true },
-      ["!"] = { fg = "bg", bg = "string", bold = true },
-      t = { fg = "bg", bg = "string", bold = true },
+      n = {fg = "bg", bg = "entity", bold = true},
+      i = {fg = "bg", bg = "string", bold = true},
+      v = {fg = "bg", bg = "accent", bold = true},
+      ["\22"] = {fg = "bg", bg = "accent", bold = true},
+      c = {fg = "bg", bg = "constant", bold = true},
+      s = {fg = "bg", bg = "accent", bold = true},
+      ["\19"] = {fg = "bg", bg = "accent", bold = true},
+      r = {fg = "bg", bg = "markup", bold = true},
+      ["!"] = {fg = "bg", bg = "string", bold = true},
+      t = {fg = "bg", bg = "string", bold = true}
     },
 
     mode_highlight = function(self)
       local mode = conditions.is_active() and vim.fn.mode():lower() or "n"
       return self.mode_highlights_map[mode]
-    end,
+    end
   },
+  mode,
+  git,
+  filename_block,
+  {provider = "%="},
+  search_count,
+  space,
+  position
 }
 
-require("heirline").setup({
-  statusline = statusline,
-})
+require("heirline").setup({statusline = statusline})

ca7e6919e9a389918d3e873c64327f66388585a3
Joshua Manchester <JManch@protonmail.com>
2025-07-05 00:08:08 +0100

    color-scheme: improve ayu blue

--- modules/home-manager/core/color-scheme.nix
+++ modules/home-manager/core/color-scheme.nix
@@ -12,7 +12,7 @@
 # base0A: "#FFD173"
 # base0B: "#D5FF80"
 # base0C: "#95E6CB"
-# base0D: "#5CCFE6"
+# base0D: "#73D0FF"
 # base0E: "#D4BFFF"
 # base0F: "#F29E74"
 
@@ -54,7 +54,26 @@ in
   opts = {
     dark = mkOption {
       type = types.attrs;
-      default = inputs.nix-colors.colorSchemes.ayu-mirage;
+      default = inputs.nix-colors.colorSchemes.ayu-mirage // {
+        palette = {
+          base00 = "171B24";
+          base01 = "1F2430";
+          base02 = "242936";
+          base03 = "707A8C";
+          base04 = "8A9199";
+          base05 = "CCCAC2";
+          base06 = "D9D7CE";
+          base07 = "F3F4F5";
+          base08 = "F28779";
+          base09 = "FFAD66";
+          base0A = "FFD173";
+          base0B = "D5FF80";
+          base0C = "95E6CB";
+          base0D = "73D0FF";
+          base0E = "D4BFFF";
+          base0F = "F29E74";
+        };
+      };
       description = "Dark color scheme";
     };
 

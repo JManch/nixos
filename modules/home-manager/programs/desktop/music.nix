79733e864a6c7b39432303ea0ec8f8f3b2efd1ee
Joshua Manchester <JManch@protonmail.com>
2025-07-18 00:47:17 +0100

    music: move unzip script into shell application

--- modules/home-manager/programs/desktop/music.nix
+++ modules/home-manager/programs/desktop/music.nix
@@ -5,8 +5,34 @@
   inputs,
 }:
 let
-  inherit (lib) ns mkIf;
+  inherit (lib) ns optional;
   inherit (config.${ns}.desktop) style hyprland;
+
+  unzipMusicHomelab = pkgs.writeShellApplication {
+    name = "unzip-music-homelab";
+    runtimeInputs = [
+      pkgs.sshfs
+      pkgs.gnugrep
+      pkgs.util-linux
+      pkgs.unzip
+      pkgs.${ns}.resample-flacs
+    ];
+    text = ''
+      mkdir -p ~/homelab-{slskd-downloads,music}
+      if ! mount -l | grep -q homelab-slskd-downloads; then
+        sshfs joshua@homelab.lan:/persist/media/slskd/downloads /home/joshua/homelab-slskd-downloads
+      fi
+
+      if ! mount -l | grep -q homelab-music; then
+        sshfs joshua@homelab.lan:/persist/media/music /home/joshua/homelab-music
+      fi
+
+      tmp=$(mktemp -d)
+      unzip "$1" -d "$tmp"
+      resample-flacs "$tmp"
+      mv "$tmp" "$HOME/homelab-slskd-downloads/''${1%.*}"
+    '';
+  };
 in
 {
   home.packages =
@@ -17,27 +43,8 @@ in
       pkgs.picard
       # pkgs.spek
       pkgs.${ns}.resample-flacs
-    ];
-
-  programs.zsh.initContent =
-    mkIf (config.home.username == "joshua") # bash
-      ''
-        unzip-music-homelab() {
-          mkdir -p ~/homelab-{slskd-downloads,music}
-          if ! mount -l | grep -q homelab-slskd-downloads; then
-            sshfs joshua@homelab.lan:/persist/media/slskd/downloads /home/joshua/homelab-slskd-downloads
-          fi
-
-          if ! mount -l | grep -q homelab-music; then
-            sshfs joshua@homelab.lan:/persist/media/music /home/joshua/homelab-music
-          fi
-
-          tmp=$(mktemp -d)
-          unzip "$1" -d "$tmp"
-          resample-flacs "$tmp"
-          mv "$tmp" ~/homelab-slskd-downloads/''${1%.*}
-        }
-      '';
+    ]
+    ++ optional (config.home.username == "joshua") unzipMusicHomelab;
 
   ns.desktop.hyprland.settings = {
     workspace = [

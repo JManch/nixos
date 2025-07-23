67ab7ab9161367d83922cca1aab0a85f27c82930
Joshua Manchester <JManch@protonmail.com>
2025-07-23 21:43:59 +0100

    images: configure swayimg

--- modules/home-manager/programs/desktop/images.nix
+++ modules/home-manager/programs/desktop/images.nix
@@ -3,11 +3,38 @@
   pkgs,
   config,
 }:
+let
+  inherit (config.${lib.ns}) desktop;
+in
 {
   home.packages = with pkgs; [
     swayimg
     gthumb # image editor
   ];
+
+  xdg.configFile."swayimg/config".source = (pkgs.formats.ini { }).generate "config" {
+    general.compositor = false;
+    font.name = desktop.style.font.family;
+
+    "keys.viewer" = {
+      Left = "prev_file";
+      Right = "next_file";
+      f = "fullscreen";
+      plus = "zoom +10";
+      underscore = "zoom -10";
+      ScrollUp = "zoom +5";
+      ScrollDown = "zoom -5";
+      r = "zoom optimal";
+      less = "rotate_left";
+      greater = "rotate_right";
+      x = "flip_vertical";
+      z = "flip_horizontal";
+      "Ctrl+r" = "reload";
+      i = "info viewer";
+      q = "exit";
+      question = "help";
+    };
+  };
 
   programs.zsh.shellAliases = {
     img = "swayimg";

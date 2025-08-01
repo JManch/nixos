ba439716d46dcd836bd8d505f239f497024d60fe
Joshua Manchester <JManch@protonmail.com>
2025-08-01 12:22:28 +0100

    starship: do not use home-manager module

--- modules/home-manager/programs/shell/starship.nix
+++ modules/home-manager/programs/shell/starship.nix
@@ -1,49 +1,57 @@
-{ lib, config }:
+{
+  lib,
+  pkgs,
+  config,
+}:
 let
   inherit (config.${lib.ns}.programs.shell) promptColor;
 in
 {
   enableOpt = false;
 
-  programs.starship = {
-    enable = true;
+  home.packages = [ pkgs.starship ];
 
-    settings = {
-      add_newline = false;
-      format = "$directory$character";
-      right_format = "$all";
-      memory_usage.disabled = true;
-      hostname.format = "$hostname ";
-      username.format = "$user@";
+  xdg.configFile."starship.toml".source = (pkgs.formats.toml { }).generate "starship-config" {
+    add_newline = false;
+    format = "$directory$character";
+    right_format = "$all";
+    memory_usage.disabled = true;
+    hostname.format = "$hostname ";
+    username.format = "$user@";
 
-      character = {
-        success_symbol = "[❯](${promptColor})";
-        error_symbol = "[❯](red)";
-        vimcmd_symbol = "[❮](${promptColor})";
-        vimcmd_replace_one_symbol = "[❮](purple)";
-        vimcmd_replace_symbol = "[❮](purple)";
-        vimcmd_visual_symbol = "[❮](yellow)";
-      };
+    character = {
+      success_symbol = "[❯](${promptColor})";
+      error_symbol = "[❯](red)";
+      vimcmd_symbol = "[❮](${promptColor})";
+      vimcmd_replace_one_symbol = "[❮](purple)";
+      vimcmd_replace_symbol = "[❮](purple)";
+      vimcmd_visual_symbol = "[❮](yellow)";
+    };
 
-      directory = {
-        format = "[$path]($style)[$read_only]($read_only_style) ";
-        style = "cyan";
-      };
+    directory = {
+      format = "[$path]($style)[$read_only]($read_only_style) ";
+      style = "cyan";
+    };
 
-      git_metrics = {
-        disabled = false;
-        added_style = "green";
-        deleted_style = "red";
-      };
+    git_metrics = {
+      disabled = false;
+      added_style = "green";
+      deleted_style = "red";
+    };
 
-      git_status = {
-        stashed = "";
-        ahead = "󱦲";
-        behind = "󱦳";
-        diverged = "󰹺";
-      };
+    git_status = {
+      stashed = "";
+      ahead = "󱦲";
+      behind = "󱦳";
+      diverged = "󰹺";
     };
   };
+
+  programs.zsh.initContent = ''
+    if [[ $TERM != "dumb" ]]; then
+      eval "$(${lib.getExe pkgs.starship} init zsh)"
+    fi
+  '';
 
   ns.persistence.directories = [ ".cache/starship" ];
 }

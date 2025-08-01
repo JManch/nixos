d52a4fdb8e25298b5940d2472a2319ea515a14dc
Joshua Manchester <JManch@protonmail.com>
2025-08-01 14:23:05 +0100

    obs: drop whep source patch and do not use hm module

--- modules/home-manager/programs/desktop/obs.nix
+++ modules/home-manager/programs/desktop/obs.nix
@@ -1,26 +1,10 @@
 { lib, pkgs }:
 {
-  programs.obs-studio = {
-    enable = true;
-    plugins = [ pkgs.obs-studio-plugins.obs-pipewire-audio-capture ];
-
-    package =
-      lib.${lib.ns}.addPatches
-        # Have to use obs 30.2.3 as the OBS WHEP source patch has not been rebased onto latest version
-        (import (fetchTree "github:NixOS/nixpkgs/028048884dc9517e548703beb24a11408cc51402") {
-          inherit (pkgs) system;
-        }).obs-studio
-        [
-          # https://github.com/obsproject/obs-studio/pull/10353
-          "obs-whep-source.patch"
-          (pkgs.fetchpatch {
-            name = "webrtc-pacing-handler";
-            # https://github.com/obsproject/obs-studio/pull/10966
-            url = "https://github.com/obsproject/obs-studio/commit/2aae0f4d6849a7c23f44760f548e13f3a307426b.patch";
-            hash = "sha256-VKNA0N4JHat/tLSRk3CLY+NcHsIoIvRHYfHo572KSGk=";
-          })
-        ];
-  };
+  home.packages = lib.singleton (
+    pkgs.wrapOBS.override { obs-studio = pkgs.obs-studio; } {
+      plugins = [ pkgs.obs-studio-plugins.obs-pipewire-audio-capture ];
+    }
+  );
 
   ns.persistence.directories = [ ".config/obs-studio" ];
 }

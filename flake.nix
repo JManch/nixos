c16210b2c628a4af967478298083fe55f36ff37b
Joshua Manchester <JManch@protonmail.com>
2025-08-01 14:10:04 +0100

    flake: unpin broadcast-box nixpkgs

--- flake.nix
+++ flake.nix
@@ -126,7 +126,7 @@
     mint.url = "github:trumank/mint";
 
     broadcast-box.url = "github:JManch/broadcast-box/discord";
-    broadcast-box.inputs.nixpkgs.follows = "nixpkgs";
+    # broadcast-box.inputs.nixpkgs.follows = "nixpkgs"; broken for some reason
 
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";

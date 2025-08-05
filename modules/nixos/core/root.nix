4c747bc8bf76e7951dfdf422d98b0f22a0330e2e
Joshua Manchester <JManch@protonmail.com>
2025-08-05 16:03:31 -0400

    timezone: enable tzupdate service on laptops

--- modules/nixos/core/root.nix
+++ modules/nixos/core/root.nix
@@ -2,9 +2,18 @@
   lib,
   cfg,
   pkgs,
+  config,
 }:
 let
-  inherit (lib) ns mkOption types;
+  inherit (lib)
+    ns
+    mkIf
+    mkOption
+    mkDefault
+    mkForce
+    types
+    ;
+  inherit (config.${ns}.core) device;
 in
 {
   opts.namespace = mkOption {
@@ -28,7 +37,17 @@ in
     inherit (cfg.users) adminUsername;
   };
 
-  time.timeZone = "Europe/London";
+  time.timeZone = mkDefault "Europe/London";
+
+  services.tzupdate = {
+    enable = device.type == "laptop";
+    timer.enable = false;
+  };
+
+  # Would rather run service manually when we are away from home
+  systemd.services.tzupdate = mkIf (device.type == "laptop") {
+    wantedBy = mkForce [ ];
+  };
 
   environment.sessionVariables = {
     XDG_CACHE_HOME = "$HOME/.cache";

598ed5f2616a90e5140792ab6ece83b1639e8ff9
Joshua Manchester <JManch@protonmail.com>
2025-08-27 23:40:47 +0100

    filen-desktop: remove unneeded windowrules

--- modules/home-manager/programs/desktop/filen-desktop.nix
+++ modules/home-manager/programs/desktop/filen-desktop.nix
@@ -1,12 +1,5 @@
 { pkgs }:
 {
   home.packages = [ pkgs.filen-desktop ];
-
-  ns.desktop.hyprland.settings.windowrule = [
-    "nomaxsize, class:^(filen-desktop)$"
-    # The progress window instantly closes as soon as it loses focus
-    "stayfocused, class:^(filen-desktop)$"
-  ];
-
   ns.persistence.directories = [ ".config/filen-desktop" ];
 }

27b2009c993a152749522f28285965c127a4fe57
Joshua Manchester <JManch@protonmail.com>
2025-09-23 23:14:24 +0100

    caddy: update plugin hash

--- modules/nixos/services/caddy.nix
+++ modules/nixos/services/caddy.nix
@@ -201,7 +201,7 @@ in
 
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/porkbun@v0.3.1" ];
-      hash = "sha256-g/Nmi4X/qlqqjY/zoG90iyP5Y5fse6Akr8exG5Spf08=";
+      hash = "sha256-PUHu+KPywdJMuPLHPtQhUaw3Cv1pED5XQ1MOzlT/6h4=";
     };
 
     globalConfig = ''

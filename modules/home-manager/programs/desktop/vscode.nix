76288d8ef288a9eba9344b1503d9fbe2dbb4480e
Joshua Manchester <JManch@protonmail.com>
2025-09-26 23:59:13 +0100

    vscode: configure imperatively

--- modules/home-manager/programs/desktop/vscode.nix
+++ modules/home-manager/programs/desktop/vscode.nix
@@ -1,14 +1,7 @@
 # NOTE: To fix credential saving (signing in with github) need to add
 # "password-store": "gnome" to ~/.vscode/argv.json
 # https://code.visualstudio.com/docs/editor/settings-sync#_troubleshooting-keychain-issues
-{
-  lib,
-  pkgs,
-  config,
-}:
-let
-  inherit (config.${lib.ns}) desktop;
-in
+{ pkgs }:
 {
   programs.vscode = {
     enable = true;
@@ -17,18 +10,12 @@ in
     # environments. Solution is to run nix shell inside the vscode terminal.
     # Also, some tooling such as LSP servers may need to be added here. I don't
     # use VSCode as my main editor though so not a big deal.
-    package = pkgs.vscode.fhsWithPackages (ps: with ps; [ gdb ]);
-
-    profiles.default.userSettings = {
-      # Prevents wayland crash on launch
-      "window.titleBarStyle" = "custom";
-      "window.menuBarVisibility" = "toggle";
-      "editor.fontFamily" = desktop.style.font.family;
-      "git.autofetch" = true;
-      "workbench.colorTheme" = "Ayu Mirage Bordered";
-      "cmake.showOptionsMovedNotification" = false;
-      "cmake.configureOnOpen" = false;
-    };
+    package = pkgs.vscode.fhsWithPackages (
+      ps: with ps; [
+        corretto21
+        gdb
+      ]
+    );
   };
 
   ns.persistence.directories = [

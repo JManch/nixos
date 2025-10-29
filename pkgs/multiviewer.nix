cb28e9d0b1fbe582dd9e24dcbf28090d4ebae215
Joshua Manchester <JManch@protonmail.com>
2025-10-29 10:50:02 +0000

    multiviewer: update package

--- pkgs/multiviewer.nix
+++ pkgs/multiviewer.nix
@@ -4,16 +4,15 @@
   fetchurl,
   libudev0-shim,
   libglvnd,
-  icoutils,
 }:
 multiviewer-for-f1.overrideAttrs (
   final: prev: {
     pname = "multiviewer";
-    version = "2.2.1";
+    version = "2.3.0";
 
     src = fetchurl {
-      url = "https://releases.multiviewer.app/download/295039426/multiviewer_${final.version}_amd64.deb";
-      hash = "sha256-y40LJVjnkvIxElDooF5xNmpkeVCRRS30o/nvqZnmH+I=";
+      url = "https://releases.multiviewer.app/download/305607196/multiviewer_${final.version}_amd64.deb";
+      hash = "sha256-Uc4db2o4XBV9eRNugxS6pA9Z5YhjY5QnEkwOICXmUwc=";
     };
 
     # Add libglvnd to library path for hardware acceleration
@@ -24,21 +23,16 @@ multiviewer-for-f1.overrideAttrs (
       mv -t $out/share usr/share/* usr/lib/multiviewer
 
       makeWrapper "$out/share/multiviewer/multiviewer" $out/bin/multiviewer \
-      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}" \
-      --prefix LD_LIBRARY_PATH : "${
-        lib.makeLibraryPath [
-          libudev0-shim
-          libglvnd
-        ]
-      }:\"$out/share/multiviewer\""
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}" \
+        --prefix LD_LIBRARY_PATH : "${
+          lib.makeLibraryPath [
+            libudev0-shim
+            libglvnd
+          ]
+        }:\"$out/share/multiviewer\""
 
       runHook postInstall
     '';
-
-    # Fix the icon because it uses an unreadable windows format
-    # postInstall = ''
-    #   ${lib.getExe' icoutils "icotool"} -x -w 256 -o $out/share/pixmaps/multiviewer.png $out/share/pixmaps/multiviewer.png
-    # '';
 
     meta = prev.meta // {
       mainProgram = "multiviewer";

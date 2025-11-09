8c20f3cb57c41394516b68ff128fccabe88389f6
Joshua Manchester <JManch@protonmail.com>
2025-11-09 20:06:35 +0000

    bluetooth: fix bluetui desktop icon

--- modules/nixos/hardware/bluetooth.nix
+++ modules/nixos/hardware/bluetooth.nix
@@ -36,12 +36,12 @@ in
 
   ns.hm = mkIf home-manager.enable {
     xdg.desktopEntries.bluetui = mkIf config.${ns}.hmNs.desktop.enable {
-      name = "bluetui";
+      name = "Bluetui";
       genericName = "Bluetooth Manager";
       exec = "xdg-terminal-exec --title=bluetui --app-id=bluetui bluetui";
       terminal = false;
       type = "Application";
-      icon = "application-x-generic";
+      icon = "preferences-bluetooth";
       categories = [ "System" ];
     };
 

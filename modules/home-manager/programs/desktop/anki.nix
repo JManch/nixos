aea7842c4accddff62b7086ebd5e8d28d81efc83
Joshua Manchester <JManch@protonmail.com>
2025-11-14 12:20:00 +0000

    anki: workaround hang on shutdown

--- modules/home-manager/programs/desktop/anki.nix
+++ modules/home-manager/programs/desktop/anki.nix
@@ -9,5 +9,12 @@
     };
 
     persistence.directories = [ ".local/share/Anki2" ];
+
+    # If the add card window is open anki hangs when attemping to stop the
+    # unit. SIGKILL after 10 secs instead of default 90 secs.
+    desktop.uwsm.appUnitOverrides."anki@.service" = ''
+      [Service]
+      TimeoutStopSec=10
+    '';
   };
 }

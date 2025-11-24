e86fdf42278218a520d2131265709d90b773bfa4
Joshua Manchester <JManch@protonmail.com>
2025-11-24 02:05:53 +0000

    hyprsunset: add temperature key binds

--- modules/home-manager/desktop/services/hyprsunset.nix
+++ modules/home-manager/desktop/services/hyprsunset.nix
@@ -67,7 +67,7 @@ in
     Install.WantedBy = [ "graphical-session.target" ];
   };
 
-  ns.desktop.hyprland.binds =
+  ns.desktop.hyprland.settings =
     let
       inherit (config.${ns}.desktop.hyprland) modKey;
       modifyGamma = pkgs.writeShellScript "hypr-modify-gamma" ''
@@ -82,11 +82,33 @@ in
         # Not using the hyprsunset increment/decrement functionality because it has rounding issues
         hyprctl hyprsunset gamma "$new"
         ${getExe pkgs.libnotify} --urgency=low -t 2000 \
-          -h 'string:x-canonical-private-synchronous:gamma' "Hyprsunset" "Gamma $new%"
+          -h 'string:x-canonical-private-synchronous:hyprsunset' "Hyprsunset" "Gamma $new%"
+      '';
+
+      modifyTemperature = pkgs.writeShellScript "hypr-modify-temperature" ''
+        hyprctl hyprsunset temperature "$1"
+        ${getExe pkgs.libnotify} --urgency=low -t 2000 \
+          -h 'string:x-canonical-private-synchronous:hyprsunset' "Hyprsunset" "Temperature $(hyprctl hyprsunset temperature)K"
+      '';
+
+      resetTemperature = pkgs.writeShellScript "hypr-modify-temperature" ''
+        hyprctl hyprsunset reset temperature
+        ${getExe pkgs.libnotify} --urgency=low -t 2000 \
+          -h 'string:x-canonical-private-synchronous:hyprsunset' "Hyprsunset" "Temperature reset to $(hyprctl hyprsunset temperature)K"
       '';
     in
-    [
-      "${modKey}, XF86MonBrightnessUp, exec, ${modifyGamma} 5"
-      "${modKey}, XF86MonBrightnessDown, exec, ${modifyGamma} -5"
-    ];
+    {
+      bind = [
+        "${modKey}, XF86MonBrightnessUp, exec, ${modifyGamma} 5"
+        "${modKey}, XF86MonBrightnessDown, exec, ${modifyGamma} -5"
+        "${modKey}SHIFT, XF86MonBrightnessUp, exec, ${modifyTemperature} +200"
+        "${modKey}SHIFT, XF86MonBrightnessDown, exec, ${modifyTemperature} -200"
+      ];
+
+      bindo = [
+        # Reset temperature with long press
+        "${modKey}SHIFT, XF86MonBrightnessUp, exec, ${resetTemperature}"
+        "${modKey}SHIFT, XF86MonBrightnessDown, exec, ${resetTemperature}"
+      ];
+    };
 }

4d88c8fa2c3b02eac7b731b4277630bf0b15cc18
Joshua Manchester <JManch@protonmail.com>
2025-11-28 22:53:05 +0000

    ssh: disable visual host key

--- modules/nixos/system/ssh.nix
+++ modules/nixos/system/ssh.nix
@@ -89,35 +89,33 @@ in
     startAgent = cfg.agent.enable && !desktop.enable;
     agentTimeout = null;
     pubkeyAcceptedKeyTypes = [ "ssh-ed25519" ];
-    extraConfig = ''
-      VisualHostKey yes
-    ''
-    + optionalString cfg.agent.enable ''
-      AddKeysToAgent yes
-    ''
-    + optionalString (username == "joshua") (
+    extraConfig =
+      optionalString cfg.agent.enable ''
+        AddKeysToAgent yes
       ''
-        Host uni
-          HostName ${inputs.nix-resources.secrets.uniSSHHostname}
-          User ${inputs.nix-resources.secrets.uniUsername}
-          # The server is misconfigured and doesn't advertise ed25519 cert support
-          PubkeyAcceptedAlgorithms +ssh-ed25519-cert-v01@openssh.com
-      ''
-      # Enable agent forwarding on specific personal hosts
-      +
-        concatMapStrings
-          (
-            host:
-            optionalString (host != hostname && cfg.agent.enable) ''
-              Host ${host}*.lan
-                ForwardAgent yes
-            ''
-          )
-          [
-            "ncase-m1"
-            "framework"
-          ]
-    );
+      + optionalString (username == "joshua") (
+        ''
+          Host uni
+            HostName ${inputs.nix-resources.secrets.uniSSHHostname}
+            User ${inputs.nix-resources.secrets.uniUsername}
+            # The server is misconfigured and doesn't advertise ed25519 cert support
+            PubkeyAcceptedAlgorithms +ssh-ed25519-cert-v01@openssh.com
+        ''
+        # Enable agent forwarding on specific personal hosts
+        +
+          concatMapStrings
+            (
+              host:
+              optionalString (host != hostname && cfg.agent.enable) ''
+                Host ${host}*.lan
+                  ForwardAgent yes
+              ''
+            )
+            [
+              "ncase-m1"
+              "framework"
+            ]
+      );
 
     knownHosts =
       (mapAttrs (host: _: {

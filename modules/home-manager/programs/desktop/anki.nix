f164834f1ec1654e1ba109cd756357af0b3ca244
Joshua Manchester <JManch@protonmail.com>
2025-12-01 14:59:41 +0000

    anki: fix browse window rule

--- modules/home-manager/programs/desktop/anki.nix
+++ modules/home-manager/programs/desktop/anki.nix
@@ -19,7 +19,8 @@
 
     desktop.hyprland.settings.windowrule = [
       "match:class anki, match:float true, center true"
-      "match:class anki, match:title Browse.*, float true, size monitor_w*0.75 monitor_h*0.75, center true"
+      # The browse window's initial title is Anki before switching to Browse.*
+      "match:class anki, match:title Anki, float true, size monitor_w*0.75 monitor_h*0.75, center true"
     ];
   };
 }

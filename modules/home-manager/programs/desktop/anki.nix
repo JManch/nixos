de2274e265be47ea3969c1428ca698ef72e0c9b5
Joshua Manchester <JManch@protonmail.com>
2025-12-02 01:03:54 +0000

    anki: use KillMode mixed

--- modules/home-manager/programs/desktop/anki.nix
+++ modules/home-manager/programs/desktop/anki.nix
@@ -11,10 +11,10 @@
     persistence.directories = [ ".local/share/Anki2" ];
 
     # If the add card window is open anki hangs when attemping to stop the
-    # unit. SIGKILL after 10 secs instead of default 90 secs.
+    # unit since it wants the main PID to be killed first
     desktop.uwsm.appUnitOverrides."anki@.service" = ''
       [Service]
-      TimeoutStopSec=10
+      KillMode=mixed
     '';
 
     desktop.hyprland.settings.windowrule = [

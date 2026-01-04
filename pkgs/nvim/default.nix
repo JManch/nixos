a9791beffec223763e76384d3e1901620aaa716d
Joshua Manchester <JManch@protonmail.com>
2026-01-04 23:50:38 +0000

    nvim: use pkgs set from nvf nixpkgs input

--- pkgs/nvim/default.nix
+++ pkgs/nvim/default.nix
@@ -1,10 +1,15 @@
 {
-  pkgs,
   self,
+  pkgs,
   sources,
 }:
 self.inputs.nvf.lib.neovimConfiguration {
-  inherit pkgs;
+  # For maximum stability, rather than using our flake's primary nixpkgs pin,
+  # use the nixpkgs revision pinned to nvf. This way we avoid breakages when
+  # nixpkgs-unstable changes something e.g.
+  # https://github.com/NotAShelf/nvf/issues/1312
+  pkgs = self.inputs.nvf.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+  # inherit pkgs;
 
   extraSpecialArgs = {
     inherit sources;

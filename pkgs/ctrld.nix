996ae5497ee5d784d2f43ec86ea0944681e62e29
Joshua Manchester <JManch@protonmail.com>
2026-01-13 15:17:00 +0000

    ctrld: use 1.4.9 release

--- pkgs/ctrld.nix
+++ pkgs/ctrld.nix
@@ -2,21 +2,11 @@
   lib,
   buildGoModule,
   sources,
-  fetchFromGitHub,
 }:
 buildGoModule {
   pname = "ctrld";
-  # inherit (sources.ctrld) version;
-  version = "v1.4.9";
-  src =
-    # When removing this consider switching to v2.0.0 assuming the branch is up to date
-    assert lib.assertMsg (sources.ctrld.version == "v1.4.8") "Remove the ctrld override";
-    fetchFromGitHub {
-      owner = "Control-D-Inc";
-      repo = "ctrld";
-      rev = "3beffd0dc8701a0d3eba4030dbcb27fe919b0360";
-      hash = "sha256-FYlsnBuYgLXw2d2ppuHuDsricKYKPZ2IlSD3HjEpI/Y=";
-    };
+  inherit (sources.ctrld) version;
+  src = sources.ctrld;
 
   vendorHash = "sha256-Wydxn7tRFU3SXdY3NIlI+Welwxs1UcNHzYz5AwDI/L8=";
 

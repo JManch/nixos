f6507cc303597b8548dc7d4a52f44a8e1f4ce8c2
Joshua Manchester <JManch@protonmail.com>
2026-01-23 22:32:52 +0000

    nvim: drop deprecated treesitter options

--- pkgs/nvim/core/treesitter.nix
+++ pkgs/nvim/core/treesitter.nix
@@ -3,9 +3,7 @@
   vim.treesitter = {
     enable = true;
     autotagHtml = true;
-    incrementalSelection.enable = false;
     addDefaultGrammars = false;
     grammars = pkgs.vimPlugins.nvim-treesitter.allGrammars;
-    indent.disable = [ "nix" ];
   };
 }

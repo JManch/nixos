6fca1659da4b87fbbc91aa6e23c899ce9620c780
Joshua Manchester <JManch@protonmail.com>
2026-02-04 10:53:19 +0000

    air-vpn: add endpoint switch script

--- modules/nixos/services/air-vpn.nix
+++ modules/nixos/services/air-vpn.nix
@@ -1,6 +1,7 @@
 {
   lib,
   cfg,
+  pkgs,
   config,
   inputs,
 }:
@@ -23,6 +24,31 @@ in
       air-vpn-up = "sudo systemctl start wg-quick-air-vpn";
       air-vpn-down = "sudo systemctl stop wg-quick-air-vpn";
     };
+
+    ns.userPackages = [
+      (pkgs.writeShellApplication {
+        name = "air-vpn-switch-endpoint";
+        runtimeInputs = [ pkgs.gnused ];
+        text = ''
+          if [ "$(id -u)" != "0" ]; then
+             echo "This script must be run as root" >&2
+             exit 1
+          fi
+
+          if [[ $# -ne 1 ]]; then
+            echo "Usage: air-vpn-switch-endpoint <endpoint>" >&2
+            exit 1
+          fi
+
+          sed -i "s/^Endpoint = .*:/Endpoint = $1:/" ${airVpnConfig.path}
+          if systemctl restart air-vpn.service; then
+            echo "Endpoint successfully switched to '$1'"
+          else
+            echo "VPN failed to start with endpoint '$1'"
+          fi
+        '';
+      })
+    ];
   }
 
   (lib.mkIf cfg.confinement.enable {

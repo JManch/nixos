06e5e729bb463da32efb4033012f3f40047f4850
Joshua Manchester <JManch@protonmail.com>
2026-02-17 18:26:36 +0000

    filen-rclone: fix eval

--- pkgs/filen-rclone.nix
+++ pkgs/filen-rclone.nix
@@ -5,7 +5,13 @@
 rclone.overrideAttrs (old: {
   pname = "filen-rclone";
   version = "0-unstable-${sources.filen-rclone.revision}";
-  src = sources.filen-rclone;
+  # Due to finalAttrs pattern changein
+  # https://github.com/NixOS/nixpkgs/pull/487704, even though we override
+  # ldflags it still complains about missing tag attribute for the old
+  # ldflags??
+  src = sources.filen-rclone // {
+    tag = "";
+  };
   patches = [ ];
 
   ldflags = [

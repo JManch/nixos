bff38536295b273e4e1dc3e29d88938571d54e25
Joshua Manchester <JManch@protonmail.com>
2026-02-25 00:11:30 +0000

    font: fix missing cjk font

--- modules/home-manager/desktop/font.nix
+++ modules/home-manager/desktop/font.nix
@@ -1,7 +1,17 @@
-{ lib, config }:
+{
+  lib,
+  pkgs,
+  config,
+}:
 {
   enableOpt = false;
   fonts.fontconfig.enable = true;
-  home.packages = [ config.${lib.ns}.desktop.style.font.package ];
+
+  home.packages = [
+    config.${lib.ns}.desktop.style.font.package
+    # For Chinese, Japanese, Korean fallback in electron apps
+    pkgs.noto-fonts-cjk-sans
+  ];
+
   ns.persistence.directories = [ ".cache/fontconfig" ];
 }

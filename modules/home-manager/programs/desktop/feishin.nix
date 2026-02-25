3a03b6d3c68bfbe5dec1a37f333b4e40ded62b28
Joshua Manchester <JManch@protonmail.com>
2026-02-25 00:00:25 +0000

    feishin: patch with better notifications

--- modules/home-manager/programs/desktop/feishin.nix
+++ modules/home-manager/programs/desktop/feishin.nix
@@ -1,14 +1,17 @@
 { lib, pkgs }:
+let
+  inherit (lib) ns mkBefore;
+in
 {
   home.packages = [
     # we don't want feishin loading mpv scripts
     (pkgs.symlinkJoin {
       name = "feishin-mpv-unwrapped";
-      paths = [ pkgs.feishin ];
+      paths = [ (lib.${ns}.addPatches pkgs.feishin [ "feishin-notifications.patch" ]) ];
       nativeBuildInputs = [ pkgs.makeWrapper ];
       postBuild = ''
         wrapProgram $out/bin/feishin \
-          --prefix PATH : ${pkgs.mpv-unwrapped}/bin
+          --prefix PATH : ${pkgs.mpv-unwrapped}/bin:${pkgs.libnotify}/bin
       '';
     })
   ];
@@ -16,7 +19,7 @@
   ns.programs.desktop.music.enable = true;
 
   ns.desktop = {
-    services.playerctl.musicPlayers = lib.mkBefore [ "Feishin" ];
+    services.playerctl.musicPlayers = mkBefore [ "Feishin" ];
 
     uwsm.appUnitOverrides."feishin@.service" = ''
       [Service]

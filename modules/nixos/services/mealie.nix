2fd7309932509ea3abc79e13e285e25f58859dc0
Joshua Manchester <JManch@protonmail.com>
2026-03-02 20:10:38 +0000

    mealie: drop package override

--- modules/nixos/services/mealie.nix
+++ modules/nixos/services/mealie.nix
@@ -1,7 +1,6 @@
 {
   lib,
   cfg,
-  pkgs,
   config,
   inputs,
 }:
@@ -22,10 +21,6 @@ in
 
   services.mealie = {
     enable = true;
-    package =
-      (import (fetchTree "github:NixOS/nixpkgs/c6a788f552b7b7af703b1a29802a7233c0067908") {
-        inherit (pkgs.stdenv.hostPlatform) system;
-      }).mealie;
     listenAddress = "127.0.0.1";
     port = cfg.port;
     settings = {

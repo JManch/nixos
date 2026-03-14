d275b214be44f314f756eff41038e6b26511d6e2
Joshua Manchester <JManch@protonmail.com>
2026-03-14 11:58:41 +0000

    ssh: move external hosts to nix-resources

--- modules/nixos/system/ssh.nix
+++ modules/nixos/system/ssh.nix
@@ -149,10 +149,9 @@ in
         );
       }) (self.nixosConfigurations // self.nixOnDroidConfigurations))
       // {
-        "github.com".publicKey =
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
         "router.lan".publicKey = keys.misc.router;
-      };
+      }
+      // mapAttrs (_: v: { publicKey = v; }) keys.external-ssh-host;
   };
 
   # We want sshfs mounts to disconnect on connection loss otherwise they will

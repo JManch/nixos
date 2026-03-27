063f4a0830ea61b54956d12c21c615ecb130744e
Joshua Manchester <JManch@protonmail.com>
2026-03-27 19:22:01 +0000

    gtk: use gtk package option and explicitly disable gtk4 theming

--- modules/home-manager/desktop/programs/gtk.nix
+++ modules/home-manager/desktop/programs/gtk.nix
@@ -18,32 +18,28 @@ in
 {
   enableOpt = false;
 
-  opts.customTheme = mkEnableOption ''custom GTK icon theme and adw-gtk3 theme'' // {
+  opts.customTheme = mkEnableOption "custom GTK icon theme and adw-gtk3 theme" // {
     default = osConfig.${ns}.system.desktop.desktopEnvironment == null;
   };
-
-  home.packages = [
-    # Not generating a custom gtk.css file from our base16 colorscheme like
-    # stylix does because using gtk.css does not allow hot-reloading when
-    # switching between light and dark theme variants.
-
-    # Used to generate a gtk theme using nix colors but the theme made a bunch
-    # of apps look terrible. Sticking to adwaita seems best.
-    pkgs.adw-gtk3
-  ];
 
   gtk = {
     enable = true; # always need this enabled for cursor configuration
 
     theme = mkIf cfg.customTheme {
       name = "adw-gtk3-dark";
-      # We do not set the package here because it causes home manager to
-      # generate a gtk-4.0/gtk.css file which results in broken GTK 4 theming
-      # that does not respond to light/dark theme switches (because gtk.css
-      # themes cannot be hot-reloaded). Just gonna give up on GTK 4 theming and
-      # use Adwaita (the default) in GTK 4 apps.
-      # https://github.com/nix-community/home-manager/blob/2f23fa308a7c067e52dfcc30a0758f47043ec176/modules/misc/gtk.nix#L239
+      # Not generating a custom gtk.css file from our base16 colorscheme like
+      # stylix does because using gtk.css does not allow hot-reloading when
+      # switching between light and dark theme variants.
+
+      # Used to generate a gtk theme using nix colors but the theme made a bunch
+      # of apps look terrible. Sticking to adwaita seems best.
+      package = pkgs.adw-gtk3;
     };
+
+    # Do not theme gtk4 because it generates a gtk-4.0/gtk.css file which
+    # breaks light/dark theme switches (gtk.css themes cannot be hot-reloaded).
+    # Just gonna give up on GTK 4 theming.
+    gtk4.theme = null;
 
     iconTheme = mkIf cfg.customTheme {
       name = "Papirus-Dark";

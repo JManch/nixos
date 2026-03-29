99c5b6e11d067e52248517af47b3c03dd9d2aee2
Joshua Manchester <JManch@protonmail.com>
2026-03-29 16:49:17 +0100

    discord: suppress service logging

--- modules/home-manager/programs/desktop/discord.nix
+++ modules/home-manager/programs/desktop/discord.nix
@@ -34,10 +34,19 @@ in
   # Electron apps core dump on exit with the default KillMode control-group.
   # This causes compositor exit to get delayed so just aggressively kill
   # these apps with Killmode mixed.
-  ns.desktop.uwsm.appUnitOverrides = lib.genAttrs [ "vesktop@.service" "discord@.service" ] (_: ''
-    [Service]
-    KillMode=mixed
-  '');
+  ns.desktop.uwsm.appUnitOverrides = {
+    "vesktop@.service" = ''
+      [Service]
+      KillMode=mixed
+    '';
+
+    "discord@.service" = ''
+      [Service]
+      # discord spams "The resource..." logs
+      StandardOutput=null
+      KillMode=mixed
+    '';
+  };
 
   ns.persistence.directories = [
     ".config/discord"

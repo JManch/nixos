0793edb49d2cace8a91607c90e649b0dc3849130
Joshua Manchester <JManch@protonmail.com>
2026-04-02 02:18:43 +0100

    tor-browser: suppress maximize on launch

--- modules/home-manager/programs/desktop/tor-browser.nix
+++ modules/home-manager/programs/desktop/tor-browser.nix
@@ -1,4 +1,9 @@
 { pkgs }:
 {
   home.packages = [ pkgs.tor-browser ];
+
+  # maximizes on launch for some reason
+  ns.desktop.hyprland.settings.windowrule = [
+    "match:class Tor Browser, suppress_event maximize"
+  ];
 }

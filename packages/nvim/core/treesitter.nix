3634a58b36209a416d671b1b71bbbb84f99f21f0
Joshua Manchester <JManch@protonmail.com>
2026-04-05 01:35:24 +0100

    nvim: add treesitter incremental selection mappings

--- packages/nvim/core/treesitter.nix
+++ packages/nvim/core/treesitter.nix
@@ -1,4 +1,7 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
+let
+  inherit (lib.nvim.binds) mkKeymap;
+in
 {
   vim.treesitter = {
     enable = true;
@@ -6,4 +9,38 @@
     addDefaultGrammars = false;
     grammars = pkgs.vimPlugins.nvim-treesitter.allGrammars;
   };
+
+  vim.keymaps = [
+    (mkKeymap [ "x" "o" ] "ah"
+      ''
+        function()
+          if vim.treesitter.get_parser(nil, nil, { error = false }) then
+            require('vim.treesitter._select').select_parent(vim.v.count1)
+          else
+            vim.lsp.buf.selection_range(vim.v.count1)
+          end
+        end
+      ''
+      {
+        lua = true;
+        desc = "Select parent treesitter node or outer incremental lsp selections";
+      }
+    )
+
+    (mkKeymap [ "x" "o" ] "ih"
+      ''
+        function()
+          if vim.treesitter.get_parser(nil, nil, { error = false }) then
+            require('vim.treesitter._select').select_child(vim.v.count1)
+          else
+            vim.lsp.buf.selection_range(-vim.v.count1)
+          end
+        end
+      ''
+      {
+        lua = true;
+        desc = "Select child treesitter node or inner incremental lsp selections";
+      }
+    )
+  ];
 }

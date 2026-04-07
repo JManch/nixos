3dc6f68882b94b5b0622bf24019c34061cae7e6c
Joshua Manchester <JManch@protonmail.com>
2026-04-07 18:07:21 +0100

    nvim: temporarily override nvf pkgs for 0.12

--- packages/nvim/package.nix
+++ packages/nvim/package.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   self,
   pkgs,
   sources,
@@ -8,8 +9,12 @@
   # use the nixpkgs revision pinned to nvf. This way we avoid breakages when
   # nixpkgs-unstable changes something e.g.
   # https://github.com/NotAShelf/nvf/issues/1312
-  pkgs = self.inputs.nvf.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
-  # inherit pkgs;
+  # pkgs = self.inputs.nvf.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+  pkgs =
+    assert lib.assertMsg (
+      self.inputs.nvf.inputs.nixpkgs.rev == "46db2e09e1d3f113a13c0d7b81e2f221c63b8ce9"
+    ) "Remove nvf nixpkgs override";
+    pkgs;
 
   extraSpecialArgs = {
     inherit sources;

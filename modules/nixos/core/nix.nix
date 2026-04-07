021abfaf6fc7d55b7f39138ad3bbd22736056b95
Joshua Manchester <JManch@protonmail.com>
2026-04-07 14:36:20 +0100

    calibre-web: remove overlay

--- modules/nixos/core/nix.nix
+++ modules/nixos/core/nix.nix
@@ -464,14 +464,6 @@ in
         #   )
         #   navidrome
         #   ;
-
-        calibre-web =
-          assert assertMsg (
-            inputs.nixpkgs.rev == "46db2e09e1d3f113a13c0d7b81e2f221c63b8ce9"
-          ) "Remove calibre-web overlay";
-          prev.calibre-web.overrideAttrs (old: {
-            pythonRelaxDeps = old.pythonRelaxDeps ++ [ "wand" ];
-          });
       })
     ];
   };

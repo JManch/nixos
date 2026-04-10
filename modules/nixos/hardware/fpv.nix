921eeae5ab499b07c2ebe623afafac310a895c5a
Joshua Manchester <JManch@protonmail.com>
2026-04-10 23:07:10 +0100

    fpv: disable edgetx for now

--- modules/nixos/hardware/fpv.nix
+++ modules/nixos/hardware/fpv.nix
@@ -12,25 +12,29 @@ in
     "The fpv module requires the chromium home-manager module to be enabled"
   ];
 
-  ns.userPackages = [
-    (
-      assert lib.assertMsg (
-        pkgs.edgetx.version == "2.11.3"
-      ) "Remove edgetx override if 2.12 has released";
-      pkgs.${ns}.edgetx
-    )
-    pkgs.${ns}.expresslrs-configurator
-    (pkgs.makeDesktopItem {
-      name = "betaflight";
-      desktopName = "Betaflight";
-      type = "Application";
-      exec = "chromium --app=https://app.betaflight.com/";
-      icon = pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/betaflight/betaflight-configurator/0c72c9f4d9a346800e07f45d8bb813dddde0249d/assets/linux/icon/bf_icon_128.png";
-        hash = "sha256-O5O/igPx4Ufh+iqW9Xed9pPUDzEc5K90i9BhBA115ts=";
-      };
-    })
-  ];
+  ns.userPackages =
+    assert lib.assertMsg (
+      pkgs.imgui.version == "1.91.4"
+    ) "Edgetx can be re-enabled now that imgui is fixed";
+    [
+      # (
+      #   assert lib.assertMsg (
+      #     pkgs.edgetx.version == "2.11.3"
+      #   ) "Remove edgetx override if 2.12 has released";
+      #   pkgs.${ns}.edgetx
+      # )
+      pkgs.${ns}.expresslrs-configurator
+      (pkgs.makeDesktopItem {
+        name = "betaflight";
+        desktopName = "Betaflight";
+        type = "Application";
+        exec = "chromium --app=https://app.betaflight.com/";
+        icon = pkgs.fetchurl {
+          url = "https://raw.githubusercontent.com/betaflight/betaflight-configurator/0c72c9f4d9a346800e07f45d8bb813dddde0249d/assets/linux/icon/bf_icon_128.png";
+          hash = "sha256-O5O/igPx4Ufh+iqW9Xed9pPUDzEc5K90i9BhBA115ts=";
+        };
+      })
+    ];
 
   services.udev.packages = [
     # These udev rules are also required for betaflight connectivity

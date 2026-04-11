35c0fd99fe1617bdc13d264ae192b95ee21d388b
Joshua Manchester <JManch@protonmail.com>
2026-04-11 01:11:15 +0100

    waylock: set lock color

--- modules/home-manager/desktop/programs/locker/waylock.nix
+++ modules/home-manager/desktop/programs/locker/waylock.nix
@@ -1,6 +1,20 @@
-{ pkgs, categoryCfg }:
+{
+  lib,
+  pkgs,
+  config,
+  categoryCfg,
+}:
+let
+  inherit (config.${lib.ns}.core) color-scheme;
+in
 {
   enableOpt = false;
   conditions = [ (categoryCfg.locker == "waylock") ];
-  categoryConfig.package = pkgs.waylock;
+  categoryConfig = {
+    defaultArgs = [
+      "-init-color"
+      "0x${color-scheme.light.palette."base04"}"
+    ];
+    package = pkgs.waylock;
+  };
 }

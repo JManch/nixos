0c0fb8383350d3bc6494ef9ac479003796e40108
Joshua Manchester <JManch@protonmail.com>
2026-05-09 22:17:20 +0100

    cachy-kernel: add zfs package and ability to change base kernel

--- modules/nixos/hardware/cachy-kernel.nix
+++ modules/nixos/hardware/cachy-kernel.nix
@@ -13,27 +13,35 @@
       {
         arch,
         flake ? inputs.nix-cachyos-kernel,
+        kernelPackage ? "linux-cachyos-latest",
       }:
-      (pkgs.callPackage "${flake.outPath}/helpers.nix" { }).kernelModuleLLVMOverride (
-        pkgs.linuxKernel.packagesFor (
-          # pkgs.cachyosKernels.linux-cachyos-latest.override {
-          flake.packages.${pkgs.stdenv.hostPlatform.system}.linux-cachyos-latest.override {
-            lto = "thin"; # basically same performance as full with better build time
-            processorOpt = arch;
-            cpusched = "bore";
-            # no reason to make this lower on laptops according to:
-            # https://discourse.ubuntu.com/t/enable-low-latency-features-in-the-generic-ubuntu-kernel-for-24-04/42255
-            # https://www.phoronix.com/news/Linux-250Hz-1000Hz-Kernel-2025
-            # https://www.phoronix.com/news/Linux-2025-Proposal-1000Hz
-            hzTicks = "1000";
-            tickrate = "full";
-            performanceGovernor = false; # we handle performance scaling with tlp or gamemode
-            preemptType = "full";
-            ccHarder = true; # extra compiler optimisations
-            bbr3 = true; # better tcp congestion control
-            hugepage = "always";
+      let
+        kernel = flake.packages.${pkgs.stdenv.hostPlatform.system}.${kernelPackage}.override {
+          lto = "thin"; # basically same performance as full with better build time
+          processorOpt = arch;
+          cpusched = "bore";
+          # no reason to make this lower on laptops according to:
+          # https://discourse.ubuntu.com/t/enable-low-latency-features-in-the-generic-ubuntu-kernel-for-24-04/42255
+          # https://www.phoronix.com/news/Linux-250Hz-1000Hz-Kernel-2025
+          # https://www.phoronix.com/news/Linux-2025-Proposal-1000Hz
+          hzTicks = "1000";
+          tickrate = "full";
+          performanceGovernor = false; # we handle performance scaling with tlp or gamemode
+          preemptType = "full";
+          ccHarder = true; # extra compiler optimisations
+          bbr3 = true; # better tcp congestion control
+          hugepage = "always";
+        };
+      in
+      ((pkgs.callPackage "${flake.outPath}/helpers.nix" { }).kernelModuleLLVMOverride (
+        pkgs.linuxKernel.packagesFor kernel
+      )).extend
+        (
+          _: _: {
+            zfs_cachyos = flake.packages.${pkgs.stdenv.hostPlatform.system}.zfs-cachyos.override {
+              inherit kernel;
+            };
           }
-        )
-      );
+        );
   };
 }

613fdc62f48469136f0136e75d92ad7fa8a2f3d9
Joshua Manchester <JManch@protonmail.com>
2026-05-28 22:59:13 +0100

    cachy-kernel: add overrides option

--- modules/nixos/hardware/cachy-kernel.nix
+++ modules/nixos/hardware/cachy-kernel.nix
@@ -14,24 +14,28 @@
         arch,
         flake ? inputs.nix-cachyos-kernel,
         kernelPackage ? "linux-cachyos-latest",
+        overrides ? { },
       }:
       let
-        kernel = flake.packages.${pkgs.stdenv.hostPlatform.system}.${kernelPackage}.override {
-          lto = "thin"; # basically same performance as full with better build time
-          processorOpt = arch;
-          cpusched = "bore";
-          # no reason to make this lower on laptops according to:
-          # https://discourse.ubuntu.com/t/enable-low-latency-features-in-the-generic-ubuntu-kernel-for-24-04/42255
-          # https://www.phoronix.com/news/Linux-250Hz-1000Hz-Kernel-2025
-          # https://www.phoronix.com/news/Linux-2025-Proposal-1000Hz
-          hzTicks = "1000";
-          tickrate = "full";
-          performanceGovernor = false; # we handle performance scaling with tlp or gamemode
-          preemptType = "full";
-          ccHarder = true; # extra compiler optimisations
-          bbr3 = true; # better tcp congestion control
-          hugepage = "always";
-        };
+        kernel = flake.packages.${pkgs.stdenv.hostPlatform.system}.${kernelPackage}.override (
+          {
+            lto = "thin"; # basically same performance as full with better build time
+            processorOpt = arch;
+            cpusched = "bore";
+            # no reason to make this lower on laptops according to:
+            # https://discourse.ubuntu.com/t/enable-low-latency-features-in-the-generic-ubuntu-kernel-for-24-04/42255
+            # https://www.phoronix.com/news/Linux-250Hz-1000Hz-Kernel-2025
+            # https://www.phoronix.com/news/Linux-2025-Proposal-1000Hz
+            hzTicks = "1000";
+            tickrate = "full";
+            performanceGovernor = false; # we handle performance scaling with tlp or gamemode
+            preemptType = "full";
+            ccHarder = true; # extra compiler optimisations
+            bbr3 = true; # better tcp congestion control
+            hugepage = "always";
+          }
+          // overrides
+        );
       in
       ((pkgs.callPackage "${flake.outPath}/helpers.nix" { }).kernelModuleLLVMOverride (
         pkgs.linuxKernel.packagesFor kernel

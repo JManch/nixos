1e0717406cdeb96cdf450051febc5e58d2e073aa
Joshua Manchester <JManch@protonmail.com>
2026-06-04 11:51:20 +0100

    nvidia: update module and fix hardware accel

--- modules/nixos/hardware/graphics/nvidia.nix
+++ modules/nixos/hardware/graphics/nvidia.nix
@@ -7,26 +7,41 @@ let
   inherit (lib) ns mkIf;
   inherit (config.${ns}.core.device) gpu;
   inherit (config.${ns}.system) desktop;
+  inherit (config.${ns}.core) home-manager;
 in
 {
   enableOpt = false;
   conditions = [ (gpu.type == "nvidia") ];
 
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [
-      libva-vdpau-driver
-      nvidia-vaapi-driver
-    ];
-  };
+  hardware.graphics.enable = true;
 
   services.xserver.videoDrivers = mkIf desktop.enable [ "nvidia" ];
 
   hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
     open = true;
+    branch = "stable";
     nvidiaSettings = false; # does not work on wayland
+    videoAcceleration = true; # this installs nvidia-vaapi-driver
     powerManagement.enable = desktop.suspend.enable;
+  };
+
+  # https://github.com/elFarto/nvidia-vaapi-driver#configuration
+  environment = {
+    systemPackages = [ pkgs.libva-utils ];
+    variables = {
+      MOZ_DISABLE_RDD_SANDBOX = 1;
+      NVD_BACKEND = "direct";
+      LIBVA_DRIVER_NAME = "nvidia";
+    };
+  };
+
+  ns.hm = mkIf (home-manager.enable && config.hardware.nvidia.videoAcceleration) {
+    programs.firefox.profiles.default.settings = {
+      "media.hardware-video-decoding.force-enabled" = true;
+      "media.av1.enabled" = true; # override to false on a per-host basis
+      "gfx.x11-egl.force-enabled" = true;
+      "widget.dmabuf.force-enabled" = true;
+    };
   };
 
   ns.persistenceHome.directories = [ ".cache/nvidia" ];

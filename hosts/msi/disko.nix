2e5ee5f6ad6276f1a2b32f8a2bc595a77541c88d
Joshua Manchester <JManch@protonmail.com>
2026-06-04 11:51:48 +0100

    msi disko: switch to simple ext4 layout

--- hosts/msi/disko.nix
+++ hosts/msi/disko.nix
@@ -1,67 +1,32 @@
-{ lib, inputs, ... }:
-let
-  vmInstall = inputs.vmInstall.value;
-in
+{ inputs, ... }:
 {
   imports = [ inputs.disko.nixosModules.default ];
 
-  disko.devices = {
-    disk."256GB-SATA" = {
-      type = "disk";
-      device =
-        if vmInstall then
-          "/dev/disk/by-path/pci-0000:04:00.0"
-        else
-          "/dev/disk/by-id/ata-Crucial_CT275MX300SSD1_163313B135A9";
-      content = {
-        type = "gpt";
-        partitions = {
-          esp = {
-            name = "boot";
-            size = "512M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [
-                "defaults"
-                "umask=0077"
-              ];
-            };
-          };
-          zfs = {
-            size = "100%";
-            content = {
-              type = "zfs";
-              pool = "msi-zroot";
-            };
+  disko.devices.disk."1TB-SATA" = {
+    type = "disk";
+    device = if inputs.vmInstall.value then "/dev/disk/by-path/pci-0000:04:00.0" else null;
+    content = {
+      type = "gpt";
+      partitions = {
+        esp = {
+          size = "512M";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "umask=0077" ];
           };
         };
-      };
-    };
 
-    zpool.msi-zroot = {
-      type = "zpool";
-      options.ashift = "12";
-
-      rootFsOptions = {
-        atime = "off";
-        mountpoint = "none";
-        xattr = "sa";
-        acltype = "posixacl";
-        compression = "lz4";
-      }
-      // lib.optionalAttrs (!vmInstall) {
-        encryption = "aes-256-gcm";
-        keyformat = "passphrase";
-        keylocation = "prompt";
-      };
-
-      datasets.root = {
-        type = "zfs_fs";
-        mountpoint = "/";
-        options.mountpoint = "legacy";
+        root = {
+          size = "100%";
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/";
+          };
+        };
       };
     };
   };

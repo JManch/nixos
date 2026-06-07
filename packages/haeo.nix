74895be6cc3d3fe6cdc0a7316ab33d929b281d69
Joshua Manchester <JManch@protonmail.com>
2026-06-08 00:31:30 +0100

    haeo: v0.4.0rc8 -> v0.4.0

--- packages/haeo.nix
+++ packages/haeo.nix
@@ -13,12 +13,7 @@ home-assistant.python.pkgs.callPackage (
   buildHomeAssistantComponent {
     owner = "hass-energy";
     domain = "haeo";
-    # inherit (sources.haeo) version;
-    version =
-      assert lib.assertMsg (
-        sources.haeo.revision == "92e687fa9cfedc5f70c8d7d65fd5e9ef18a830f7"
-      ) "remove haeo override";
-      "v0.4.0rc8";
+    inherit (sources.haeo) version;
     src = sources.haeo;
 
     dependencies = [

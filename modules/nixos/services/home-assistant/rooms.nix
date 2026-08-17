5438735e98e138984f80144713fb6fd110a2d111
Joshua Manchester <JManch@protonmail.com>
2026-08-17 16:38:59 +0100

    home-assistant/rooms: increase upper luminence threshold

--- modules/nixos/services/home-assistant/rooms.nix
+++ modules/nixos/services/home-assistant/rooms.nix
@@ -177,7 +177,7 @@ in
           luminence = {
             sensor = "joshua_presence_illuminance";
             threshold.lower = 12.0;
-            threshold.upper = 16.0;
+            threshold.upper = 32.0;
           };
 
           presenceTriggers = [

87088b4e2e136925ef40df756ff038c1630eb13f
Joshua Manchester <JManch@protonmail.com>
2026-09-01 13:01:18 +0100

    virtualisation: enable emergencyAccess in vmVariant

--- modules/nixos/system/virtualisation.nix
+++ modules/nixos/system/virtualisation.nix
@@ -187,8 +187,12 @@ in
     # We configure the vmVariant regardless of whether or not the host has
     # virtualisation enabled because it should be possible to create a VM of any host
     virtualisation.vmVariant = {
-      # Otherwise VM fails to start
-      boot.resumeDevice = mkVMOverride "";
+      boot = {
+        # Otherwise VM fails to start
+        resumeDevice = mkVMOverride "";
+        # For debugging initrd failures
+        initrd.systemd.emergencyAccess = true;
+      };
 
       ${ns} = {
         core.device = {
